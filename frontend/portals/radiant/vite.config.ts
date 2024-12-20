336068c882c037d19119994e5f3c530e44929785
Francis Lavoie <lav.francis+gh@gmail.com>
2024-12-20 10:02:27 -0500

    add docker run

--- frontend/portals/radiant/vite.config.ts
+++ frontend/portals/radiant/vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()]
 });
