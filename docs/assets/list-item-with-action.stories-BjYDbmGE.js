a5a163ea229c83c16de4ea6634fd8ca4dd130a39
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-03-12 11:52:00 +0000

    Deploying to storybook from @ Ferlab-Ste-Justine/radiant-portal@6512002d2ed71e4155405bc11cb98d042d38626e 🚀

---
+++ docs/assets/list-item-with-action.stories-BjYDbmGE.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{f as o}from"./index-DhvbgntC.js";import{L as i,a as m}from"./list-item-with-action-CmITQeYC.js";import"./index-C66Dxnp2.js";import"./ActionButton-BJyb2P8g.js";import"./dropdown-menu-BQyRnyXE.js";import"./index-DUAV1Q2A.js";import"./index-aKoabQ1X.js";import"./index-VDvXe9nZ.js";import"./index-CqVsPxxY.js";import"./Combination-BCojpehm.js";import"./index-BXOWtdLR.js";import"./index-5epIGEb9.js";import"./utils-BNf5BS2b.js";import"./createLucideIcon-DirO1-NP.js";import"./check-D0WsHlur.js";import"./button.variants-BeBGLAvg.js";import"./IconButton-BwvlZF0D.js";const b={title:"Base/Data Display/ListItem with action",component:i,tags:["autodocs"],args:{onEdit:o(),onDelete:o(),onShare:o(),onClick:o()}},e={args:{title:"Title",description:"Description"},render:r=>t.jsx("div",{className:"max-w-[450px]",children:t.jsxs(m,{bordered:!0,children:[t.jsx(i,{...r}),t.jsx(i,{...r}),t.jsx(i,{...r}),t.jsx(i,{...r})]})})};var s,n,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: "Title",
+    description: "Description"
+  },
+  render: args => <div className="max-w-[450px]">
+      <List bordered>
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+      </List>
+    </div>
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const N=["Default"];export{e as Default,N as __namedExportsOrder,b as default};
---
+++ docs/assets/tooltip.stories-CFfuGp3i.js
@@ -0,0 +1,12 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{T as p,a as n,b as a,c as m}from"./tooltip-DW4kxM8r.js";import{B as l}from"./button-C4hRftkF.js";import"./index-DUAV1Q2A.js";import"./index-aKoabQ1X.js";import"./index-VDvXe9nZ.js";import"./index-CqVsPxxY.js";import"./Combination-BCojpehm.js";import"./dropdown-menu-BQyRnyXE.js";import"./index-BXOWtdLR.js";import"./index-5epIGEb9.js";import"./utils-BNf5BS2b.js";import"./createLucideIcon-DirO1-NP.js";import"./check-D0WsHlur.js";import"./ActionButton-BJyb2P8g.js";import"./button.variants-BeBGLAvg.js";import"./index-C66Dxnp2.js";import"./IconButton-BwvlZF0D.js";const N={title:"Base/Overlays/Tooltip",component:p,args:{children:"Tooltip Content",sideOffset:8,side:"top"},decorators:[t=>r.jsx(n,{delayDuration:0,children:r.jsx(t,{})})]},o={render:t=>r.jsx("div",{className:"flex justify-center p-24",children:r.jsxs(a,{children:[r.jsx(m,{asChild:!0,children:r.jsx(l,{color:"primary",children:"Hover Me"})}),r.jsx(p,{...t})]})})};var e,i,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: args => {
+    return <div className="flex justify-center p-24">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button color="primary">Hover Me</Button>
+          </TooltipTrigger>
+          <TooltipContent {...args} />
+        </Tooltip>
+      </div>;
+  }
+}`,...(s=(i=o.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const _=["Default"];export{o as Default,_ as __namedExportsOrder,N as default};
