76ce32e58dbc0d0437a8a5c4a27f46d3250f5bd6
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-03-28 13:29:49 +0000

    Deploying to storybook from @ Ferlab-Ste-Justine/radiant-portal@d27b874faf16c9c96e75c64613e03a9ee53488b8 🚀

---
+++ docs/assets/input-search.stories-vwEbxjTf.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{f as g}from"./index-DZ0MIZXx.js";import{a as s}from"./index-B-lxVbXh.js";import{c as p}from"./utils-BNf5BS2b.js";import{B as R}from"./button-CxxI4gy-.js";import{I as q}from"./input-BBoUXjlS.js";import{r as i}from"./index-DUAV1Q2A.js";import{c as _}from"./createLucideIcon-BJ1WAg3L.js";import"./v4-CtRu48qb.js";import"./index-CpwtQhPK.js";import"./index-CSO_qfi8.js";import"./index-CqVsPxxY.js";import"./ActionButton-DnR3bAYu.js";import"./dropdown-menu-B4A7Rs1f.js";import"./Combination-GE29BHtn.js";import"./index-D74gQ3ji.js";import"./check-CSglOr1T.js";import"./button.variants-DG8R81Cn.js";import"./index-C66Dxnp2.js";import"./IconButton-C7j2XGxo.js";/**
+ * @license lucide-react v0.482.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const F=[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]],L=_("Search",F);function m({ref:a,className:u,wrapperClassName:N,type:j,onSearch:k,searchButtonProps:r,...o}){const d=i.useRef(null),[f,h]=i.useState(!1),[E,T]=i.useState(o.value),v=i.useCallback(async e=>{h(!0),await k(e),h(!1)},[]),D=i.useCallback(e=>{const n=d.current;n&&(e.key==="Enter"&&(e.preventDefault(),n.value!==""&&v(n.value)),e.key==="Escape"&&n.blur())},[d.current]);return t.jsxs("div",{className:p("flex items-center w-full",N),children:[t.jsx(q,{type:j,className:p("rounded-r-none focus:z-[2]",u),...o,onKeyDown:e=>{var n;D(e),(n=o.onKeyDown)==null||n.call(o,e)},onChange:e=>{var n;T(e.target.value),(n=o.onChange)==null||n.call(o,e)},ref:e=>{d.current=e,typeof a=="function"?a(e):a&&(a.current=e)}}),t.jsx(R,{color:"default",variant:"outlined",...r,loading:f||(r==null?void 0:r.loading),className:p("h-10 px-3 rounded-l-none ml-[-1px] hover:z-[2]",r==null?void 0:r.className),onClick:()=>v(E),children:!(r!=null&&r.loading)&&!f&&t.jsx(L,{})})]})}m.__docgenInfo={description:"",methods:[],displayName:"InputSearch",props:{onSearch:{required:!0,tsType:{name:"signature",type:"function",raw:"(value: string) => void | Promise<void>",signature:{arguments:[{type:{name:"string"},name:"value"}],return:{name:"union",raw:"void | Promise<void>",elements:[{name:"void"},{name:"Promise",elements:[{name:"void"}],raw:"Promise<void>"}]}}},description:""},searchButtonProps:{required:!1,tsType:{name:"Omit",elements:[{name:"ButtonProps"},{name:"literal",value:"'onClick'"}],raw:"Omit<ButtonProps, 'onClick'>"},description:""},ref:{required:!1,tsType:{name:"ReactRef",raw:"React.Ref<HTMLInputElement>",elements:[{name:"HTMLInputElement"}]},description:""},wrapperClassName:{required:!1,tsType:{name:"string"},description:""}}};const ae={title:"Base/Data Entry/Inputs/Input Search",component:m,args:{value:"Search value",onSearch:g(),onChange:g(),placeholder:"Placeholder",searchButtonProps:{}}},c={render:()=>t.jsx(m,{onChange:a=>s("onChange")(a),onSearch:a=>{s("onSearch")(a)},className:"max-w-[300px]",placeholder:"Placeholder",autoFocus:!0,searchButtonProps:{color:"primary",variant:"filled"}})},l={render:()=>t.jsx(m,{onChange:a=>s("onChange")(a),onSearch:a=>(s("onSearch")(a),new Promise(u=>setTimeout(()=>u(),1e3))),className:"max-w-[300px]",placeholder:"Placeholder",autoFocus:!0,searchButtonProps:{color:"primary",variant:"filled"}})};var y,x,S;c.parameters={...c.parameters,docs:{...(y=c.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => {
+    return <InputSearch onChange={e => action('onChange')(e)} onSearch={value => {
+      action('onSearch')(value);
+    }} className="max-w-[300px]" placeholder="Placeholder" autoFocus searchButtonProps={{
+      color: 'primary',
+      variant: 'filled'
+    }} />;
+  }
+}`,...(S=(x=c.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};var w,C,I;l.parameters={...l.parameters,docs:{...(w=l.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => {
+    return <InputSearch onChange={e => action('onChange')(e)} onSearch={value => {
+      action('onSearch')(value);
+      return new Promise(resolve => setTimeout(() => resolve(), 1000));
+    }} className="max-w-[300px]" placeholder="Placeholder" autoFocus searchButtonProps={{
+      color: 'primary',
+      variant: 'filled'
+    }} />;
+  }
+}`,...(I=(C=l.parameters)==null?void 0:C.docs)==null?void 0:I.source}}};const re=["Default","Async"];export{l as Async,c as Default,re as __namedExportsOrder,ae as default};
