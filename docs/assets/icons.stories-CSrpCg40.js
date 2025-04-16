5659c7a827b03225c88e41231e31d109ceb5214b
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-04-16 16:38:25 +0000

    Deploying to storybook from @ Ferlab-Ste-Justine/radiant-portal@1675823042ff1a7e95a7501a075284bb80d5bf62 🚀

---
+++ docs/assets/icons.stories-CSrpCg40.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{a as c,E as g,c as x,G as f,b as I,L as v}from"./less-than-or-equal-operator-icon-BeMEXZ7b.js";import{N as j}from"./not-in-operator-icon-C8Fl8SMz.js";import{T,a as C,b as w}from"./transcript-mane-plus-icon-DF5vGstD.js";import"./index-yBjzXJbu.js";function d({size:a=16,fill:n="currentColor",...s}){return e.jsx("svg",{height:a,width:a,viewBox:"0 0 16 16",xmlns:"http://www.w3.org/2000/svg",fill:n,...s,children:e.jsx("path",{d:"M7.152 2.9568L0.956249 12.87C0.53997 13.5361 1.01881 14.4 1.80425 14.4H14.1958C14.9812 14.4 15.46 13.5361 15.0437 12.87L8.848 2.9568C8.45633 2.33014 7.54367 2.33014 7.152 2.9568Z"})})}d.__docgenInfo={description:"",methods:[],displayName:"ShapeTriangleUpIcon",props:{size:{required:!1,tsType:{name:"number"},description:"",defaultValue:{value:"16",computed:!1}},fill:{defaultValue:{value:"'currentColor'",computed:!1},required:!1}}};function p({size:a=16,fill:n="currentColor",...s}){return e.jsx("svg",{height:a,width:a,viewBox:"0 0 16 16",xmlns:"http://www.w3.org/2000/svg",fill:n,...s,children:e.jsx("path",{d:"M7.152 13.0432L0.956249 3.12999C0.539969 2.46394 1.01881 1.59999 1.80425 1.59999H14.1958C14.9812 1.59999 15.46 2.46395 15.0437 3.12999L8.848 13.0432C8.45633 13.6699 7.54367 13.6699 7.152 13.0432Z"})})}p.__docgenInfo={description:"",methods:[],displayName:"ShapeTriangleDownIcon",props:{size:{required:!1,tsType:{name:"number"},description:"",defaultValue:{value:"16",computed:!1}},fill:{defaultValue:{value:"'currentColor'",computed:!1},required:!1}}};function u({size:a=16,fill:n="currentColor",...s}){return e.jsx("svg",{height:a,width:a,viewBox:"0 0 16 16",xmlns:"http://www.w3.org/2000/svg",fill:n,...s,children:e.jsx("circle",{cx:"7.99985",cy:"8.00001",r:"6.4"})})}u.__docgenInfo={description:"",methods:[],displayName:"ShapeCircleIcon",props:{size:{required:!1,tsType:{name:"number"},description:"",defaultValue:{value:"16",computed:!1}},fill:{defaultValue:{value:"'currentColor'",computed:!1},required:!1}}};function h({size:a=16,fill:n="currentColor",...s}){return e.jsx("svg",{height:a,width:a,viewBox:"0 0 16 16",xmlns:"http://www.w3.org/2000/svg",fill:n,...s,children:e.jsx("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M7.99985 14.3998C11.5345 14.3998 14.3999 11.5344 14.3999 7.99982C14.3999 4.4652 11.5345 1.59982 7.99985 1.59982C4.46523 1.59982 1.59985 4.4652 1.59985 7.99982C1.59985 11.5344 4.46523 14.3998 7.99985 14.3998ZM8 11.9998C10.2091 11.9998 12 10.2089 12 7.9998C12 5.79066 10.2091 3.9998 8 3.9998C5.79086 3.9998 4 5.79066 4 7.9998C4 10.2089 5.79086 11.9998 8 11.9998Z"})})}h.__docgenInfo={description:"",methods:[],displayName:"ShapeDonutIcon",props:{size:{required:!1,tsType:{name:"number"},description:"",defaultValue:{value:"16",computed:!1}},fill:{defaultValue:{value:"'currentColor'",computed:!1},required:!1}}};function m({size:a=16,fill:n="currentColor",...s}){return e.jsx("svg",{height:a,width:a,viewBox:"0 0 16 16",xmlns:"http://www.w3.org/2000/svg",fill:n,...s,children:e.jsx("path",{d:"M14.3999 13.4C14.3999 13.9523 13.9521 14.4 13.3999 14.4H2.59985C2.04757 14.4 1.59985 13.9523 1.59985 13.4V2.60001C1.59985 2.04772 2.04757 1.60001 2.59985 1.60001H13.3999C13.9521 1.60001 14.3999 2.04772 14.3999 2.60001V13.4Z"})})}m.__docgenInfo={description:"",methods:[],displayName:"ShapeSquareIcon",props:{size:{required:!1,tsType:{name:"number"},description:"",defaultValue:{value:"16",computed:!1}},fill:{defaultValue:{value:"'currentColor'",computed:!1},required:!1}}};const O={title:"General/Icons",component:c,args:{size:24,fill:"currentColor",className:"shrink-0"}},t={args:{},render:a=>e.jsxs("div",{className:"space-y-6",children:[e.jsxs("div",{children:[e.jsx("h1",{className:"font-bold text-xl",children:"Icons:"}),e.jsxs("div",{children:["We use Lucide Icons:"," ",e.jsx("a",{href:"https://lucide.dev/icons/",target:"_blank",className:"text-primary underline hover:no-underline",children:"https://lucide.dev/icons/"})]})]}),e.jsx("h1",{className:"font-bold text-xl",children:"Operators:"}),e.jsxs("div",{className:"grid grid-cols-12 gap-2",children:[e.jsx(r,{icon:c,title:"Element",args:a}),e.jsx(r,{icon:g,title:"Equal",args:a}),e.jsx(r,{icon:x,title:"Greater Than",args:a}),e.jsx(r,{icon:f,title:"Grater Than Or Equal",args:a}),e.jsx(r,{icon:I,title:"Less Than",args:a}),e.jsx(r,{icon:v,title:"Less Than Or Equal",args:a}),e.jsx(r,{icon:j,title:"Not In",args:a})]}),e.jsx("h1",{className:"font-bold text-xl",children:"Shapes:"}),e.jsxs("div",{className:"grid grid-cols-12 gap-2",children:[e.jsx(r,{icon:d,title:"Triangle Up",args:a}),e.jsx(r,{icon:p,title:"Triangle Down",args:a}),e.jsx(r,{icon:u,title:"Circle",args:a}),e.jsx(r,{icon:h,title:"Donut",args:a}),e.jsx(r,{icon:m,title:"Square",args:a})]}),e.jsx("h1",{className:"font-bold text-xl",children:"Transcript:"}),e.jsxs("div",{className:"grid grid-cols-12 gap-2",children:[e.jsx(r,{icon:T,title:"Canonical",args:a}),e.jsx(r,{icon:C,title:"Mane Plus",args:a}),e.jsx(r,{icon:w,title:"Mane Select",args:a})]})]})},r=({icon:a,title:n,args:s})=>e.jsxs("div",{className:"flex flex-col gap-1 items-center",children:[e.jsx(a,{...s}),e.jsx("div",{className:"text-xs text-center",children:n})]});var l,o,i;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {},
+  render: args => <div className="space-y-6">
+      <div>
+        <h1 className="font-bold text-xl">Icons:</h1>
+        <div>
+          We use Lucide Icons:{' '}
+          <a href="https://lucide.dev/icons/" target="_blank" className="text-primary underline hover:no-underline">
+            https://lucide.dev/icons/
+          </a>
+        </div>
+      </div>
+      <h1 className="font-bold text-xl">Operators:</h1>
+      <div className="grid grid-cols-12 gap-2">
+        <IconDisplay icon={ElementOperatorIcon} title="Element" args={args} />
+        <IconDisplay icon={EqualOperatorIcon} title="Equal" args={args} />
+        <IconDisplay icon={GreaterThanOperatorIcon} title="Greater Than" args={args} />
+        <IconDisplay icon={GreaterThanOrEqualOperatorIcon} title="Grater Than Or Equal" args={args} />
+        <IconDisplay icon={LessThanOperatorIcon} title="Less Than" args={args} />
+        <IconDisplay icon={LessThanOrEqualOperatorIcon} title="Less Than Or Equal" args={args} />
+        <IconDisplay icon={NotInOperatorIcon} title="Not In" args={args} />
+      </div>
+      <h1 className="font-bold text-xl">Shapes:</h1>
+      <div className="grid grid-cols-12 gap-2">
+        <IconDisplay icon={ShapeTriangleUpIcon} title="Triangle Up" args={args} />
+        <IconDisplay icon={ShapeTriangleDownIcon} title="Triangle Down" args={args} />
+        <IconDisplay icon={ShapeCircleIcon} title="Circle" args={args} />
+        <IconDisplay icon={ShapeDonutIcon} title="Donut" args={args} />
+        <IconDisplay icon={ShapeSquareIcon} title="Square" args={args} />
+      </div>
+      <h1 className="font-bold text-xl">Transcript:</h1>
+      <div className="grid grid-cols-12 gap-2">
+        <IconDisplay icon={TranscriptCanonicalIcon} title="Canonical" args={args} />
+        <IconDisplay icon={TranscriptManePlusIcon} title="Mane Plus" args={args} />
+        <IconDisplay icon={TranscriptManeSelectIcon} title="Mane Select" args={args} />
+      </div>
+    </div>
+}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const E=["Default"];export{t as Default,E as __namedExportsOrder,O as default};
