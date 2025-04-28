3bcb195401cde8b9975484757e4e8b6c1578d627
lflangis <65532894+lflangis@users.noreply.github.com>
2025-04-28 19:10:36 +0000

    Deploying to storybook from @ Ferlab-Ste-Justine/radiant-portal@22e2d31696626deff1d24c5852722dd9ba56fb97 🚀

---
+++ docs/assets/action-button.stories-DCS2YjK1.js
@@ -0,0 +1,13 @@
+import{j as o}from"./jsx-runtime-Cf8x2fCZ.js";import{f as a}from"./index-BZkcKs8Z.js";import{A as n}from"./ActionButton-C_4cAaVG.js";import{b as p}from"./utils-DCfAwncM.js";import"./index-yBjzXJbu.js";import"./dropdown-menu-BtHca8-r.js";import"./index-tvICUrOf.js";import"./index-Csi1vtvD.js";import"./index-_r67kdfS.js";import"./index-fNjTmf9T.js";import"./Combination-CmKP2mMJ.js";import"./index-CueSDTQu.js";import"./index-DJkr1wGX.js";import"./index-pLOVI5Ig.js";import"./utils-CytzSlOG.js";import"./check-CfPT3E_d.js";import"./createLucideIcon-DKFpjrVJ.js";import"./button.variants-B79LQKoe.js";import"./index-C66Dxnp2.js";import"./ellipsis-NIzCCAdy.js";const D={title:"Buttons/Action Button",component:n,args:{onClick:a(),size:"default",disabled:!1,actions:[],onDefaultAction:a()}},t={args:{size:"default",children:"Button"},render:m=>o.jsx("div",{className:"flex flex-col gap-2",children:p.map(r=>o.jsx(n,{variant:r,...m,children:r}))})};var i,e,s;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: 'default',
+    children: 'Button'
+  },
+  render: args => {
+    return <div className="flex flex-col gap-2">
+        {buttonVariants.map(variant => <ActionButton variant={variant} {...args}>
+            {variant}
+          </ActionButton>)}
+      </div>;
+  }
+}`,...(s=(e=t.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const O=["Variants"];export{t as Variants,O as __namedExportsOrder,D as default};
