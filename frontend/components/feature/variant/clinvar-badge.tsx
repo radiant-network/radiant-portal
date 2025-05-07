53f3a3bc64b43b73e2b23c809fb93fe3284258c5
Olivier Castro-Perrier <28421005+oliviercperrier@users.noreply.github.com>
2025-05-07 09:45:43 -0400

    🩹 Small fix clinvar badge

--- frontend/components/feature/variant/clinvar-badge.tsx
+++ frontend/components/feature/variant/clinvar-badge.tsx
@@ -1,4 +1,6 @@
+import ConditionalWrapper from '@/components/base/conditional-wrapper';
 import { Badge, BadgeProps } from '@/components/base/ui/badge';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/base/ui/tooltip';
 import { useI18n } from '@/components/hooks/i18n';
 
 interface ClinVarBadgeProps extends BadgeProps {
@@ -6,36 +8,95 @@ interface ClinVarBadgeProps extends BadgeProps {
 }
 
 // TODO: Should be replace with enum with API exposes one
-export const ClinVarValueMap: Record<string, BadgeProps['variant']> = {
-  other: 'slate',
-  association_not_found: 'slate',
-  uncertain_significance: 'orange',
-  likely_benign: 'lime',
-  likely_pathogenic: 'orange',
-  _low_penetrance: 'slate',
-  low_penetrance: 'slate',
-  risk_factor: 'slate',
-  association: 'slate',
-  uncertain_risk_allele: 'slate',
-  pathogenic: 'red',
-  protective: 'slate',
-  conflicting_interpretations_of_pathogenicity: 'orange',
-  not_provided: 'slate',
-  established_risk_allele: 'slate',
-  likely_risk_allele: 'slate',
-  drug_response: 'slate',
-  benign: 'green',
-  confers_sensitivity: 'slate',
+export const ClinVarValueMap: Record<
+  string,
+  {
+    color: BadgeProps['variant'];
+    tooltipKey?: string;
+  }
+> = {
+  other: {
+    color: 'slate',
+  },
+  association_not_found: {
+    color: 'slate',
+  },
+  uncertain_significance: {
+    color: 'yellow',
+    tooltipKey: 'variant.clinVarsTooltips.uncertain_significance',
+  },
+  likely_benign: {
+    color: 'blue',
+    tooltipKey: 'variant.clinVarsTooltips.likely_benign',
+  },
+  likely_pathogenic: {
+    color: 'orange',
+    tooltipKey: 'variant.clinVarsTooltips.likely_pathogenic',
+  },
+  _low_penetrance: {
+    color: 'slate',
+  },
+  low_penetrance: {
+    color: 'slate',
+  },
+  risk_factor: {
+    color: 'slate',
+  },
+  association: {
+    color: 'slate',
+  },
+  uncertain_risk_allele: {
+    color: 'yellow',
+    tooltipKey: 'variant.clinVarsTooltips.uncertain_risk_allele',
+  },
+  pathogenic: {
+    color: 'red',
+  },
+  protective: {
+    color: 'slate',
+  },
+  conflicting_interpretations_of_pathogenicity: {
+    color: 'orange',
+  },
+  not_provided: {
+    color: 'slate',
+  },
+  established_risk_allele: {
+    color: 'slate',
+  },
+  likely_risk_allele: {
+    color: 'slate',
+  },
+  drug_response: {
+    color: 'slate',
+  },
+  benign: {
+    color: 'green',
+  },
+  confers_sensitivity: {
+    color: 'slate',
+  },
 };
 
 function ClinVarBadge({ value, ...props }: ClinVarBadgeProps) {
   const { t } = useI18n();
   const normalizedValue = value.toLowerCase();
+  const meta = ClinVarValueMap[normalizedValue];
 
   return (
-    <Badge variant={ClinVarValueMap[normalizedValue] || 'slate'} {...props}>
-      {t(`variant.clinVars.${normalizedValue}`)}
-    </Badge>
+    <ConditionalWrapper
+      condition={!!meta.tooltipKey}
+      wrapper={children => (
+        <Tooltip>
+          <TooltipTrigger>{children}</TooltipTrigger>
+          <TooltipContent>{t(meta.tooltipKey!)}</TooltipContent>
+        </Tooltip>
+      )}
+    >
+      <Badge variant={meta.color || 'slate'} {...props}>
+        {t(`variant.clinVars.${normalizedValue}`)}
+      </Badge>
+    </ConditionalWrapper>
   );
 }
 
