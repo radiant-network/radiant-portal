79a0d5308aa49bf9a6cc4391591ef0f54dbbf9d0
Luc-Frédéric Langis <luc-frederic.langis.hsj@ssss.gouv.qc.ca>
2025-05-09 09:46:12 -0400

    ✨feat(table): SJRA-187 remove row pinning feature

--- frontend/components/base/data-table/cells/pin-row-cell.tsx
+++ frontend/components/base/data-table/cells/pin-row-cell.tsx
@@ -2,6 +2,24 @@ import { Button } from '@/components/base/ui/button';
 import { CellContext } from '@tanstack/react-table';
 import { PinIcon, PinOffIcon } from 'lucide-react';
 
+/**
+ * Config for TableColumnDef
+ * {
+ *   id: 'pinRow',
+ *   cell: PinRowCell,
+ *   size: 40,
+ *   enableResizing: false,
+ *   enablePinning: false,
+ * },
+ *
+ * Config for ColumnSettings
+ * {
+ *   id: 'pinRow',
+ *   visible: true,
+ *   fixed: true,
+ *   pinningPosition: 'left',
+ * },
+ */
 function PinRowCell({ row }: CellContext<any, any>) {
   return (
     <div className="flex justify-center items-center">
