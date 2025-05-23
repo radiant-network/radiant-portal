11c240baa25e9994ff7dce21ba098e27b8b5333f
Francis L <lav.francis+git@gmail.com>
2025-05-23 11:01:43 -0400

    fix: sjra-232 RQDM tooltip, icon and typo

--- frontend/apps/variant-exploration/src/feature/occurrence-table/frequency-section.tsx
+++ frontend/apps/variant-exploration/src/feature/occurrence-table/frequency-section.tsx
@@ -1,6 +1,9 @@
 import { useI18n } from '@/components/hooks/i18n';
 import DetailSection, { DetailItem } from './detail-section';
 import { ExpendedOccurrence } from '@/api/api';
+import { Diamond } from 'lucide-react';
+import ShapeDiamondIcon from '@/components/base/icons/shape-diamond-icon';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/base/ui/tooltip';
 
 type FrequencySectionProps = {
   data: ExpendedOccurrence;
@@ -8,11 +11,37 @@ type FrequencySectionProps = {
 
 export default function FrequencySection({ data }: FrequencySectionProps) {
   const { t } = useI18n();
+  const affected = (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <span className='inline-flex gap-1 items-center'>
+          {t('occurrenceExpend.frequencies.myOrganization')} 
+          <ShapeDiamondIcon className='w-[13px] h-[13px] text-red-500' />
+        </span>
+      </TooltipTrigger>
+      <TooltipContent>
+        {t('occurrenceExpend.frequencies.myOrganizationAffectedTooltip')}
+      </TooltipContent>
+    </Tooltip>
+  );
 
+  const nonAffected = (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <span className='inline-flex gap-1 items-center'>
+          {t('occurrenceExpend.frequencies.myOrganization')} 
+          <Diamond className='w-[13px] h-[13px] text-green-500' />
+        </span>
+      </TooltipTrigger>
+      <TooltipContent>
+        {t('occurrenceExpend.frequencies.myOrganizationNonAffectedTooltip')}
+      </TooltipContent>
+    </Tooltip>
+  );
   return (
     <DetailSection title={t('occurrenceExpend.frequencies.title')}>
-      <DetailItem title={t('occurrenceExpend.frequencies.myOrganization')} value="-" />
-      <DetailItem title={t('occurrenceExpend.frequencies.myOrganization')} value="-" />
+      <DetailItem title={affected} value="-" />
+      <DetailItem title={nonAffected} value="-" />
       <DetailItem title={t('occurrenceExpend.frequencies.gnomad')} value="-" />
     </DetailSection>
   );
