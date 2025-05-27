6c866e6b1efdf86b3f8ff4a8bd12bc0410672454
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-05-27 11:42:11 +0000

    Deploying to storybook from @ Ferlab-Ste-Justine/radiant-portal@efbd9aa17af288230cb41e1fb4ba5bd74f4cbac2 🚀

---
+++ docs/assets/auto-complete.stories-9xyEU1Vl.js
@@ -0,0 +1,26 @@
+import{j as S}from"./jsx-runtime-Cf8x2fCZ.js";import{r as s}from"./index-t5q4d8OJ.js";import{fn as A}from"./index-BgJgh-x_.js";import{a as u}from"./index-B-lxVbXh.js";import{A as i}from"./auto-complete-uKrr_USB.js";import"./index-yBjzXJbu.js";import"./v4-CtRu48qb.js";import"./useDebounce-zphzDq9b.js";import"./index-w83wLIv_.js";import"./index-Bjkhh2p3.js";import"./index-CC5eZYhG.js";import"./index-fNjTmf9T.js";import"./index-KhTUl610.js";import"./Combination-CdAak5pT.js";import"./index-V1T-MO6M.js";import"./utils-CytzSlOG.js";import"./dialog-DVZakI6I.js";import"./x-Bt7o6wSZ.js";import"./createLucideIcon-BOZfVBeY.js";import"./skeleton-CfkhzHGY.js";import"./check-1JYhj4AL.js";const p=[{value:"next.js",label:"Next.js"},{value:"sveltekit",label:"SvelteKit"},{value:"nuxt.js",label:"Nuxt.js"},{value:"remix",label:"Remix"},{value:"astro",label:"Astro"},{value:"wordpress",label:"WordPress"},{value:"express.js",label:"Express.js"},{value:"nest.js",label:"Nest.js"}],B={title:"Data Entry/Inputs/Auto Complete",component:i,args:{value:"",options:p,onChange:A(),placeholder:"Placeholder"}},a={render:()=>{const[r,n]=s.useState("nest.js");return S.jsx(i,{value:r,onChange:e=>{n(e),u("onChange")(e)},className:"max-w-[300px]",placeholder:"Placeholder",options:p})}},o={render:()=>{const[r,n]=s.useState(""),[e,c]=s.useState(!1),[f,j]=s.useState([]),C=async t=>(c(!0),u("onSearch")(t),new Promise(l=>{setTimeout(()=>l(p),1e3)}).then(l=>j(l)).finally(()=>c(!1)));return S.jsx(i,{value:r,onChange:t=>{n(t),u("onChange")(t)},options:f,loading:e,onSearch:C,debounceDelay:300,className:"max-w-[300px]",placeholder:"Placeholder"})}};var m,d,h;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState<string | undefined>('nest.js');
+    return <AutoComplete value={value} onChange={value => {
+      setValue(value);
+      action('onChange')(value);
+    }} className="max-w-[300px]" placeholder="Placeholder" options={FRAMEWORKS} />;
+  }
+}`,...(h=(d=a.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};var v,g,x;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState<string | undefined>('');
+    const [loading, setLoading] = useState(false);
+    const [options, setOptions] = useState<Option[]>([]);
+    const handleSearch = async (searchValue: string) => {
+      setLoading(true);
+      action('onSearch')(searchValue);
+      return new Promise<Option[]>(resolve => {
+        setTimeout(() => resolve(FRAMEWORKS), 1000);
+      }).then(results => setOptions(results)).finally(() => setLoading(false));
+    };
+    return <AutoComplete value={value} onChange={value => {
+      setValue(value);
+      action('onChange')(value);
+    }} options={options} loading={loading} onSearch={handleSearch} debounceDelay={300} className="max-w-[300px]" placeholder="Placeholder" />;
+  }
+}`,...(x=(g=o.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const G=["Default","Async"];export{o as Async,a as Default,G as __namedExportsOrder,B as default};
---
+++ docs/assets/input-search.stories-Bj_-swSw.js
@@ -0,0 +1,20 @@
+import{j as u}from"./jsx-runtime-Cf8x2fCZ.js";import{fn as n}from"./index-BgJgh-x_.js";import{a}from"./index-B-lxVbXh.js";import{I as t}from"./input-search-DEgtsB5u.js";import"./index-yBjzXJbu.js";import"./v4-CtRu48qb.js";import"./utils-CytzSlOG.js";import"./button-CbbVWId-.js";import"./index-t5q4d8OJ.js";import"./index-Bjkhh2p3.js";import"./index-CC5eZYhG.js";import"./index-fNjTmf9T.js";import"./ActionButton-D4jVLNva.js";import"./dropdown-menu-xf-jiMEf.js";import"./index-KhTUl610.js";import"./Combination-CdAak5pT.js";import"./index-BiFLoO8l.js";import"./index-CTFHtJli.js";import"./index-V1T-MO6M.js";import"./check-1JYhj4AL.js";import"./createLucideIcon-BOZfVBeY.js";import"./button.variants-B79LQKoe.js";import"./index-C66Dxnp2.js";import"./ellipsis-e_tKH1yv.js";import"./spinner-BoKAmKqu.js";import"./input-DXdBqDMR.js";const G={title:"Data Entry/Inputs/Input Search",component:t,args:{value:"Search value",onSearch:n(),onChange:n(),placeholder:"Placeholder",searchButtonProps:{}}},e={render:()=>u.jsx(t,{onChange:r=>a("onChange")(r),onSearch:r=>{a("onSearch")(r)},className:"max-w-[300px]",placeholder:"Placeholder",autoFocus:!0,searchButtonProps:{color:"primary",variant:"filled"}})},o={render:()=>u.jsx(t,{onChange:r=>a("onChange")(r),onSearch:r=>(a("onSearch")(r),new Promise(h=>setTimeout(()=>h(),1e3))),className:"max-w-[300px]",placeholder:"Placeholder",autoFocus:!0,searchButtonProps:{color:"primary",variant:"filled"}})};var c,s,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    return <InputSearch onChange={e => action('onChange')(e)} onSearch={value => {
+      action('onSearch')(value);
+    }} className="max-w-[300px]" placeholder="Placeholder" autoFocus searchButtonProps={{
+      color: 'primary',
+      variant: 'filled'
+    }} />;
+  }
+}`,...(p=(s=e.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};var m,i,l;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    return <InputSearch onChange={e => action('onChange')(e)} onSearch={value => {
+      action('onSearch')(value);
+      return new Promise(resolve => setTimeout(() => resolve(), 1000));
+    }} className="max-w-[300px]" placeholder="Placeholder" autoFocus searchButtonProps={{
+      color: 'primary',
+      variant: 'filled'
+    }} />;
+  }
+}`,...(l=(i=o.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const H=["Default","Async"];export{o as Async,e as Default,H as __namedExportsOrder,G as default};
