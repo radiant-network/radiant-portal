6dd5578ebd8eb7f1c87ec316b03d335ca6f0fae7
Celine Pelletier <cpelletier@ferlab.bio>
2025-05-27 13:17:41 -0400

    feat: SJRA-267 rename tables

---
+++ backend/scripts/init-sql/migrations/000005_rename_join_tables.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE task_has_sequencing_experiments RENAME TO task_has_sequencing_experiment;
+ALTER TABLE task_has_related_tasks RENAME TO task_has_related_task;
+ALTER TABLE task_has_documents RENAME TO task_has_document;
+ALTER TABLE document_has_patients RENAME TO document_has_patient;
