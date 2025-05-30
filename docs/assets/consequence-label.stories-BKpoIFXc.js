3421d5f1b2cc10b7a624cbd12a038c5e0ec3cb2d
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-05-30 16:34:33 +0000

    Deploying to storybook from @ Ferlab-Ste-Justine/radiant-portal@1daad25f863fea822039760872e7e87bb3ad543e 🚀

---
+++ docs/assets/consequence-label.stories-BKpoIFXc.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{V as n}from"./api-CU3RBd8i.js";import{C as c}from"./consequence-label-DCEmJRuv.js";import"./index-yBjzXJbu.js";import"./impact-icon-DFO-Wu40.js";import"./shape-circle-icon-CMMXr8pB.js";import"./utils-CytzSlOG.js";import"./i18n-CRCotlm3.js";import"./iframe-ChEvkuyd.js";import"./index-t5q4d8OJ.js";import"./index-C66Dxnp2.js";const x={title:"Feature/Variant/Consequence Label",component:c,args:{vepImpact:"HIGH",consequence:"missense_variant",size:"default"}},a={render:()=>e.jsxs("div",{className:"space-y-6",children:[e.jsxs("div",{className:"space-y-2",children:[e.jsx("label",{children:"Size: default"}),Object.keys(n).map(s=>e.jsx(c,{vepImpact:s,consequence:"Consequence"},s))]}),e.jsxs("div",{className:"space-y-2",children:[e.jsx("label",{children:"Size: sm"}),Object.keys(n).map(s=>e.jsx(c,{size:"sm",vepImpact:s,consequence:"Consequence"},s))]})]})};var t,p,m;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="space-y-6">
+        <div className="space-y-2">
+          <label>Size: default</label>
+          {Object.keys(VepImpact).map(impact => <ConsequenceLabel key={impact} vepImpact={impact as VepImpact} consequence="Consequence" />)}
+        </div>
+        <div className="space-y-2">
+          <label>Size: sm</label>
+          {Object.keys(VepImpact).map(impact => <ConsequenceLabel key={impact} size="sm" vepImpact={impact as VepImpact} consequence="Consequence" />)}
+        </div>
+      </div>;
+  }
+}`,...(m=(p=a.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const I=["Default"];export{a as Default,I as __namedExportsOrder,x as default};
---
+++ docs/assets/utils-4U2-jAxh.js
@@ -0,0 +1,1 @@
+const e=["default","secondary","destructive","outline","red","orange","yellow","lime","green","cyan","blue","violet","fuchsia","slate"],a=["default","ghost","destructive"];export{e as b,a as n};
