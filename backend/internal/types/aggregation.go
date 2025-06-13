5329cdd0c062090d588b49b453f984369d977d79
Celine Pelletier <cpelletier@ferlab.bio>
2025-06-13 12:59:56 -0400

    feat: SJRA-314 add cases filters endpoint

--- backend/internal/types/aggregation.go
+++ backend/internal/types/aggregation.go
@@ -3,6 +3,7 @@ package types
 // Aggregation represents an aggregation result
 // @Description Aggregation represents an aggregation result
 type Aggregation struct {
-	Bucket string `json:"key"`   // Bucket key
-	Count  int64  `json:"count"` // Count in the bucket
+	Bucket string `json:"key"`             // Bucket key
+	Label  string `json:"label,omitempty"` // Label corresponding to the key
+	Count  int64  `json:"count"`           // Count in the bucket
 } // @Name Aggregation
