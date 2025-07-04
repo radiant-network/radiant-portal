d5dd9c4f1031b224bf0a5b10c3733a638a08c3ab
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-07-04 02:58:21 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@400ef0d9efb43e5926e88d61f5c27dcc7667dcb2 🚀

--- docs/assets/transcript-id-link.stories-Cz7WYna2.js
+++
@@ -1,5 +0,0 @@
-import{j as p}from"./jsx-runtime-D_zvdyIk.js";import{T as a}from"./transcript-id-link-DvAlsieC.js";import"./transcript-canonical-icon-DVFe8yV-.js";import"./tooltip-CUn8_aCA.js";import"./index-DQLiH3RP.js";import"./index-D-AYaadb.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./index-ClAAgfyD.js";import"./index-8F5FSkgb.js";import"./index-C5A_jyAq.js";import"./index-Czeu9DHN.js";import"./utils-D-KgF5mV.js";import"./i18n-CMxpINeg.js";import"./iframe-1ppY9nM3.js";const E={title:"Feature/Variant/Transcript ID Link",component:a,args:{transcriptId:"ENST00000367770",isCanonical:!0}},r={render:i=>p.jsx(a,{...i})};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: args => {
-    return <TranscriptIdLink {...args} />;
-  }
-}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const L=["Default"];export{r as Default,L as __namedExportsOrder,E as default};
