4138d12bda514d31c9e2e56c5dd0a1b2a05dea46
Olivier Castro-Perrier <28421005+oliviercperrier@users.noreply.github.com>
2025-07-06 22:42:07 -0400

    🎨 Fix styling and remove unecessary classNames

--- frontend/components/base/buttons/filter-button.tsx
+++ frontend/components/base/buttons/filter-button.tsx
@@ -77,9 +77,7 @@ export default function FilterButton({
   };
 
   const handleSelect = (value: string) => {
-    const newSelected = selected.includes(value)
-      ? selected.filter((item) => item !== value)
-      : [...selected, value];
+    const newSelected = selected.includes(value) ? selected.filter(item => item !== value) : [...selected, value];
     onSelect(newSelected);
   };
 
@@ -92,10 +90,12 @@ export default function FilterButton({
   return (
     <Popover open={open} onOpenChange={handleOpenChange}>
       <PopoverTrigger asChild>
-        <Button variant={variant} className={cn('h-8 py-2 px-3 border-dashed text-sm text-foreground', className)}>
+        <Button variant={variant} size="sm" className={cn('border-dashed', className)}>
           {icon ? icon : <PlusCircle className="" />}
           <span>{label}</span>
-          {!actionMode && selectedCount > 0 && <Badge className="w-[20px] h-[20px] items-center justify-center">{selectedCount}</Badge>}
+          {!actionMode && selectedCount > 0 && (
+            <Badge className="size-5 items-center justify-center">{selectedCount}</Badge>
+          )}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[229px] max-h-[240px] p-0 flex flex-col" align="start">
@@ -103,13 +103,11 @@ export default function FilterButton({
           <CommandInput
             placeholder={placeholder || label || 'Search...'}
             leftAddon={<Search size={16} className="text-muted-foreground mr-1" />}
-            className="rounded-none"
-            wrapperClassName="shrink-0 h-[40px] border-t-0 border-l-0 border-b-1 border-r-0 border-color-border rounded-none"
           />
-          <CommandList className="overflow-y-auto p-0 flex-1 grow-1 basis-auto">
+          <CommandList>
             <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup className="p-0">
-              {options.map((option) => {
+            <CommandGroup>
+              {options.map(option => {
                 const IconComponent = option.icon;
                 return (
                   <CommandItem
@@ -120,12 +118,12 @@ export default function FilterButton({
                         setOpen(false);
                       }
                     }}
-                    className="h-[32px] px-0 py-0 overflow-hidden"
+                    className="p-0 overflow-hidden"
                   >
                     {actionMode ? (
                       <Button
                         variant="ghost"
-                        className="mx-2 my-1.5 p-0 h-full w-full items-center justify-start"
+                        className="mx-2 my-1.5 p-0 h-full w-full items-center justify-start font-normal"
                       >
                         {option.label}
                       </Button>
@@ -137,32 +135,26 @@ export default function FilterButton({
                           onCheckedChange={() => {
                             handleSelect(option.key || '');
                           }}
-                          onMouseDown={(e) => {
+                          onMouseDown={e => {
                             e.preventDefault();
                           }}
                         />
-                        <div className="flex w-full items-center gap-1 p-0 overflow-hidden min-w-0">
+                        <div className="flex w-full items-center gap-1 overflow-hidden min-w-0">
                           {IconComponent && <IconComponent size={20} />}
-                          { withTooltip ? (
+                          {withTooltip ? (
                             <TooltipProvider>
                               <Tooltip>
                                 <TooltipTrigger asChild>
                                   <div className="truncate">
-                                    <span className="flex-1 min-w-0">
-                                      {option.key}
-                                    </span>
+                                    <span className="flex-1 min-w-0">{option.key}</span>
                                     <span className="text-muted-foreground"> - {option.label}</span>
                                   </div>
                                 </TooltipTrigger>
-                                <TooltipContent>
-                                  {option.tooltip || option.label}
-                                </TooltipContent>
+                                <TooltipContent>{option.tooltip || option.label}</TooltipContent>
                               </Tooltip>
                             </TooltipProvider>
                           ) : (
-                            <span className="truncate flex-1 min-w-0">
-                              {option.label}
-                            </span>
+                            <span className="truncate flex-1 min-w-0">{option.label}</span>
                           )}
                         </div>
                       </div>
@@ -174,10 +166,11 @@ export default function FilterButton({
           </CommandList>
         </Command>
         {selectedCount !== 0 && !actionMode && (
-          <div className="h-[40px] flex-shrink-0">
+          <div className="px-1 pb-1">
             <Button
               variant="ghost"
-              className="w-full h-full"
+              size="sm"
+              className="size-full"
               onClick={handleClear}
               disabled={selectedCount === 0}
             >
