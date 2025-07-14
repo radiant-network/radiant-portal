7ef513abb2c6a28c6de1862bce8e7c29232be4e4
Luc-Frédéric Langis <luc-frederic.langis.hsj@ssss.gouv.qc.ca>
2025-07-14 08:28:07 -0400

    fix(aggregate): SJRA-400 update seq_id for aggregate

--- frontend/components/feature/query-filters/use-aggregation-builder.ts
+++ frontend/components/feature/query-filters/use-aggregation-builder.ts
@@ -1,12 +1,20 @@
+import React from 'react';
 import useSWR from 'swr';
-import { SqonContent, SqonOpEnum, type Aggregation, type AggregationBody } from '@/api/api';
+import { AggregationBodyWithSqon, SqonContent, SqonOpEnum, type Aggregation } from '@/api/api';
 import { occurrencesApi } from '@/utils/api';
 import { queryBuilderRemote } from '@/components/model/query-builder-core/query-builder-remote';
 
+type AggregateContextProps = {
+  seqId: string;
+}
+export const AggregateContext = React.createContext<AggregateContextProps>({ seqId: '1' });
+
+
 type OccurrenceAggregationInput = {
   seqId: string;
-  aggregationBody: AggregationBody;
+  aggregationBody: AggregationBodyWithSqon;
 };
+
 
 const fetcher = (input: OccurrenceAggregationInput): Promise<Aggregation[]> => {
   return occurrencesApi
@@ -17,11 +25,13 @@ const fetcher = (input: OccurrenceAggregationInput): Promise<Aggregation[]> => {
 export function useAggregationBuilder(field: string, size: number = 30, shouldFetch: boolean = false, appId: string) {
   let data: OccurrenceAggregationInput | null;
 
+  const { seqId } = React.useContext(AggregateContext);
+
   if (!shouldFetch) {
     data = null;
   } else {
     data = {
-      seqId: '1',
+      seqId,
       aggregationBody: {
         field: field,
         size: size,
