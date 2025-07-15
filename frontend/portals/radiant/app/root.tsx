90a1989ec91ed074d47a3859ba66ee89e0607d0c
Francis Lavoie <lav.francis@gmail.com>
2025-07-14 23:22:16 -0400

    feat: sjra-432 update favicon - suppor themes

--- frontend/portals/radiant/app/root.tsx
+++ frontend/portals/radiant/app/root.tsx
@@ -34,14 +34,24 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const loaderData = useLoaderData<{ language: string }>();
-  
   return (
     <html lang={loaderData?.language || 'en'}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={__PROJECT__.portal.name} />
+        <meta property="og:description" content="" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="application-name" content={__PROJECT__.portal.name} />
         <Meta />
         <Links />
+        <link rel="icon" type="image/x-icon" href={`/${__PROJECT__.portal.name.toLowerCase()}/favicon.ico`} />
+        <link rel="icon" type="image/png" href={`/${__PROJECT__.portal.name.toLowerCase()}/favicon-16x16.png`} sizes="16x16" />
+        <link rel="icon" type="image/png" href={`/${__PROJECT__.portal.name.toLowerCase()}/favicon-32x32.png`} sizes="32x32" />
+        <link rel="icon" type="image/png" href={`/${__PROJECT__.portal.name.toLowerCase()}/favicon-96x96.png`} sizes="96x96" />
+        <link rel="icon" type="image/png" href={`/${__PROJECT__.portal.name.toLowerCase()}/favicon-128x128.png`} sizes="128x128" />
+        <link rel="icon" type="image/png" href={`/${__PROJECT__.portal.name.toLowerCase()}/favicon-196x196.png`} sizes="196x196" />
+        <link rel="manifest" href={`/${__PROJECT__.portal.name.toLowerCase()}/site.webmanifest`} />
       </head>
       <body>
         <I18nProvider initialLanguage={loaderData?.language}>
