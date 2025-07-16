c7bac96e09adc63b9ad6eaac83a73074b75d269b
lflangis <65532894+lflangis@users.noreply.github.com>
2025-07-16 14:21:58 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@7a16dc44b3fb55bd554f522fcb4f2162f5ee1761 🚀

---
+++ docs/assets/input-search.stories-0Hw4eCEf.js
@@ -0,0 +1,20 @@
+import{j as u}from"./jsx-runtime-D_zvdyIk.js";import{f as n}from"./index-B7YJKKKT.js";import{a}from"./index-B-lxVbXh.js";import{I as t}from"./input-search-DV5eFBuk.js";import"./v4-CtRu48qb.js";import"./utils-D-KgF5mV.js";import"./button-GdlRjDSK.js";import"./index-b4Krvw3J.js";import"./index-DQLiH3RP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./ActionButton-yaQJeqfP.js";import"./dropdown-menu-5z4VmTsG.js";import"./index-DZeBqZZX.js";import"./index-CKWZTibS.js";import"./index-CS2et-gJ.js";import"./index-BlJj-Uol.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-Czj0iLzG.js";import"./index-C66Dxnp2.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./tooltip-2a7OmUZw.js";import"./i18n-D-P4Fd2b.js";import"./iframe-C32Di235.js";import"./input-DyY2UfVx.js";import"./search-DYa5HqpW.js";const J={title:"Data Entry/Inputs/Input Search",component:t,args:{value:"Search value",onSearch:n(),onChange:n(),placeholder:"Placeholder",searchButtonProps:{}}},e={render:()=>u.jsx(t,{onChange:r=>a("onChange")(r),onSearch:r=>{a("onSearch")(r)},className:"max-w-[300px]",placeholder:"Placeholder",autoFocus:!0,searchButtonProps:{color:"primary",variant:"filled"}})},o={render:()=>u.jsx(t,{onChange:r=>a("onChange")(r),onSearch:r=>(a("onSearch")(r),new Promise(h=>setTimeout(()=>h(),1e3))),className:"max-w-[300px]",placeholder:"Placeholder",autoFocus:!0,searchButtonProps:{color:"primary",variant:"filled"}})};var c,s,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    return <InputSearch onChange={e => action('onChange')(e)} onSearch={value => {
+      action('onSearch')(value);
+    }} className="max-w-[300px]" placeholder="Placeholder" autoFocus searchButtonProps={{
+      color: 'primary',
+      variant: 'filled'
+    }} />;
+  }
+}`,...(p=(s=e.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};var m,i,l;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    return <InputSearch onChange={e => action('onChange')(e)} onSearch={value => {
+      action('onSearch')(value);
+      return new Promise(resolve => setTimeout(() => resolve(), 1000));
+    }} className="max-w-[300px]" placeholder="Placeholder" autoFocus searchButtonProps={{
+      color: 'primary',
+      variant: 'filled'
+    }} />;
+  }
+}`,...(l=(i=o.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const K=["Default","Async"];export{o as Async,e as Default,K as __namedExportsOrder,J as default};
