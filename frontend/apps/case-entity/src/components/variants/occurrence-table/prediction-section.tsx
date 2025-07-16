469ce138f3dc608764546cd4412c1ffd78e53779
Celine Pelletier <cpelletier@ferlab.bio>
2025-07-16 16:43:33 -0400

    feat: SJRA-451 add exomiser counts in variant overview and expand occurrence

--- frontend/apps/case-entity/src/components/variants/occurrence-table/prediction-section.tsx
+++ frontend/apps/case-entity/src/components/variants/occurrence-table/prediction-section.tsx
@@ -1,6 +1,8 @@
 import { useI18n } from '@/components/hooks/i18n';
 import DetailSection, { DetailItem } from './detail-section';
 import { ExpandedOccurrence } from '@/api/api';
+import ClinVarBadge from '@/components/feature/variant/clinvar-badge';
+import NumberBadge from '@/components/base/number-badge';
 
 type PredictionSectionProps = {
   data: ExpandedOccurrence;
@@ -9,9 +11,19 @@ type PredictionSectionProps = {
 export default function PredictionSection({ data }: PredictionSectionProps) {
   const { t } = useI18n();
 
+  const exomiserCounts = data.exomiser_acmg_classification_counts ?? {};
+
+  const exomiser = Object.keys(exomiserCounts).map(classification => (
+    <NumberBadge count={exomiserCounts[classification]} variant="ghost">
+      <ClinVarBadge id={classification} abbreviated value={classification}>
+        {classification}
+      </ClinVarBadge>
+    </NumberBadge>
+    ));
+
   return (
     <DetailSection title={t('occurrenceExpand.predictions.title')}>
-      <DetailItem title={t('occurrenceExpand.predictions.exomiser')} value="-" />
+      <DetailItem title={t('occurrenceExpand.predictions.exomiser')} value={exomiser.length ? <div className="flex items-center gap-2">{exomiser}</div> : '-'} />
     </DetailSection>
   );
 }
