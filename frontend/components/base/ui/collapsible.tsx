f357a998c675d6a4fe338e7e981d216da3b39e68
Luc-Frédéric Langis <luc-frederic.langis.hsj@ssss.gouv.qc.ca>
2025-07-17 15:36:59 -0400

    feat(clinical): SJRA-458 add CollapsibleList component

--- frontend/components/base/ui/collapsible.tsx
+++ frontend/components/base/ui/collapsible.tsx
@@ -1,4 +1,7 @@
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+import { Button } from "./button"
+import { useState } from "react"
+import { useI18n } from "@/components/hooks/i18n"
 
 const Collapsible = CollapsiblePrimitive.Root
 
@@ -6,4 +9,39 @@ const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
 const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
 
-export { Collapsible, CollapsibleTrigger, CollapsibleContent }
+type CollapsibleListProps = {
+  list: any[];
+  render: Function;
+  limit: number;
+}
+function CollapsibleList({ list, render, limit }: CollapsibleListProps) {
+  const { t } = useI18n();
+  const [open, setOpen] = useState<boolean>(false);
+
+  if (list.length === 0) return;
+
+  return (
+    <>
+      {list.slice(0, limit).map(e => render(e))}
+      {list.length > limit && (
+        <Collapsible open={open}>
+          <CollapsibleContent className="flex flex-col gap-2">
+            {list.slice(limit).map(e => render(e))}
+          </CollapsibleContent>
+          <CollapsibleTrigger>
+            <Button
+              className="p-0 text-sm font-medium"
+              variant='link'
+              onClick={() => setOpen(!open)}
+            >
+              {!open ? t('common.view_more') : t('common.view_less')}
+            </Button>
+          </CollapsibleTrigger>
+        </Collapsible>
+      )}
+    </>
+  );
+
+}
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent, CollapsibleList }
