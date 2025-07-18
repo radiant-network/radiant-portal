d6a45432a5a74647217338a7a54dcd7fa64b9a58
francisl <lav.francis@gmail.com>
2025-07-18 19:19:50 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@a194d80494bbfb09e8e2c5aa98e17ad526a2b28b 🚀

---
+++ docs/assets/page-header.stories-C6xwe1N-.js
@@ -0,0 +1,34 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{P as o}from"./page-header-O8VSf8QZ.js";import{U as c}from"./users-BFGqzbhB.js";import{B as l}from"./chunk-QMGIS6GS-B2N4RFn-.js";import"./index-C66Dxnp2.js";import"./utils-D-KgF5mV.js";import"./skeleton-Shk8p_SP.js";import"./button-DBAEQ15X.js";import"./index-b4Krvw3J.js";import"./index-DQLiH3RP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./ActionButton-DuC4fis2.js";import"./dropdown-menu-DGmzL58B.js";import"./index-DZeBqZZX.js";import"./index-CKWZTibS.js";import"./index-CS2et-gJ.js";import"./index-BlJj-Uol.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-BQkt_1YJ.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./tooltip-2a7OmUZw.js";import"./i18n-DTzGgVww.js";import"./iframe-CebnNTpv.js";import"./badge-D_UfbDzk.js";import"./x-ClsbQ_rO.js";const C={title:"data-display/Page-Header",component:o,args:{}},e={args:{},render:()=>r.jsx(o,{})},t={args:{isLoading:!1,breadcrumbs:[{to:"#",text:"Level 1"}],title:"Title",badges:[{variant:"secondary",children:r.jsxs(r.Fragment,{children:[r.jsx(c,{}),"Icon"]})},{variant:"outline",children:r.jsx(r.Fragment,{children:"outline"})}],buttons:[{children:"primary"},{variant:"secondary",children:"Secondary"}],description:"Optional description text…"},render:d=>r.jsx(l,{children:r.jsx(o,{...d})})};var a,i,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    return <PageHeader />;
+  }
+}`,...(s=(i=e.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var n,m,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isLoading: false,
+    breadcrumbs: [{
+      to: '#',
+      text: 'Level 1'
+    }],
+    title: 'Title',
+    badges: [{
+      variant: "secondary",
+      children: <><Users />Icon</>
+    }, {
+      variant: "outline",
+      children: <>outline</>
+    }],
+    buttons: [{
+      children: 'primary'
+    }, {
+      variant: 'secondary',
+      children: 'Secondary'
+    }],
+    description: 'Optional description text…'
+  },
+  render: args => {
+    return <BrowserRouter>
+        <PageHeader {...args} />
+      </BrowserRouter>;
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const G=["Loading","Default"];export{t as Default,e as Loading,G as __namedExportsOrder,C as default};
---
+++ docs/assets/result.stories-CstTQImT.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{R as m}from"./result-1kP5sDP1.js";import{B as p}from"./button-DBAEQ15X.js";import"./utils-D-KgF5mV.js";import"./index-b4Krvw3J.js";import"./index-DQLiH3RP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./ActionButton-DuC4fis2.js";import"./dropdown-menu-DGmzL58B.js";import"./index-DZeBqZZX.js";import"./index-CKWZTibS.js";import"./index-CS2et-gJ.js";import"./index-BlJj-Uol.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./tooltip-2a7OmUZw.js";import"./i18n-DTzGgVww.js";import"./iframe-CebnNTpv.js";const O={title:"Feedback/Result",component:m,args:{status:"404",message:"Page not found",extra:t.jsx(p,{children:"Go to dashboard"})}},r={render:i=>t.jsx("div",{className:"flex justify-center p-24",children:t.jsx(m,{...i})})};var o,e,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    return <div className="flex justify-center p-24">
+        <Result {...args} />
+      </div>;
+  }
+}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const P=["Default"];export{r as Default,P as __namedExportsOrder,O as default};
