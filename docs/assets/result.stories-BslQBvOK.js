ff0cede45a43816030a492d11ea8e5a4b65fd7a9
lflangis <65532894+lflangis@users.noreply.github.com>
2025-07-21 15:13:04 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@65ecc8844a3e7f6b091e5ecf2aa5d13fb1274592 🚀

--- docs/assets/result.stories-BslQBvOK.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{R as m}from"./result-1kP5sDP1.js";import{B as p}from"./button-Dab86akK.js";import"./utils-D-KgF5mV.js";import"./index-Dw-eQTPP.js";import"./index-DQLiH3RP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./ActionButton-BqVcLudk.js";import"./dropdown-menu-Begym49V.js";import"./index-DTwDs4x6.js";import"./index-BkhUCz-k.js";import"./index-Buy_jb7k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./tooltip-CqCjg2e9.js";import"./i18n-Bu8UxWvx.js";import"./iframe-DZ8au31n.js";const P={title:"Feedback/Result",component:m,args:{status:"404",message:"Page not found",extra:t.jsx(p,{children:"Go to dashboard"})}},r={render:i=>t.jsx("div",{className:"flex justify-center p-24",children:t.jsx(m,{...i})})};var o,e,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: args => {
-    return <div className="flex justify-center p-24">
-        <Result {...args} />
-      </div>;
-  }
-}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const S=["Default"];export{r as Default,S as __namedExportsOrder,P as default};
--- docs/assets/transcript-id-link.stories-B73O2dtL.js
+++
@@ -1,5 +0,0 @@
-import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{T as p}from"./transcript-id-link-BXenuoJT.js";import"./utils-D-KgF5mV.js";import"./anchor-link-C1gFowOy.js";import"./index-C66Dxnp2.js";import"./external-link-CdxgTZcI.js";import"./createLucideIcon-BMP5cxO1.js";import"./index-DQLiH3RP.js";import"./transcript-mane-plus-icon-C9yhxXSz.js";import"./tooltip-CqCjg2e9.js";import"./index-DTwDs4x6.js";import"./index-Dw-eQTPP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./index-BkhUCz-k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./i18n-Bu8UxWvx.js";import"./iframe-DZ8au31n.js";const C={title:"Feature/Variant/Transcript ID Link",component:p,args:{transcriptId:"ENST00000367770",isCanonical:!0}},r={render:e=>a.jsx(p,{...e})};var t,o,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: args => {
-    return <TranscriptIdLink {...args} />;
-  }
-}`,...(i=(o=r.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const F=["Default"];export{r as Default,F as __namedExportsOrder,C as default};
