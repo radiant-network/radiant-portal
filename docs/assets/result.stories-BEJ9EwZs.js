f8e0ce8fa371e4ca8ee09971f2943a1a47bfe98b
francisl <lav.francis@gmail.com>
2025-07-22 14:28:01 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@bea087408f3228a852853359356f3e828315a480 🚀

---
+++ docs/assets/result.stories-BEJ9EwZs.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{R as m}from"./result-1kP5sDP1.js";import{B as p}from"./button-Ce-A9CB8.js";import"./utils-D-KgF5mV.js";import"./index-Dw-eQTPP.js";import"./index-DQLiH3RP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./ActionButton-BqVcLudk.js";import"./dropdown-menu-Begym49V.js";import"./index-DTwDs4x6.js";import"./index-BkhUCz-k.js";import"./index-Buy_jb7k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./tooltip-CqCjg2e9.js";import"./i18n-CIF0C2fG.js";import"./iframe-Vm-qE_e6.js";const P={title:"Feedback/Result",component:m,args:{status:"404",message:"Page not found",extra:t.jsx(p,{children:"Go to dashboard"})}},r={render:i=>t.jsx("div",{className:"flex justify-center p-24",children:t.jsx(m,{...i})})};var o,e,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    return <div className="flex justify-center p-24">
+        <Result {...args} />
+      </div>;
+  }
+}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const S=["Default"];export{r as Default,S as __namedExportsOrder,P as default};
