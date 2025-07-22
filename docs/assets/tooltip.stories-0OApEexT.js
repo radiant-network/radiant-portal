38e74802450eca4cf1492b875b1679b127bc8c35
lflangis <65532894+lflangis@users.noreply.github.com>
2025-07-22 18:18:01 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@09b5a1c31f5fb88995a499bfd46443f086520f89 🚀

---
+++ docs/assets/tooltip.stories-0OApEexT.js
@@ -0,0 +1,12 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{b as p,T as m,a as n}from"./tooltip-CqCjg2e9.js";import{B as a}from"./button-Cm5m-NHT.js";import"./index-DQLiH3RP.js";import"./index-DTwDs4x6.js";import"./index-Dw-eQTPP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./index-BkhUCz-k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./utils-D-KgF5mV.js";import"./ActionButton-BqVcLudk.js";import"./dropdown-menu-Begym49V.js";import"./index-Buy_jb7k.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./i18n-DuJ_wecI.js";import"./iframe-CYXaGRdh.js";import"./context-DNAkM4o1.js";const R={title:"Feedback/Tooltip",component:p,args:{children:"Tooltip Content",sideOffset:8,side:"top"}},o={render:s=>r.jsx("div",{className:"flex justify-center p-24",children:r.jsxs(m,{children:[r.jsx(n,{asChild:!0,children:r.jsx(a,{color:"primary",children:"Hover Me"})}),r.jsx(p,{...s})]})})};var t,i,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => {
+    return <div className="flex justify-center p-24">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button color="primary">Hover Me</Button>
+          </TooltipTrigger>
+          <TooltipContent {...args} />
+        </Tooltip>
+      </div>;
+  }
+}`,...(e=(i=o.parameters)==null?void 0:i.docs)==null?void 0:e.source}}};const S=["Default"];export{o as Default,S as __namedExportsOrder,R as default};
