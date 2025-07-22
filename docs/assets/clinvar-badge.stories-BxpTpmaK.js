df5a3784e70aa4d0a6019c221c85eb1e2b80e5ba
lflangis <65532894+lflangis@users.noreply.github.com>
2025-07-22 17:10:18 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@1aa99f49df7349a65c41c1b0009532e06f95d6ac 🚀

---
+++ docs/assets/clinvar-badge.stories-BxpTpmaK.js
@@ -0,0 +1,10 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{C as t,a as o}from"./clinvar-badge-CMae73I9.js";import"./conditional-wrapper-BBX8pIPQ.js";import"./badge-BEvKHJki.js";import"./utils-D-KgF5mV.js";import"./index-C66Dxnp2.js";import"./separator-PkW7KAol.js";import"./index-DQLiH3RP.js";import"./index-Dw-eQTPP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./x-ClsbQ_rO.js";import"./createLucideIcon-BMP5cxO1.js";import"./tooltip-CqCjg2e9.js";import"./index-DTwDs4x6.js";import"./index-BkhUCz-k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./i18n-CnbQ0kq8.js";import"./iframe-DFdUXpQQ.js";const D={title:"Feature/Variant/ClinVar Badge",component:t,args:{value:"other",variant:"neutral"}},r={render:()=>e.jsx("div",{className:"flex flex-col gap-2 items-start",children:Object.keys(o).map(a=>e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(t,{value:a}),e.jsx(t,{value:a,abbreviated:!0})]},a))})};var i,s,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => {
+    return <div className="flex flex-col gap-2 items-start">
+        {Object.keys(ClinVarValueMap).map(key => <div className="flex items-center gap-2" key={key}>
+            <ClinVarBadge value={key} />
+            <ClinVarBadge value={key} abbreviated />
+          </div>)}
+      </div>;
+  }
+}`,...(m=(s=r.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,D as default};
