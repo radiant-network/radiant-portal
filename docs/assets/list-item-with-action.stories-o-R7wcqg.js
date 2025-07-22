7dc0de6048a17d3e87801c9e0c69e755c921398a
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-07-22 03:54:19 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@07662a34cfc90274a7b3a990aa593de398a95966 🚀

---
+++ docs/assets/list-item-with-action.stories-o-R7wcqg.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{a as o}from"./index-B-lxVbXh.js";import{L as i,a}from"./list-item-with-action-BlxavRiU.js";import"./v4-CtRu48qb.js";import"./index-C66Dxnp2.js";import"./utils-D-KgF5mV.js";import"./conditional-wrapper-BBX8pIPQ.js";import"./button-C8zZzjHa.js";import"./index-Dw-eQTPP.js";import"./index-DQLiH3RP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./ActionButton-BqVcLudk.js";import"./dropdown-menu-Begym49V.js";import"./index-DTwDs4x6.js";import"./index-BkhUCz-k.js";import"./index-Buy_jb7k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-BQkt_1YJ.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./tooltip-CqCjg2e9.js";import"./i18n-nK4zJx9i.js";import"./iframe-szmZ-SCp.js";import"./trash-DCT5GB8X.js";const q={title:"Data Display/ListItem with action",component:i,args:{onEdit:o("onEdit"),onDelete:o("onDelete"),onShare:o("onShare"),onClick:o("onClick")}},e={args:{title:"Title",description:"Description"},render:r=>t.jsx("div",{className:"max-w-[450px]",children:t.jsxs(a,{bordered:!0,children:[t.jsx(i,{...r}),t.jsx(i,{...r}),t.jsx(i,{...r}),t.jsx(i,{...r})]})})};var s,m,p;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: 'Title',
+    description: 'Description'
+  },
+  render: args => <div className="max-w-[450px]">
+      <List bordered>
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+      </List>
+    </div>
+}`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const z=["Default"];export{e as Default,z as __namedExportsOrder,q as default};
