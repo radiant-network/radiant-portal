432e351f4b9adabbe533908fe8574efd3ee4119d
oliviercperrier <28421005+oliviercperrier@users.noreply.github.com>
2025-07-25 04:16:58 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@327498d9351b115812db12ae16469b39ed556599 🚀

--- docs/assets/rich-text-editor.stories-Cu1OaULm.js
+++
@@ -1,8 +0,0 @@
-import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{f as s}from"./index-B7YJKKKT.js";import{a as r}from"./index-B-lxVbXh.js";import{u as l}from"./i18n-aZe3Qt8M.js";import{R as m}from"./rich-text-editor-CF06UYEq.js";import"./v4-CtRu48qb.js";import"./iframe-keurxVmR.js";import"./context-DNAkM4o1.js";import"./index-DQLiH3RP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./utils-D-KgF5mV.js";import"./index-DTwDs4x6.js";import"./index-Dw-eQTPP.js";import"./index-C66Dxnp2.js";import"./separator-PkW7KAol.js";import"./dropdown-menu-Begym49V.js";import"./index-BkhUCz-k.js";import"./index-Buy_jb7k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./chevron-down-QEmn0_AS.js";const S={title:"Data Entry/RichText Editor",component:m,args:{value:"<h3>Hello</h3>",onChange:s(),placeholder:"Placeholder"}},o={render:p=>{const{t:i}=l();return n.jsx(m,{value:p.value,onChange:r("onChange"),onBlur:r("onBlur"),placeholder:i("common.editor.placeholder"),wrapperClassName:"max-w-[500px]",autoFocus:!0})}};var t,e,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: args => {
-    const {
-      t
-    } = useI18n();
-    return <RichTextEditor value={args.value} onChange={action('onChange')} onBlur={action('onBlur')} placeholder={t('common.editor.placeholder')} wrapperClassName="max-w-[500px]" autoFocus />;
-  }
-}`,...(a=(e=o.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const b=["Default"];export{o as Default,b as __namedExportsOrder,S as default};
---
+++ docs/assets/toggle-filter.stories-CMOTW7oO.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{a as d}from"./index-B-lxVbXh.js";import{q as y}from"./query-builder-remote-DQDcrGW9.js";import{T as o}from"./toggle-filter-B4t8HGkw.js";import{C as g}from"./applications-config-pyPLye2e.js";import{c as a}from"./config-mock-BXxtYAjK.js";import"./v4-CtRu48qb.js";import"./api-fYFwtGmU.js";import"./index-DQLiH3RP.js";import"./button-BnqgY2TY.js";import"./index-Dw-eQTPP.js";import"./index-CECE1b4A.js";import"./index-CJPVTaBz.js";import"./ActionButton-BqVcLudk.js";import"./dropdown-menu-Begym49V.js";import"./index-DTwDs4x6.js";import"./index-BkhUCz-k.js";import"./index-Buy_jb7k.js";import"./index-BsCFIxoT.js";import"./index-uZc0PFVk.js";import"./utils-D-KgF5mV.js";import"./check-DSCf8CVO.js";import"./createLucideIcon-BMP5cxO1.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BtlAG3ey.js";import"./spinner-DHVcj7-u.js";import"./tooltip-DogpHjiW.js";import"./i18n-ZjLIO4Tq.js";import"./iframe-C_wvF6kO.js";import"./context-DNAkM4o1.js";import"./label-E-AdJZe_.js";import"./index-DDGWSPzp.js";const u={...a,variant_exploration:{...a.variant_exploration,aggregations:{variant:{items:[{key:"chromosome",type:"multiple"},{key:"filter",type:"multiple"},{key:"zygosity",type:"multiple"},{key:"impact_score",type:"multiple"},{key:"variant_class",type:"multiple"},{key:"symbol",type:"multiple"},{key:"is_canonical",type:"boolean"}]}}}},V={title:"Feature/Query Filters/Toggle Filter",component:o,args:{field:{key:"is_canonical",type:"boolean"}},decorators:[r=>e.jsx(g,{config:u,children:e.jsx(r,{})})]},t={render:r=>e.jsx("div",{className:"space-y-6",children:e.jsx(o,{...r})})},i={render:r=>(d("activeQuery")(y.updateActiveQueryField(u.variant_exploration.app_id,{field:"is_canonical",value:["true"]})),e.jsx("div",{className:"space-y-6",children:e.jsx(o,{...r})}))};var p,s,m;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => {
+    return <div className="space-y-6">
+        <ToggleFilter {...args} />
+      </div>;
+  }
+}`,...(m=(s=t.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var l,n,c;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => {
+    action('activeQuery')(queryBuilderRemote.updateActiveQueryField(config.variant_exploration.app_id, {
+      field: 'is_canonical',
+      value: ['true']
+    }));
+    return <div className="space-y-6">
+        <ToggleFilter {...args} />
+      </div>;
+  }
+}`,...(c=(n=i.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const W=["Default","DataAppliedToQueryBuilder"];export{i as DataAppliedToQueryBuilder,t as Default,W as __namedExportsOrder,V as default};
