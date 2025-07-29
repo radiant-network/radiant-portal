c02b717dfd1750be79a3084b032e6e50065e0b57
lflangis <65532894+lflangis@users.noreply.github.com>
2025-07-29 18:34:10 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@a33e0bea4a32b8ed2954aaa61ea76e8b4369364e 🚀

---
+++ docs/assets/rating.stories-DU2L40rA.js
@@ -0,0 +1,4 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{R as s}from"./rating-DJaxevL7.js";import"./utils-D-KgF5mV.js";import"./star-DfQf5eJj.js";import"./createLucideIcon-BMP5cxO1.js";import"./index-DQLiH3RP.js";const u={title:"Data Display/Rating",component:s,args:{starSize:16,numberOfStars:4,rating:3}},r={args:{},render:o=>n.jsx(s,{...o})};var a,t,e;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {},
+  render: args => <Rating {...args} />
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
