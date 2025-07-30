e5cfdd7ea7893275d7109f4a209348c21750f189
lflangis <65532894+lflangis@users.noreply.github.com>
2025-07-30 18:38:52 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@65b0683d9f410c7e36691ab810235a8da09239dd 🚀

---
+++ docs/assets/anchor-link.stories-BL0pkjWr.js
@@ -0,0 +1,20 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{A as a}from"./anchor-link-B_NBPliZ.js";import"./index-C66Dxnp2.js";import"./createLucideIcon-BMP5cxO1.js";import"./index-DQLiH3RP.js";const l=["primary","secondary"],k=["default","xs","sm","lg"],S={title:"Navigation/AnchorLink",component:a,argTypes:{size:{options:k,control:{type:"select"}},variant:{options:l,control:{type:"select"}}},args:{external:!0,size:"default",variant:"primary",mono:!1}},n={args:{},render:r=>s.jsx(a,{...r,children:"Anchor Link"})},e={args:{variant:"secondary"},render:r=>s.jsx(a,{...r,children:"Anchor Link"})},o={args:{mono:!0},render:r=>s.jsx(a,{...r,children:"Anchor Link"})};var t,c,i;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <AnchorLink {...args}>Anchor Link</AnchorLink>;
+  }
+}`,...(i=(c=n.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var m,d,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  },
+  render: args => {
+    return <AnchorLink {...args}>Anchor Link</AnchorLink>;
+  }
+}`,...(p=(d=e.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var u,g,h;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    mono: true
+  },
+  render: args => {
+    return <AnchorLink {...args}>Anchor Link</AnchorLink>;
+  }
+}`,...(h=(g=o.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};const j=["Default","Secondary","Mono"];export{n as Default,o as Mono,e as Secondary,j as __namedExportsOrder,S as default};
