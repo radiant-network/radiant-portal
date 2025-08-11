fccaa300806d4b070e58134c84d3fbf4ec4f2c20
lflangis <65532894+lflangis@users.noreply.github.com>
2025-08-11 14:58:15 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@2bbf21b268d26e9683b1254f626113b0d5211cff 🚀

---
+++ docs/assets/multi-selector.stories-U3jNRv5v.js
@@ -0,0 +1,26 @@
+import{j as d}from"./jsx-runtime-D_zvdyIk.js";import{r as x}from"./index-CGj_12n1.js";import{f as b}from"./index-B7YJKKKT.js";import{a as l}from"./index-B-lxVbXh.js";import{M as o}from"./multi-selector-lZZoK2PC.js";import"./v4-CtRu48qb.js";import"./command-TO5HfRr6.js";import"./index-CGTI_uD1.js";import"./index-CcLUv2_A.js";import"./index-COcwYKbe.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./index-CIckazZy.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-CKNrATXZ.js";import"./utils-D-KgF5mV.js";import"./dialog-BOHJZham.js";import"./x-CubKniSv.js";import"./createLucideIcon-8Lr1oLzj.js";import"./checkbox-CZhHNwpD.js";import"./index-qxuqJ0RB.js";import"./index-A6VgBoaw.js";import"./index-C66Dxnp2.js";import"./check-DRc1RmCY.js";import"./badge-aQXnmXeU.js";import"./separator-6xmuS_PL.js";import"./useDebounce-CWd6BpJ3.js";import"./skeleton-Shk8p_SP.js";const Q={title:"Inputs/Multi Selector",component:o,args:{value:[],onChange:b(),placeholder:"Placeholder"}},v=[{label:"Nextjs",value:"Nextjs"},{label:"Vite",value:"vite"},{label:"Nuxt",value:"nuxt",disable:!0},{label:"Vue",value:"vue, disable: true",disable:!0},{label:"Remix",value:"remix"},{label:"Svelte",value:"svelte",disable:!0},{label:"Angular",value:"angular",disable:!0},{label:"Ember",value:"ember",disable:!0},{label:"React",value:"react"},{label:"Gatsby",value:"gatsby",disable:!0},{label:"Astro",value:"astro",disable:!0}],a={render:()=>{const[t,s]=x.useState(["Nextjs"]);return d.jsx(o,{value:t,onChange:e=>{s(e),l("onChange")(e)},className:"max-w-[300px]",placeholder:"Placeholder",commandProps:{className:"max-w-[300px]"},defaultOptions:v})}},r={render:()=>{const[t,s]=x.useState(["Nextjs"]);return d.jsx(o,{value:t,onChange:e=>{s(e),l("onChange")(e)},onSearch:async e=>(l("onSearch")(e),new Promise(h=>{setTimeout(()=>h(v),1e3)})),debounceDelay:300,className:"max-w-[300px]",placeholder:"Placeholder",commandProps:{className:"max-w-[300px]"}})}};var u,m,n;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => {
+    const [values, setValues] = useState<string[]>(['Nextjs']);
+    return <MultiSelector value={values} onChange={newValues => {
+      setValues(newValues);
+      action('onChange')(newValues);
+    }} className="max-w-[300px]" placeholder="Placeholder" commandProps={{
+      className: 'max-w-[300px]'
+    }} defaultOptions={defaultOptions} />;
+  }
+}`,...(n=(m=a.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var c,i,p;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    const [values, setValues] = useState<string[]>(['Nextjs']);
+    return <MultiSelector value={values} onChange={newValues => {
+      setValues(newValues);
+      action('onChange')(newValues);
+    }} onSearch={async searchValue => {
+      action('onSearch')(searchValue);
+      return new Promise(resolve => {
+        setTimeout(() => resolve(defaultOptions), 1000);
+      });
+    }} debounceDelay={300} className="max-w-[300px]" placeholder="Placeholder" commandProps={{
+      className: 'max-w-[300px]'
+    }} />;
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const U=["Default","AsyncSearch"];export{r as AsyncSearch,a as Default,U as __namedExportsOrder,Q as default};
