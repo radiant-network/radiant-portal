a760a70410c8265bf76a3629713bc5924c4a6b63
lflangis <65532894+lflangis@users.noreply.github.com>
2025-08-13 11:15:57 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@eb17d0600db96f64b8ce2bc70ddea2128e26c08b 🚀

---
+++ docs/assets/alert.stories-BJZtU258.js
@@ -0,0 +1,82 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{B as r}from"./button-DqnR_DF4.js";import{a as o}from"./alert-dialog-store-CZoSfA8g.js";import"./index-COcwYKbe.js";import"./index-CGj_12n1.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./ActionButton-3Jbj_BdW.js";import"./dropdown-menu-CdOBzT_z.js";import"./index-CcLUv2_A.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-CbbSLEvm.js";import"./index-A6VgBoaw.js";import"./index-CKNrATXZ.js";import"./index-CIckazZy.js";import"./utils-D-KgF5mV.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BM4jpslE.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-Bh6uXa7k.js";import"./i18n-CPuzaEhR.js";import"./iframe-JenTUqYO.js";import"./context-DkqwYzW-.js";const Z={title:"Alerts/Alert",component:r},t={render:()=>e.jsx(r,{onClick:()=>{o.open({type:"info",title:"Title",description:"Description",actionProps:{children:"Ok"}})},color:"primary",children:"Open Alert Dialog"})},i={render:()=>e.jsx(r,{onClick:()=>{o.open({type:"warning",title:"Title",description:"Description",actionProps:{children:"Ok"}})},color:"primary",children:"Open Alert Dialog"})},n={render:()=>e.jsx(r,{onClick:()=>{o.open({type:"error",title:"Title",description:"Description",actionProps:{children:"Ok"}})},color:"primary",children:"Open Alert Dialog"})},p={render:()=>e.jsx(r,{onClick:()=>{o.open({type:"success",title:"Title",description:"Description",actionProps:{children:"Ok"}})},color:"primary",children:"Open Alert Dialog"})},c={render:()=>e.jsx(r,{onClick:()=>{o.open({type:"success",title:"Title",description:"Description",actionProps:{children:"Save",onClick:P=>(P.preventDefault(),new Promise(T=>{setTimeout(()=>T(!0),3e3)}))}})},color:"primary",children:"Open Alert Dialog"})};var s,a,l;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => {
+    return <Button onClick={() => {
+      alertDialog.open({
+        type: 'info',
+        title: 'Title',
+        description: 'Description',
+        actionProps: {
+          children: 'Ok'
+        }
+      });
+    }} color="primary">
+        Open Alert Dialog
+      </Button>;
+  }
+}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};var m,d,u;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    return <Button onClick={() => {
+      alertDialog.open({
+        type: 'warning',
+        title: 'Title',
+        description: 'Description',
+        actionProps: {
+          children: 'Ok'
+        }
+      });
+    }} color="primary">
+        Open Alert Dialog
+      </Button>;
+  }
+}`,...(u=(d=i.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var D,g,y;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: () => {
+    return <Button onClick={() => {
+      alertDialog.open({
+        type: 'error',
+        title: 'Title',
+        description: 'Description',
+        actionProps: {
+          children: 'Ok'
+        }
+      });
+    }} color="primary">
+        Open Alert Dialog
+      </Button>;
+  }
+}`,...(y=(g=n.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var k,O,h;p.parameters={...p.parameters,docs:{...(k=p.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: () => {
+    return <Button onClick={() => {
+      alertDialog.open({
+        type: 'success',
+        title: 'Title',
+        description: 'Description',
+        actionProps: {
+          children: 'Ok'
+        }
+      });
+    }} color="primary">
+        Open Alert Dialog
+      </Button>;
+  }
+}`,...(h=(O=p.parameters)==null?void 0:O.docs)==null?void 0:h.source}}};var A,B,C;c.parameters={...c.parameters,docs:{...(A=c.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => {
+    return <Button onClick={() => {
+      alertDialog.open({
+        type: 'success',
+        title: 'Title',
+        description: 'Description',
+        actionProps: {
+          children: 'Save',
+          onClick: e => {
+            e.preventDefault();
+            return new Promise(resolve => {
+              setTimeout(() => resolve(true), 3000);
+            });
+          }
+        }
+      });
+    }} color="primary">
+        Open Alert Dialog
+      </Button>;
+  }
+}`,...(C=(B=c.parameters)==null?void 0:B.docs)==null?void 0:C.source}}};const $=["Info","Warning","Error","Success","Async"];export{c as Async,n as Error,t as Info,p as Success,i as Warning,$ as __namedExportsOrder,Z as default};
