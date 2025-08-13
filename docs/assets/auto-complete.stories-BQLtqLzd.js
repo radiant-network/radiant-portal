113dbe3b121ab76345efb8cdbb3ccec0bb64695e
lflangis <65532894+lflangis@users.noreply.github.com>
2025-08-13 11:16:43 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@52af27ac85e762b77cc0bf274b8678ef6f0dd838 🚀

---
+++ docs/assets/auto-complete.stories-BQLtqLzd.js
@@ -0,0 +1,26 @@
+import{j as S}from"./jsx-runtime-D_zvdyIk.js";import{r as s}from"./index-CGj_12n1.js";import{f as A}from"./index-B7YJKKKT.js";import{a as i}from"./index-B-lxVbXh.js";import{A as u}from"./auto-complete-U7tZ00rd.js";import"./v4-CtRu48qb.js";import"./command-CqjYPs6K.js";import"./index-CGTI_uD1.js";import"./index-CcLUv2_A.js";import"./index-COcwYKbe.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./index-CIckazZy.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-CKNrATXZ.js";import"./utils-D-KgF5mV.js";import"./dialog-DT2vT3g-.js";import"./index-C66Dxnp2.js";import"./x-CubKniSv.js";import"./createLucideIcon-8Lr1oLzj.js";import"./checkbox-CZhHNwpD.js";import"./index-qxuqJ0RB.js";import"./index-A6VgBoaw.js";import"./check-DRc1RmCY.js";import"./skeleton-Shk8p_SP.js";import"./useDebounce-CWd6BpJ3.js";const p=[{value:"next.js",label:"Next.js"},{value:"sveltekit",label:"SvelteKit"},{value:"nuxt.js",label:"Nuxt.js"},{value:"remix",label:"Remix"},{value:"astro",label:"Astro"},{value:"wordpress",label:"WordPress"},{value:"express.js",label:"Express.js"},{value:"nest.js",label:"Nest.js"}],X={title:"Inputs/Auto Complete",component:u,args:{value:"",options:p,onChange:A(),placeholder:"Placeholder"}},a={render:()=>{const[r,n]=s.useState("nest.js");return S.jsx(u,{value:r,onChange:e=>{n(e),i("onChange")(e)},className:"max-w-[300px]",placeholder:"Placeholder",options:p})}},o={render:()=>{const[r,n]=s.useState(""),[e,c]=s.useState(!1),[f,j]=s.useState([]),C=async t=>(c(!0),i("onSearch")(t),new Promise(l=>{setTimeout(()=>l(p),1e3)}).then(l=>j(l)).finally(()=>c(!1)));return S.jsx(u,{value:r,onChange:t=>{n(t),i("onChange")(t)},options:f,loading:e,onSearch:C,debounceDelay:300,className:"max-w-[300px]",placeholder:"Placeholder"})}};var m,d,h;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState<string | undefined>('nest.js');
+    return <AutoComplete value={value} onChange={value => {
+      setValue(value);
+      action('onChange')(value);
+    }} className="max-w-[300px]" placeholder="Placeholder" options={FRAMEWORKS} />;
+  }
+}`,...(h=(d=a.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};var v,g,x;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState<string | undefined>('');
+    const [loading, setLoading] = useState(false);
+    const [options, setOptions] = useState<Option[]>([]);
+    const handleSearch = async (searchValue: string) => {
+      setLoading(true);
+      action('onSearch')(searchValue);
+      return new Promise<Option[]>(resolve => {
+        setTimeout(() => resolve(FRAMEWORKS), 1000);
+      }).then(results => setOptions(results)).finally(() => setLoading(false));
+    };
+    return <AutoComplete value={value} onChange={value => {
+      setValue(value);
+      action('onChange')(value);
+    }} options={options} loading={loading} onSearch={handleSearch} debounceDelay={300} className="max-w-[300px]" placeholder="Placeholder" />;
+  }
+}`,...(x=(g=o.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const Y=["Default","Async"];export{o as Async,a as Default,Y as __namedExportsOrder,X as default};
