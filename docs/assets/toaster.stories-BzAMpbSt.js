0171ec049fc50369ae8939be03929deaa6479c32
lflangis <65532894+lflangis@users.noreply.github.com>
2025-08-14 18:31:52 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@662b559ed6c396d83b4b58bd7a7d00a97194fb23 🚀

---
+++ docs/assets/toaster.stories-BzAMpbSt.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{B as p}from"./button-dIFacd3z.js";import{T as n,t as c}from"./index-DspWimTI.js";import{u as d}from"./theme-context-VpWbNKed.js";import"./index-COcwYKbe.js";import"./index-CGj_12n1.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./ActionButton-3Jbj_BdW.js";import"./dropdown-menu-CdOBzT_z.js";import"./index-CcLUv2_A.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-CbbSLEvm.js";import"./index-A6VgBoaw.js";import"./index-CKNrATXZ.js";import"./index-CIckazZy.js";import"./utils-D-KgF5mV.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BM4jpslE.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-Bh6uXa7k.js";import"./i18n-Cxg2GhvV.js";import"./iframe-CQdjUV5p.js";import"./context-DkqwYzW-.js";const s=({...m})=>{const{theme:a}=d();return r.jsx(n,{theme:a,className:"toaster group",style:{"--normal-bg":"var(--popover)","--normal-text":"var(--popover-foreground)","--normal-border":"var(--border)"},...m})};s.__docgenInfo={description:"",methods:[],displayName:"Toaster"};const G={title:"Toasters/Toaster",component:p},o={render:()=>r.jsxs(r.Fragment,{children:[r.jsx(p,{onClick:()=>{c("Title",{description:"A description"})},color:"primary",children:"Open Toaster"}),r.jsx(s,{position:"top-right"})]})};var t,e,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => {
+    return <>
+        <Button onClick={() => {
+        toast('Title', {
+          description: 'A description'
+        });
+      }} color="primary">
+          Open Toaster
+        </Button>
+        <Toaster position="top-right" />
+      </>;
+  }
+}`,...(i=(e=o.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const H=["Default"];export{o as Default,H as __namedExportsOrder,G as default};
