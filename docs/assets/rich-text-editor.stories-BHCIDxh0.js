d51530cb1af9615d45a82f73f3bf0c284492ef07
lflangis <65532894+lflangis@users.noreply.github.com>
2025-08-20 14:43:31 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@d7ec27d10be09da853fcc1970d970b5d182494ff 🚀

--- docs/assets/rich-text-editor.stories-BHCIDxh0.js
+++
@@ -1,8 +0,0 @@
-import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{f as s}from"./index-B7YJKKKT.js";import{a as r}from"./index-B-lxVbXh.js";import{u as l}from"./i18n-D-WwHs2-.js";import{R as m}from"./rich-text-editor-BWmPEVlZ.js";import"./v4-CtRu48qb.js";import"./iframe-CmZvwq7c.js";import"./context-DkqwYzW-.js";import"./index-CGj_12n1.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./utils-D-KgF5mV.js";import"./index-CcLUv2_A.js";import"./index-COcwYKbe.js";import"./index-C66Dxnp2.js";import"./separator-6xmuS_PL.js";import"./dropdown-menu-CdOBzT_z.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-CbbSLEvm.js";import"./index-A6VgBoaw.js";import"./index-CKNrATXZ.js";import"./index-CIckazZy.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./chevron-down-BLzVWgYU.js";const q={title:"Inputs/RichText Editor",component:m,args:{value:"<h3>Hello</h3>",onChange:s(),placeholder:"Placeholder"}},o={render:p=>{const{t:i}=l();return n.jsx(m,{value:p.value,onChange:r("onChange"),onBlur:r("onBlur"),placeholder:i("common.editor.placeholder"),wrapperClassName:"max-w-[500px]",autoFocus:!0})}};var t,e,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: args => {
-    const {
-      t
-    } = useI18n();
-    return <RichTextEditor value={args.value} onChange={action('onChange')} onBlur={action('onBlur')} placeholder={t('common.editor.placeholder')} wrapperClassName="max-w-[500px]" autoFocus />;
-  }
-}`,...(a=(e=o.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const y=["Default"];export{o as Default,y as __namedExportsOrder,q as default};
