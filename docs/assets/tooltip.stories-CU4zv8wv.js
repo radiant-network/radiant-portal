4a98025164e9609b82ba14305ed9f5fc3974e746
lflangis <65532894+lflangis@users.noreply.github.com>
2025-08-22 16:06:23 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@ec4670275c2af16d47f5cf5a8b788b1c96e7546b 🚀

---
+++ docs/assets/tooltip.stories-CU4zv8wv.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{B as o}from"./button-CCGhEjBZ.js";import{b as t,T as r,a as s}from"./tooltip-Bh6uXa7k.js";import"./index-COcwYKbe.js";import"./index-CGj_12n1.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./ActionButton-3Jbj_BdW.js";import"./dropdown-menu-CdOBzT_z.js";import"./index-CcLUv2_A.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-CbbSLEvm.js";import"./index-A6VgBoaw.js";import"./index-CKNrATXZ.js";import"./index-CIckazZy.js";import"./utils-D-KgF5mV.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./button.variants-BQkt_1YJ.js";import"./index-C66Dxnp2.js";import"./ellipsis-BM4jpslE.js";import"./spinner-BMSZ66Eg.js";import"./i18n-fkG0mC_O.js";import"./iframe-7e_n6st_.js";import"./context-DkqwYzW-.js";const L={title:"Tooltips/Tooltip",component:t,args:{children:"Tooltip Content",sideOffset:8,side:"top"}},i={render:m=>e.jsxs("div",{className:"flex flex-col gap-3 justify-center p-24",children:[e.jsx("div",{className:"flex justify-center",children:e.jsxs(r,{children:[e.jsx(s,{asChild:!0,children:e.jsx(o,{color:"primary",children:"Hover Me"})}),e.jsx(t,{...m})]})}),e.jsx("div",{className:"flex justify-center",children:e.jsxs(r,{children:[e.jsx(s,{asChild:!0,children:e.jsx(o,{color:"primary",children:"Hover Me (long text)"})}),e.jsx(t,{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a dolor nisl. Praesent varius vestibulum dui vitae aliquam. Suspendisse ac commodo nisl, non congue diam. Aliquam elementum, lectus sit amet viverra pharetra, leo magna iaculis ex, ut volutpat nulla nisl quis lacus. Phasellus vestibulum sit amet justo non facilisis. Proin suscipit condimentum orci eu eleifend. In porttitor libero sed arcu semper, non porta neque imperdiet."})]})})]})};var n,l,a;i.parameters={...i.parameters,docs:{...(n=i.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => <div className="flex flex-col gap-3 justify-center p-24">
+      <div className="flex justify-center">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button color="primary">Hover Me</Button>
+          </TooltipTrigger>
+          <TooltipContent {...args} />
+        </Tooltip>
+      </div>
+      <div className="flex justify-center">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button color="primary">Hover Me (long text)</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a dolor nisl. Praesent varius vestibulum dui
+            vitae aliquam. Suspendisse ac commodo nisl, non congue diam. Aliquam elementum, lectus sit amet viverra
+            pharetra, leo magna iaculis ex, ut volutpat nulla nisl quis lacus. Phasellus vestibulum sit amet justo non
+            facilisis. Proin suscipit condimentum orci eu eleifend. In porttitor libero sed arcu semper, non porta neque
+            imperdiet.
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </div>
+}`,...(a=(l=i.parameters)==null?void 0:l.docs)==null?void 0:a.source}}};const O=["Default"];export{i as Default,O as __namedExportsOrder,L as default};
