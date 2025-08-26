90969675c9899de3391cebd5667b415777524f35
Luc-Frédéric Langis <luc-frederic.langis.hsj@ssss.gouv.qc.ca>
2025-08-26 11:50:55 -0400

    chore(husky): SJRA-638 add husky hooks"

--- frontend/components/base/data-table/data-table.tsx
+++ frontend/components/base/data-table/data-table.tsx
@@ -23,7 +23,7 @@ import {
   Table as TableType,
   useReactTable,
 } from '@tanstack/react-table';
-import { AlertCircle, ChevronDown, ChevronRight, CombineIcon, SearchIcon } from 'lucide-react';
+import { AlertCircle, ChevronDown, ChevronRight, SearchIcon } from 'lucide-react';
 
 import { SortBody, SortBodyOrderEnum } from '@/api/api';
 import TableColumnSettings from '@/components/base/data-table/data-table-column-settings';
@@ -51,19 +51,8 @@ import { useI18n } from '@/components/hooks/i18n';
 import { cn } from '@/lib/utils';
 
 import Empty from '../empty';
-import { Badge } from '../ui/badge';
 import { Button } from '../ui/button';
 import { Card } from '../ui/card';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '../ui/dropdown-menu';
-import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
 
 import DataTableGroupBy from './data-table-group-by';
 
