fcf54586e9b8b662e5a464737f82f839b7bad2fb
Luc-Frédéric Langis <luc-frederic.langis.hsj@ssss.gouv.qc.ca>
2025-08-26 10:29:37 -0400

    fix(table): SJRA-376 adjust group by icon

--- frontend/components/base/data-table/data-table-fullscreen-button.tsx
+++ frontend/components/base/data-table/data-table-fullscreen-button.tsx
@@ -1,35 +1,34 @@
-import { useI18n } from "@/components/hooks/i18n";
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/base/ui/tooltip';
-import { Button } from '@/components/base/ui/button';
-import { Maximize, Minimize } from "lucide-react";
-import { Skeleton } from "../ui/skeleton";
+import { Maximize, Minimize } from 'lucide-react';
 
+import { Button } from '@/components/base/ui/button';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/base/ui/tooltip';
+import { useI18n } from '@/components/hooks/i18n';
+
+import { Skeleton } from '../ui/skeleton';
 
 type DataTableFullscreenButtonProps = {
-	active: boolean;
-	handleClick: (value: boolean) => void;
-	loading?: boolean;
-}
+  active: boolean;
+  handleClick: (value: boolean) => void;
+  loading?: boolean;
+};
 
 function DataTableFullscreenButton({ active, loading = true, handleClick }: DataTableFullscreenButtonProps) {
-	const { t } = useI18n();
+  const { t } = useI18n();
 
-	if (loading) return <Skeleton className="w-[24px] h-[24px]" />
+  if (loading) return <Skeleton className="w-[24px] h-[24px]" />;
 
-	return (
-		<Tooltip>
-			<TooltipTrigger asChild>
-				<Button className="px-4 py-2 h-8" variant="ghost" iconOnly onClick={() => handleClick(!active)}>
-					{active ? <Minimize /> : <Maximize />}
-				</Button>
-			</TooltipTrigger>
-			<TooltipContent>
-				{active ? t('common.table.fullscreen.minimize') : t('common.table.fullscreen.maximize')}
-			</TooltipContent>
-		</Tooltip>
-	)
-
-
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button size="sm" variant="ghost" iconOnly onClick={() => handleClick(!active)}>
+          {active ? <Minimize /> : <Maximize />}
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        {active ? t('common.table.fullscreen.minimize') : t('common.table.fullscreen.maximize')}
+      </TooltipContent>
+    </Tooltip>
+  );
 }
 
 export default DataTableFullscreenButton;
---
+++ frontend/components/base/data-table/data-table-group-by.tsx
@@ -0,0 +1,97 @@
+import { GroupingState, Table } from '@tanstack/react-table';
+import { CombineIcon } from 'lucide-react';
+
+import { useI18n } from '@/components/hooks/i18n';
+
+import { Badge } from '../ui/badge';
+import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
+
+import { ColumnSettings, TableColumnDef } from './data-table';
+
+type DataTableGroupByProps = {
+  grouping: GroupingState;
+  table: Table<any>;
+  groupByColumns: TableColumnDef<any, any>[];
+  defaultColumnSettings: ColumnSettings[];
+};
+
+/**
+ *
+ * @WARNING: There is a small workaround to make tooltip works with DropdownMenu.
+ *           it must be set has a child of Button and edit his sideOffset manualy
+ *
+ * @DESCRIPTION: enable groupBy for a columns.
+ *               Accessor must have an id.
+ *               DropdownMenuItem label's is created with defaultColumnSettings
+ * @EXAMPLE:
+ *  [{
+ *   columnHelper.accessor('status', {
+ *     id: "status",
+ *     header: 'Status',
+ *     getGroupingValue: row => `Group By ${row.status}`,
+ *     enableGrouping: true,
+ *   }),
+ *  }]*
+ *
+ */
+function DataTableGroupBy({ table, grouping, groupByColumns, defaultColumnSettings }: DataTableGroupByProps) {
+  const { t } = useI18n();
+
+  const hasGroupedValue = grouping.length > 0;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="sm" iconOnly={!hasGroupedValue}>
+          <Tooltip>
+            <TooltipTrigger>
+              <div className="flex items-center gap-2">
+                <CombineIcon />
+                {hasGroupedValue && (
+                  <Badge variant="secondary" className="capitalize">
+                    {grouping[0]}
+                  </Badge>
+                )}
+              </div>
+            </TooltipTrigger>
+            <TooltipContent sideOffset={12}>{t('common.table.group_by')}</TooltipContent>
+          </Tooltip>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{t('common.table.group_by')}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {groupByColumns.map(column => (
+          <DropdownMenuCheckboxItem
+            key={column.id}
+            checked={grouping.includes(column.id)}
+            onClick={() => {
+              table.getFlatHeaders().forEach(header => {
+                if (header.id === column.id) {
+                  table.resetGrouping();
+                  if (!grouping.includes(header.id)) {
+                    table.setGrouping([header.id]);
+                  }
+                }
+              });
+            }}
+          >
+            <div className="flex w-full items-center justify-between">
+              <div>{defaultColumnSettings.find(setting => setting.id === column.id)?.label}</div>
+            </div>
+          </DropdownMenuCheckboxItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+export default DataTableGroupBy;
