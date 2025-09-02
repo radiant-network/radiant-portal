610ce654762984816f6ead3faf8fd4bfbc4e495e
lflangis <65532894+lflangis@users.noreply.github.com>
2025-09-02 19:31:52 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@22b0b76e15937b51c316b07e724e5320c78404d7 🚀

---
+++ docs/assets/toaster.stories-BCGe2vCW.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{B as p}from"./button-RFdG_Nog.js";import{T as n,t as c}from"./index-DspWimTI.js";import{u as d}from"./theme-context-VpWbNKed.js";import"./index-COcwYKbe.js";import"./index-CGj_12n1.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./action-button-RO42ldpG.js";import"./utils-D-KgF5mV.js";import"./dropdown-menu-Bb-Cj6Tn.js";import"./index-CcLUv2_A.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-ButkbYdn.js";import"./index-A6VgBoaw.js";import"./index-BOEjv1S3.js";import"./index-CIckazZy.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./button.variants-Du9eY_ux.js";import"./index-C66Dxnp2.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-eZCTYbea.js";import"./i18n-nLWFqpJp.js";import"./iframe-D9HXPutI.js";import"./context-DkqwYzW-.js";const s=({...m})=>{const{theme:a}=d();return r.jsx(n,{theme:a,className:"toaster group",style:{"--normal-bg":"var(--popover)","--normal-text":"var(--popover-foreground)","--normal-border":"var(--border)"},...m})};s.__docgenInfo={description:"",methods:[],displayName:"Toaster"};const z={title:"Toasters/Toaster",component:p},o={render:()=>r.jsxs(r.Fragment,{children:[r.jsx(p,{onClick:()=>{c("Title",{description:"A description"})},color:"primary",children:"Open Toaster"}),r.jsx(s,{position:"top-right"})]})};var t,e,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => {
+    return <>
+        <Button onClick={() => {
+        toast('Title', {
+          description: 'A description'
+        });
+      }} color="primary">
+          Open Toaster
+        </Button>
+        <Toaster position="top-right" />
+      </>;
+  }
+}`,...(i=(e=o.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const G=["Default"];export{o as Default,G as __namedExportsOrder,z as default};
