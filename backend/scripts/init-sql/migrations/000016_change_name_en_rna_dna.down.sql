57d7704f3c9309e81107621ee9a5593481664bff
Celine Pelletier <cpelletier@ferlab.bio>
2025-09-08 14:27:46 -0400

    feat: SJRA-450 rename sample_type name_en

---
+++ backend/scripts/init-sql/migrations/000016_change_name_en_rna_dna.down.sql
@@ -0,0 +1,2 @@
+UPDATE sample_type SET name_en = 'Dna' where name_en = 'DNA';
+UPDATE sample_type SET name_en = 'Rna' where name_en = 'RNA';
---
+++ backend/scripts/init-sql/migrations/000016_change_name_en_rna_dna.up.sql
@@ -0,0 +1,2 @@
+UPDATE sample_type SET name_en = 'DNA' where name_en = 'Dna';
+UPDATE sample_type SET name_en = 'RNA' where name_en = 'Rna';
