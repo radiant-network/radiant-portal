f82e6882d8cb184fdf7f0a7041772951eec7a1bf
Luc-Frédéric Langis <luc-frederic.langis.hsj@ssss.gouv.qc.ca>
2025-09-16 15:23:35 -0400

    fix(relationship): SJRA-686 keep raw relationship data

--- frontend/components/base/information/relationship-to-proband-field.tsx
+++ frontend/components/base/information/relationship-to-proband-field.tsx
@@ -1,10 +1,12 @@
-import { useI18n } from '@/components/hooks/i18n';
-
 type RelationshipToProbandFieldProps = {
   relationship: string;
 };
+/**
+ * Display RAW data
+ * Must not be translated
+ * @SEE: https://www.notion.so/ferlab/254b0fcecb3d80ada7aafa47c715b07c?v=254b0fcecb3d8106927b000cc6a71e78
+ */
 function RelationshipToProbandField({ relationship }: RelationshipToProbandFieldProps) {
-  const { t } = useI18n();
-  return <span className="capitalize">{t(`common.relationships.${relationship}`)}</span>;
+  return <span className="capitalize">{relationship}</span>;
 }
 export default RelationshipToProbandField;
