e59a965f3cb66828fb810eb56322dbe83fc778a3
lflangis <65532894+lflangis@users.noreply.github.com>
2025-09-23 16:55:45 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@78f22427016c4e44c902a9369ab233bfb9c7339d 🚀

---
+++ docs/assets/page-header.stories-Dv6OF9us.js
@@ -0,0 +1,33 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{P as n}from"./page-header-Bzp9t6Gi.js";import{U as c}from"./users-GEp5q2No.js";import{B as l}from"./chunk-PVWAREVJ-C1taxNkX.js";import"./index-C66Dxnp2.js";import"./utils-D-KgF5mV.js";import"./badge-aQXnmXeU.js";import"./separator-6xmuS_PL.js";import"./index-CGj_12n1.js";import"./index-COcwYKbe.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./x-CubKniSv.js";import"./createLucideIcon-8Lr1oLzj.js";import"./button-CZ0IdblP.js";import"./action-button-DDdwU0ca.js";import"./dropdown-menu-CFPCuvYI.js";import"./index-CcLUv2_A.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-ButkbYdn.js";import"./index-A6VgBoaw.js";import"./index-BOEjv1S3.js";import"./index-CIckazZy.js";import"./check-DRc1RmCY.js";import"./button.variants-Du9eY_ux.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-eZCTYbea.js";import"./i18n-CzVYxKjz.js";import"./iframe-BF56SmFJ.js";import"./context-DkqwYzW-.js";import"./skeleton-Shk8p_SP.js";const M={title:"Headers/Page-Header",component:n,args:{}},e={args:{},render:()=>r.jsx(n,{})},t={args:{isLoading:!1,title:"Title",badges:[{variant:"secondary",children:r.jsxs(r.Fragment,{children:[r.jsx(c,{}),"Icon"]})},{variant:"outline",children:r.jsx(r.Fragment,{children:"outline"})}],buttons:[{children:"primary"},{variant:"secondary",children:"Secondary"}],description:"Optional description text…"},render:d=>r.jsx(l,{children:r.jsx(n,{...d})})};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    return <PageHeader />;
+  }
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var s,m,p;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    isLoading: false,
+    title: 'Title',
+    badges: [{
+      variant: 'secondary',
+      children: <>
+            <Users />
+            Icon
+          </>
+    }, {
+      variant: 'outline',
+      children: <>outline</>
+    }],
+    buttons: [{
+      children: 'primary'
+    }, {
+      variant: 'secondary',
+      children: 'Secondary'
+    }],
+    description: 'Optional description text…'
+  },
+  render: args => {
+    return <BrowserRouter>
+        <PageHeader {...args} />
+      </BrowserRouter>;
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const N=["Loading","Default"];export{t as Default,e as Loading,N as __namedExportsOrder,M as default};
