421e504307f3c1e358fbbb6dc5e2dd2d40d84f1b
francisl <lav.francis@gmail.com>
2025-09-25 18:28:01 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@946b73a0cd4c14d26db6e235304d81a9ac1e4813 🚀

---
+++ docs/assets/toggle-filter.stories-VII7Sg2O.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{a as d}from"./index-B-lxVbXh.js";import{q as y}from"./query-builder-remote-CKam2jCw.js";import{T as o}from"./toggle-filter-C4DUiU9N.js";import{C as g}from"./applications-config-q4OA8PiL.js";import{c as a}from"./config-mock-BXxtYAjK.js";import"./v4-CtRu48qb.js";import"./api-BYPCWrIS.js";import"./index-CGj_12n1.js";import"./button-cpmHnTZq.js";import"./index-COcwYKbe.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./action-button-DDdwU0ca.js";import"./utils-D-KgF5mV.js";import"./dropdown-menu-CFPCuvYI.js";import"./index-CcLUv2_A.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-ButkbYdn.js";import"./index-A6VgBoaw.js";import"./index-BOEjv1S3.js";import"./index-CIckazZy.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./button.variants-Du9eY_ux.js";import"./index-C66Dxnp2.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-eZCTYbea.js";import"./i18n-A8H7F4nw.js";import"./iframe-DNfy-JQ5.js";import"./context-DkqwYzW-.js";import"./label-CnYaQ8j3.js";import"./index-qxuqJ0RB.js";const u={...a,variant_exploration:{...a.variant_exploration,aggregations:{variant:{items:[{key:"chromosome",type:"multiple"},{key:"filter",type:"multiple"},{key:"zygosity",type:"multiple"},{key:"impact_score",type:"multiple"},{key:"variant_class",type:"multiple"},{key:"symbol",type:"multiple"},{key:"is_canonical",type:"boolean"}]}}}},W={title:"QueryBuilder/Query Filters/Toggle Filter",component:o,args:{field:{key:"is_canonical",type:"boolean"}},decorators:[r=>e.jsx(g,{config:u,children:e.jsx(r,{})})]},t={render:r=>e.jsx("div",{className:"space-y-3",children:e.jsx(o,{...r})})},i={render:r=>(d("activeQuery")(y.updateActiveQueryField(u.variant_exploration.app_id,{field:"is_canonical",value:["true"]})),e.jsx("div",{className:"space-y-3",children:e.jsx(o,{...r})}))};var p,s,m;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => {
+    return <div className="space-y-3">
+        <ToggleFilter {...args} />
+      </div>;
+  }
+}`,...(m=(s=t.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var l,n,c;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => {
+    action('activeQuery')(queryBuilderRemote.updateActiveQueryField(config.variant_exploration.app_id, {
+      field: 'is_canonical',
+      value: ['true']
+    }));
+    return <div className="space-y-3">
+        <ToggleFilter {...args} />
+      </div>;
+  }
+}`,...(c=(n=i.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const X=["Default","DataAppliedToQueryBuilder"];export{i as DataAppliedToQueryBuilder,t as Default,X as __namedExportsOrder,W as default};
