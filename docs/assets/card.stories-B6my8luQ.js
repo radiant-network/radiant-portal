343f44b66904be300496bbf7ae60c8216fede442
lflangis <65532894+lflangis@users.noreply.github.com>
2025-10-10 15:49:52 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@31584d3885ce6f9fc5bd788abb67fd4bf6404279 🚀

---
+++ docs/assets/card.stories-B6my8luQ.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{B as l}from"./button-YjhY-NsT.js";import{C as m,a as p,b as d,c as n,d as x,e as u}from"./card-BdbO89L0.js";import{I as j}from"./input-CUZJ9Qyj.js";import{L as o}from"./label-CnYaQ8j3.js";import{S as f,a as h,b as C,c as v,d as t}from"./select-CvMCtLHe.js";import"./index-COcwYKbe.js";import"./index-CGj_12n1.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./action-button-BiAXKMq7.js";import"./utils-D-KgF5mV.js";import"./dropdown-menu-CFPCuvYI.js";import"./index-CcLUv2_A.js";import"./index-CphM_NEg.js";import"./Combination-Bb6GvI2f.js";import"./index-ButkbYdn.js";import"./index-A6VgBoaw.js";import"./index-BOEjv1S3.js";import"./index-CIckazZy.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./separator-6xmuS_PL.js";import"./button.variants-Du9eY_ux.js";import"./index-C66Dxnp2.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-eZCTYbea.js";import"./i18n-BWHuWJtH.js";import"./iframe-D0u8YpuK.js";import"./i18next-DOi7g2fS.js";import"./index-qxuqJ0RB.js";import"./chevron-down-BLzVWgYU.js";import"./chevron-up-BzG59QGX.js";const $={title:"Cards/Card",component:m,args:{}},a={args:{},render:()=>e.jsx("div",{className:"flex flex-col gap-6",children:["default","sm"].map(r=>e.jsxs(m,{size:r,className:"w-[350px]",children:[e.jsxs(p,{size:r,children:[e.jsxs(d,{children:["Card ",e.jsxs("span",{className:"bold uppercase",children:["(",r,")"]})]}),e.jsx(n,{children:"Deploy your new project in one-click."})]}),e.jsx(x,{size:r,children:e.jsx("form",{children:e.jsxs("div",{className:"grid w-full items-center gap-4",children:[e.jsxs("div",{className:"flex flex-col space-y-1.5",children:[e.jsx(o,{htmlFor:"name",children:"Name"}),e.jsx(j,{id:"name",placeholder:"Name of your project"})]}),e.jsxs("div",{className:"flex flex-col space-y-1.5",children:[e.jsx(o,{htmlFor:"framework",children:"Framework"}),e.jsxs(f,{children:[e.jsx(h,{id:"framework",children:e.jsx(C,{placeholder:"Select"})}),e.jsxs(v,{position:"popper",children:[e.jsx(t,{value:"next",children:"Next.js"}),e.jsx(t,{value:"sveltekit",children:"SvelteKit"}),e.jsx(t,{value:"astro",children:"Astro"}),e.jsx(t,{value:"nuxt",children:"Nuxt.js"})]})]})]})]})})}),e.jsxs(u,{size:r,children:[e.jsx(l,{variant:"outline",children:"Cancel"}),e.jsx(l,{color:"primary",children:"Deploy"})]})]},r))})};var s,i,c;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  render: () => <div className="flex flex-col gap-6">
+      {['default', 'sm'].map(size => <Card key={size} size={size} className="w-[350px]">
+          <CardHeader size={size}>
+            <CardTitle>
+              Card <span className="bold uppercase">({size})</span>
+            </CardTitle>
+            <CardDescription>Deploy your new project in one-click.</CardDescription>
+          </CardHeader>
+          <CardContent size={size}>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" placeholder="Name of your project" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="framework">Framework</Label>
+                  <Select>
+                    <SelectTrigger id="framework">
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="next">Next.js</SelectItem>
+                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                      <SelectItem value="astro">Astro</SelectItem>
+                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter size={size}>
+            <Button variant="outline">Cancel</Button>
+            <Button color="primary">Deploy</Button>
+          </CardFooter>
+        </Card>)}
+    </div>
+}`,...(c=(i=a.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const ee=["Default"];export{a as Default,ee as __namedExportsOrder,$ as default};
