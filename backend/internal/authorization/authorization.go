755d60da848b6c5ebcc5b40d4b30c95c9fa7d3c4
Karl Ritchie <ritchie.karl@gmail.com>
2025-10-20 10:56:19 -0400

    :sparkles: feat: SJRA-757 cleanup authorizer initialization

--- backend/internal/authorization/authorization.go
+++ backend/internal/authorization/authorization.go
@@ -9,13 +9,17 @@ import (
 )
 
 func InitAuthorizer(auth *utils.KeycloakAuth) (gin.HandlerFunc, error) {
-	if os.Getenv("RADIANT_AUTHORIZATION_PROVIDER") == "keycloak" {
+	provider := os.Getenv("RADIANT_AUTHORIZATION_PROVIDER")
+	switch provider {
+	case "keycloak":
 		return NewKeyCloakAuthorizer()
-	} else if os.Getenv("RADIANT_AUTHORIZATION_PROVIDER") == "openfga" {
+	case "openfga":
 		return NewOpenFGAAuthorizer(auth)
-	} else {
-		// To avoid breaking existing deployments, we default if nothing is set
-		log.Print("RADIANT_AUTHORIZATION_PROVIDER not set or unrecognized, defaulting to Keycloak")
+	case "":
+		log.Print("RADIANT_AUTHORIZATION_PROVIDER not set; defaulting to Keycloak")
+		return NewKeyCloakAuthorizer()
+	default:
+		log.Printf("Unrecognized RADIANT_AUTHORIZATION_PROVIDER=%q; defaulting to Keycloak", provider)
 		return NewKeyCloakAuthorizer()
 	}
 }
