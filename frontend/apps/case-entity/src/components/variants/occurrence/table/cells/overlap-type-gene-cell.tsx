07b7b1e847dcd7c05851d86754fc9167b67304f3
Luc-Frédéric Langis <luc-frederic.langis.hsj@ssss.gouv.qc.ca>
2025-10-24 10:14:47 -0400

    feat(cnv): SJRA-742 add missing translation for fr

--- frontend/apps/case-entity/src/components/variants/occurrence/table/cells/overlap-type-gene-cell.tsx
+++ frontend/apps/case-entity/src/components/variants/occurrence/table/cells/overlap-type-gene-cell.tsx
@@ -2,6 +2,8 @@ import EmptyCell from '@/components/base/data-table/cells/empty-cell';
 import GeneOverlapFullCNVIcon from '@/components/base/icons/gene-overlap-full-cnv-icon';
 import GeneOverlapFullGeneIcon from '@/components/base/icons/gene-overlap-full-gene-icon';
 import GeneOverlapPartialIcon from '@/components/base/icons/gene-overlap-partial-icon';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/base/ui/tooltip';
+import { useI18n } from '@/components/hooks/i18n';
 
 type OverlapTypeGeneCellProps = {
   type?: string;
@@ -13,16 +15,25 @@ type OverlapTypeGeneCellProps = {
  * type3 = full_cnv
  */
 function OverlapTypeGeneCell({ type }: OverlapTypeGeneCellProps) {
+  const { t } = useI18n();
+
   if (!type) return <EmptyCell />;
 
+  let icon = <GeneOverlapFullCNVIcon size={24} />;
+
   if (type == 'full_gene') {
-    return <GeneOverlapFullGeneIcon size={24} />;
+    icon = <GeneOverlapFullGeneIcon size={24} />;
   }
 
   if (type == 'partial') {
-    return <GeneOverlapPartialIcon size={24} />;
+    icon = <GeneOverlapPartialIcon size={24} />;
   }
 
-  return <GeneOverlapFullCNVIcon size={24} />;
+  return (
+    <Tooltip>
+      <TooltipTrigger>{icon}</TooltipTrigger>
+      <TooltipContent>{t(`case_entity.variants.filters.cnv_type.${type}`)}</TooltipContent>
+    </Tooltip>
+  );
 }
 export default OverlapTypeGeneCell;
