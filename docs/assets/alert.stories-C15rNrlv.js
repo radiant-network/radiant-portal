3ff3e6422a3328d7e348dca5347cf15f2478eb2b
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2025-11-04 15:17:46 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@40079c9d318e5786ab59e0a9868f67a812ac3de8 🚀

---
+++ docs/assets/alert.stories-C15rNrlv.js
@@ -0,0 +1,181 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as r,a as s,b as t,c as n,d as l,e as o,f as a}from"./alert--3-4QEOQ.js";import{B as i}from"./button-DS-lyaxR.js";import{C as c}from"./circle-alert-DsWOU-5F.js";import"./index-CGj_12n1.js";import"./index-C66Dxnp2.js";import"./utils-D-KgF5mV.js";import"./x-CubKniSv.js";import"./createLucideIcon-8Lr1oLzj.js";import"./index-B7CJuYpG.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./action-button-DglMD9AQ.js";import"./dropdown-menu-CxUCUeqn.js";import"./index-CcLUv2_A.js";import"./index-C8qycyLa.js";import"./index-Ch7hUksi.js";import"./Combination-DwMjbv-J.js";import"./index-DceihmLw.js";import"./index-A6VgBoaw.js";import"./index-CRLeYu_h.js";import"./index-CIckazZy.js";import"./check-DRc1RmCY.js";import"./separator-IJKoE26K.js";import"./button.variants-Du9eY_ux.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-anNhU4TT.js";import"./index-BDsjCN7N.js";import"./i18n-D6uYWWom.js";import"./iframe-yymcatOS.js";import"./i18next-DOi7g2fS.js";const ae={title:"Alerts/Alert",component:i},A={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"default",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Default Alert"}),e.jsx(l,{children:"This is a default alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"default",children:e.jsxs(t,{children:[e.jsx(n,{children:"Default Alert"}),e.jsx(l,{children:"This is a default alert message."})]})}),e.jsxs(r,{variant:"default",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is a default alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},d={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"info",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Info Alert"}),e.jsx(l,{children:"This is an informational alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"info",children:e.jsxs(t,{children:[e.jsx(n,{children:"Info Alert"}),e.jsx(l,{children:"This is an informational alert message."})]})}),e.jsxs(r,{variant:"info",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is an informational alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},h={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"warning",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Warning Alert"}),e.jsx(l,{children:"This is a warning alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"warning",children:e.jsxs(t,{children:[e.jsx(n,{children:"Warning Alert"}),e.jsx(l,{children:"This is a warning alert message."})]})}),e.jsxs(r,{variant:"warning",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is a warning alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},x={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"error",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Error Alert"}),e.jsx(l,{children:"This is an error alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"error",children:e.jsxs(t,{children:[e.jsx(n,{children:"Error Alert"}),e.jsx(l,{children:"This is an error alert message."})]})}),e.jsxs(r,{variant:"error",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is an error alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},m={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"success",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Success Alert"}),e.jsx(l,{children:"This is a success alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"success",children:e.jsxs(t,{children:[e.jsx(n,{children:"Success Alert"}),e.jsx(l,{children:"This is a success alert message."})]})}),e.jsxs(r,{variant:"success",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is a success alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})};var j,p,C;A.parameters={...A.parameters,docs:{...(j=A.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="default" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Default Alert</AlertTitle>
+          <AlertDescription>This is a default alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="default">
+        <AlertContent>
+          <AlertTitle>Default Alert</AlertTitle>
+          <AlertDescription>This is a default alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="default">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is a default alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(C=(p=A.parameters)==null?void 0:p.docs)==null?void 0:C.source}}};var u,g,f;d.parameters={...d.parameters,docs:{...(u=d.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="info" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Info Alert</AlertTitle>
+          <AlertDescription>This is an informational alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="info">
+        <AlertContent>
+          <AlertTitle>Info Alert</AlertTitle>
+          <AlertDescription>This is an informational alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="info">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is an informational alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(f=(g=d.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var v,k,T;h.parameters={...h.parameters,docs:{...(v=h.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="warning" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Warning Alert</AlertTitle>
+          <AlertDescription>This is a warning alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="warning">
+        <AlertContent>
+          <AlertTitle>Warning Alert</AlertTitle>
+          <AlertDescription>This is a warning alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="warning">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is a warning alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(T=(k=h.parameters)==null?void 0:k.docs)==null?void 0:T.source}}};var I,D,b;x.parameters={...x.parameters,docs:{...(I=x.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="error" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Error Alert</AlertTitle>
+          <AlertDescription>This is an error alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="error">
+        <AlertContent>
+          <AlertTitle>Error Alert</AlertTitle>
+          <AlertDescription>This is an error alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="error">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is an error alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(b=(D=x.parameters)==null?void 0:D.docs)==null?void 0:b.source}}};var B,z,M;m.parameters={...m.parameters,docs:{...(B=m.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="success" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Success Alert</AlertTitle>
+          <AlertDescription>This is a success alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="success">
+        <AlertContent>
+          <AlertTitle>Success Alert</AlertTitle>
+          <AlertDescription>This is a success alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="success">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is a success alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(M=(z=m.parameters)==null?void 0:z.docs)==null?void 0:M.source}}};const ce=["Default","Info","Warning","Error","Success"];export{A as Default,x as Error,d as Info,m as Success,h as Warning,ce as __namedExportsOrder,ae as default};
