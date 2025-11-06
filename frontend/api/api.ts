b517dfdb96130b9721a6580bc821103d73ce323c
Karl Ritchie <ritchie.karl@gmail.com>
2025-11-06 14:42:15 -0500

    feat: SJRA-827 Update doc and revert unwanted changes

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -127,6 +127,18 @@ export interface Assay {
      * @type {string}
      * @memberof Assay
      */
+    'diagnosis_lab_code'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof Assay
+     */
+    'diagnosis_lab_name'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof Assay
+     */
     'experiment_description'?: string;
     /**
      * 
@@ -157,18 +169,6 @@ export interface Assay {
      * @type {string}
      * @memberof Assay
      */
-    'diagnosis_lab_code'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof Assay
-     */
-    'diagnosis_lab_name'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof Assay
-     */
     'platform_code'?: string;
     /**
      * 
@@ -176,12 +176,6 @@ export interface Assay {
      * @memberof Assay
      */
     'read_length'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof Assay
-     */
-    'request_id'?: number;
     /**
      * 
      * @type {string}
@@ -408,12 +402,6 @@ export interface CaseAssay {
      * @type {number}
      * @memberof CaseAssay
      */
-    'request_id'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof CaseAssay
-     */
     'sample_id': number;
     /**
      * 
@@ -454,12 +442,6 @@ export interface CaseAssay {
 export interface CaseEntity {
     /**
      * 
-     * @type {Array<CaseAssay>}
-     * @memberof CaseEntity
-     */
-    'assays': Array<CaseAssay>;
-    /**
-     * 
      * @type {string}
      * @memberof CaseEntity
      */
@@ -470,6 +452,12 @@ export interface CaseEntity {
      * @memberof CaseEntity
      */
     'analysis_catalog_name'?: string;
+    /**
+     * 
+     * @type {Array<CaseAssay>}
+     * @memberof CaseEntity
+     */
+    'assays': Array<CaseAssay>;
     /**
      * 
      * @type {number}
@@ -490,6 +478,18 @@ export interface CaseEntity {
     'created_on': string;
     /**
      * 
+     * @type {string}
+     * @memberof CaseEntity
+     */
+    'diagnosis_lab_code'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof CaseEntity
+     */
+    'diagnosis_lab_name'?: string;
+    /**
+     * 
      * @type {Array<CasePatientClinicalInformation>}
      * @memberof CaseEntity
      */
@@ -505,13 +505,13 @@ export interface CaseEntity {
      * @type {string}
      * @memberof CaseEntity
      */
-    'diagnosis_lab_code'?: string;
+    'ordering_organization_code'?: string;
     /**
      * 
      * @type {string}
      * @memberof CaseEntity
      */
-    'diagnosis_lab_name'?: string;
+    'ordering_organization_name'?: string;
     /**
      * 
      * @type {string}
@@ -548,24 +548,6 @@ export interface CaseEntity {
      * @memberof CaseEntity
      */
     'project_name'?: string;
-    /**
-     * 
-     * @type {number}
-     * @memberof CaseEntity
-     */
-    'request_id'?: number;
-    /**
-     * 
-     * @type {string}
-     * @memberof CaseEntity
-     */
-    'ordering_organization_code'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof CaseEntity
-     */
-    'ordering_organization_name'?: string;
     /**
      * 
      * @type {string}
@@ -608,6 +590,12 @@ export interface CaseFilters {
      * @type {Array<Aggregation>}
      * @memberof CaseFilters
      */
+    'ordering_organization': Array<Aggregation>;
+    /**
+     * 
+     * @type {Array<Aggregation>}
+     * @memberof CaseFilters
+     */
     'priority': Array<Aggregation>;
     /**
      * 
@@ -615,12 +603,6 @@ export interface CaseFilters {
      * @memberof CaseFilters
      */
     'project': Array<Aggregation>;
-    /**
-     * 
-     * @type {Array<Aggregation>}
-     * @memberof CaseFilters
-     */
-    'requested_by': Array<Aggregation>;
     /**
      * 
      * @type {Array<Aggregation>}
@@ -739,10 +721,34 @@ export interface CaseResult {
     'created_on': string;
     /**
      * 
+     * @type {string}
+     * @memberof CaseResult
+     */
+    'diagnosis_lab_code'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof CaseResult
+     */
+    'diagnosis_lab_name'?: string;
+    /**
+     * 
      * @type {boolean}
      * @memberof CaseResult
      */
     'has_variants': boolean;
+    /**
+     * 
+     * @type {string}
+     * @memberof CaseResult
+     */
+    'ordering_organization_code'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof CaseResult
+     */
+    'ordering_organization_name'?: string;
     /**
      * 
      * @type {string}
@@ -755,18 +761,6 @@ export interface CaseResult {
      * @memberof CaseResult
      */
     'organization_name'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof CaseResult
-     */
-    'diagnosis_lab_code'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof CaseResult
-     */
-    'diagnosis_lab_name'?: string;
     /**
      * 
      * @type {string}
@@ -815,24 +809,6 @@ export interface CaseResult {
      * @memberof CaseResult
      */
     'project_name'?: string;
-    /**
-     * 
-     * @type {number}
-     * @memberof CaseResult
-     */
-    'request_id'?: number;
-    /**
-     * 
-     * @type {string}
-     * @memberof CaseResult
-     */
-    'ordering_organization_code'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof CaseResult
-     */
-    'ordering_organization_name'?: string;
     /**
      * 
      * @type {string}
@@ -1037,13 +1013,13 @@ export interface DocumentFilters {
      * @type {Array<Aggregation>}
      * @memberof DocumentFilters
      */
-    'format': Array<Aggregation>;
+    'diagnosis_lab'?: Array<Aggregation>;
     /**
      * 
      * @type {Array<Aggregation>}
      * @memberof DocumentFilters
      */
-    'diagnosis_lab'?: Array<Aggregation>;
+    'format': Array<Aggregation>;
     /**
      * 
      * @type {Array<Aggregation>}
@@ -1083,6 +1059,18 @@ export interface DocumentResult {
     'data_type_code': string;
     /**
      * 
+     * @type {string}
+     * @memberof DocumentResult
+     */
+    'diagnosis_lab_code'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof DocumentResult
+     */
+    'diagnosis_lab_name'?: string;
+    /**
+     * 
      * @type {number}
      * @memberof DocumentResult
      */
@@ -1111,18 +1099,6 @@ export interface DocumentResult {
      * @memberof DocumentResult
      */
     'patient_id': number;
-    /**
-     * 
-     * @type {string}
-     * @memberof DocumentResult
-     */
-    'diagnosis_lab_code'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof DocumentResult
-     */
-    'diagnosis_lab_name'?: string;
     /**
      * 
      * @type {string}
@@ -2739,12 +2715,6 @@ export interface Sequencing {
     'patient_id'?: number;
     /**
      * 
-     * @type {number}
-     * @memberof Sequencing
-     */
-    'request_id'?: number;
-    /**
-     * 
      * @type {string}
      * @memberof Sequencing
      */
@@ -3420,6 +3390,18 @@ export interface VariantInterpretedCase {
      * @type {string}
      * @memberof VariantInterpretedCase
      */
+    'diagnosis_lab_code'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof VariantInterpretedCase
+     */
+    'diagnosis_lab_name'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof VariantInterpretedCase
+     */
     'interpretation_updated_on': string;
     /**
      * 
@@ -3433,18 +3415,6 @@ export interface VariantInterpretedCase {
      * @memberof VariantInterpretedCase
      */
     'patient_id': number;
-    /**
-     * 
-     * @type {string}
-     * @memberof VariantInterpretedCase
-     */
-    'diagnosis_lab_code'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof VariantInterpretedCase
-     */
-    'diagnosis_lab_name'?: string;
     /**
      * 
      * @type {string}
@@ -3785,6 +3755,18 @@ export interface VariantUninterpretedCase {
      * @type {string}
      * @memberof VariantUninterpretedCase
      */
+    'diagnosis_lab_code'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof VariantUninterpretedCase
+     */
+    'diagnosis_lab_name'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof VariantUninterpretedCase
+     */
     'exomiser_acmg_classification'?: string;
     /**
      * 
@@ -3804,18 +3786,6 @@ export interface VariantUninterpretedCase {
      * @memberof VariantUninterpretedCase
      */
     'patient_id': number;
-    /**
-     * 
-     * @type {string}
-     * @memberof VariantUninterpretedCase
-     */
-    'diagnosis_lab_code'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof VariantUninterpretedCase
-     */
-    'diagnosis_lab_name'?: string;
     /**
      * 
      * @type {string}
@@ -6909,7 +6879,7 @@ export const SavedFiltersApiAxiosParamCreator = function (configuration?: Config
         /**
          * Get user saved filters
          * @summary Get user saved filters
-         * @param {string} [type] Saved Filter Type
+         * @param {string} [type] Saved Filter CaseCategoryCode
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
@@ -7068,7 +7038,7 @@ export const SavedFiltersApiFp = function(configuration?: Configuration) {
         /**
          * Get user saved filters
          * @summary Get user saved filters
-         * @param {string} [type] Saved Filter Type
+         * @param {string} [type] Saved Filter CaseCategoryCode
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
@@ -7138,7 +7108,7 @@ export const SavedFiltersApiFactory = function (configuration?: Configuration, b
         /**
          * Get user saved filters
          * @summary Get user saved filters
-         * @param {string} [type] Saved Filter Type
+         * @param {string} [type] Saved Filter CaseCategoryCode
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
@@ -7203,7 +7173,7 @@ export class SavedFiltersApi extends BaseAPI {
     /**
      * Get user saved filters
      * @summary Get user saved filters
-     * @param {string} [type] Saved Filter Type
+     * @param {string} [type] Saved Filter CaseCategoryCode
      * @param {*} [options] Override http request option.
      * @throws {RequiredError}
      * @memberof SavedFiltersApi
