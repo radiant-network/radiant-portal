2597638c3d24ac69bf24f50894eeca49f71e20df
Celine Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2025-11-07 19:50:11 -0500

    feat: SJRA-828 sql scripts to update model and test datas

---
+++ backend/scripts/init-sql/migrations/000025_upgrade_sequencing_task_model_v2.up.sql
@@ -0,0 +1,122 @@
+---- Sequencing experiment ----
+CREATE TABLE IF NOT EXISTS "sequencing_read_technology"
+(
+    "code"    TEXT PRIMARY KEY,
+    "name_en" TEXT NOT NULL
+);
+INSERT INTO sequencing_read_technology(code, name_en)
+VALUES ('short_read', 'Short Read'),
+       ('long_read', 'Long Read');
+
+ALTER TABLE sequencing_experiment
+    ADD COLUMN experimental_strategy_code TEXT REFERENCES "experimental_strategy" ("code");
+ALTER TABLE sequencing_experiment
+    ADD COLUMN sequencing_read_technology_code TEXT REFERENCES "sequencing_read_technology" ("code");
+ALTER TABLE sequencing_experiment
+    ADD COLUMN platform_code TEXT REFERENCES "platform" ("code");
+ALTER TABLE sequencing_experiment
+    RENAME COLUMN "performer_lab_id" TO "sequencing_lab_id";
+
+INSERT INTO experimental_strategy(code, name_en)
+VALUES ('rnaseq', 'RNA-Seq'),
+       ('targeted_dna', 'Targeted DNA Sequencing');
+
+DELETE FROM experimental_strategy WHERE code = 'wts';
+
+UPDATE sequencing_experiment
+SET experimental_strategy_code = (SELECT experimental_strategy_code
+                                  FROM experiment
+                                  WHERE sequencing_experiment.experiment_id = experiment.id)
+WHERE experimental_strategy_code IS NULL;
+UPDATE sequencing_experiment
+SET platform_code = (SELECT platform_code FROM experiment WHERE sequencing_experiment.experiment_id = experiment.id)
+where platform_code IS NULL;
+UPDATE sequencing_experiment
+SET sequencing_read_technology_code = 'short_read'
+WHERE sequencing_read_technology_code IS NULL;
+
+ALTER TABLE sequencing_experiment
+    ALTER COLUMN experimental_strategy_code set NOT NULL;
+ALTER TABLE sequencing_experiment
+    ALTER COLUMN platform_code set NOT NULL;
+ALTER TABLE sequencing_experiment
+    ALTER COLUMN sequencing_read_technology_code set NOT NULL;
+
+ALTER TABLE sequencing_experiment
+    DROP COLUMN patient_id;
+ALTER TABLE sequencing_experiment
+    DROP COLUMN experiment_id;
+ALTER TABLE sequencing_experiment
+    DROP COLUMN request_id;
+ALTER TABLE sequencing_experiment
+    DROP COLUMN is_paired_end;
+ALTER TABLE sequencing_experiment
+    DROP COLUMN read_length;
+
+-- Drop experiment table
+DROP TABLE experiment;
+
+---- Case has sequencing experiment ----
+CREATE TABLE IF NOT EXISTS "case_has_sequencing_experiment"
+(
+    "case_id"                  INTEGER NOT NULL REFERENCES "cases" ("id"),
+    "sequencing_experiment_id" INTEGER NOT NULL REFERENCES "sequencing_experiment" ("id")
+);
+
+INSERT INTO case_has_sequencing_experiment (case_id, sequencing_experiment_id)
+SELECT case_id, id
+FROM sequencing_experiment;
+
+ALTER TABLE sequencing_experiment
+    DROP COLUMN case_id;
+
+---- Task context ----
+CREATE TABLE IF NOT EXISTS "task_context"
+(
+    "task_id"                  INTEGER NOT NULL REFERENCES "task" ("id"),
+    "case_id"                  INTEGER,
+    "sequencing_experiment_id" INTEGER NOT NULL,
+    FOREIGN KEY (case_id, sequencing_experiment_id)
+        REFERENCES case_has_sequencing_experiment (case_id, sequencing_experiment_id)
+);
+
+---- Task ----
+
+-- Drop existing join tables
+DROP TABLE task_has_related_task;
+DROP TABLE task_has_sequencing_experiment;
+
+-- Empty existing task_has_document and task
+TRUNCATE TABLE task_has_document;
+TRUNCATE TABLE task;
+
+-- Update task table
+ALTER TABLE task
+    ADD COLUMN pipeline_name TEXT NOT NULL;
+ALTER TABLE task
+    ADD COLUMN pipeline_version TEXT NOT NULL;
+ALTER TABLE task
+    ADD COLUMN genome_build TEXT;
+ALTER TABLE task
+    RENAME COLUMN "type_code" TO "task_type_code";
+ALTER TABLE task
+    DROP COLUMN pipeline_id;
+
+-- Update task_has_document
+CREATE TYPE task_has_document_type AS ENUM ('input', 'output');
+ALTER TABLE task_has_document
+    ADD COLUMN type task_has_document_type NOT NULL;
+
+-- Drop pipeline table
+DROP TABLE pipeline;
+
+-- Drop request table
+DROP TABLE request;
+
+-- Update task_type content
+TRUNCATE TABLE task_type;
+INSERT INTO task_type(code, name_en)
+VALUES ('alignment', 'Alignment'),
+       ('variant_calling', 'Variant Calling'),
+       ('exomiser', 'Exomiser'),
+       ('genotyping', 'Genotyping');
--- backend/test/data/clinical/01_pipeline.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "pipeline" (id, description, genome_build)
-VALUES (1, 'Dragen', 'GRch38'),
-       (2, 'Nexflow Variant Annotation', 'GRch38')
-ON CONFLICT (id) DO NOTHING;
