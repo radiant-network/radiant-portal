f1f5ad82e9b6c25bc645904f6c5fbba514099639
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2025-11-12 15:26:00 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@2e74351802922b7c07be55faa6874587edc443d7 🚀

---
+++ docs/assets/toggle-button-group.stories-Bdx4qEon.js
@@ -0,0 +1,15 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{T as m}from"./toggle-button-group-DJWwoX6d.js";import{C as s,A as r}from"./applications-config-DwuB3Ut-.js";import{b as u}from"./utils-DT0TDhH2.js";import{B as l}from"./chunk-PVWAREVJ-CX-3hWew.js";import"./index-CGj_12n1.js";import"./button-DbO26Beu.js";import"./index-B7CJuYpG.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./action-button-DglMD9AQ.js";import"./utils-D-KgF5mV.js";import"./dropdown-menu-CxUCUeqn.js";import"./index-CcLUv2_A.js";import"./index-C8qycyLa.js";import"./index-Ch7hUksi.js";import"./Combination-DwMjbv-J.js";import"./index-DceihmLw.js";import"./index-A6VgBoaw.js";import"./index-CRLeYu_h.js";import"./index-CIckazZy.js";import"./check-DRc1RmCY.js";import"./createLucideIcon-8Lr1oLzj.js";import"./separator-IJKoE26K.js";import"./button.variants-Du9eY_ux.js";import"./index-C66Dxnp2.js";import"./spinner-BMSZ66Eg.js";import"./tooltip-anNhU4TT.js";import"./index-BDsjCN7N.js";import"./i18n-CkcvQOyN.js";import"./iframe-DyYStTlT.js";import"./i18next-DOi7g2fS.js";import"./index-CGrAONsN.js";const c={variant_entity:{app_id:r.variant_entity},snv_occurrence:{app_id:r.snv_occurrence,aggregations:[]},cnv_occurrence:{app_id:r.cnv_occurrence,aggregations:[]},admin:{admin_code:"admin",app_id:r.admin},portal:{name:"",navigation:{}}},M={title:"Buttons/ToggleButtonGroup",component:m,args:{},decorators:[o=>t.jsx(l,{children:t.jsx(s,{config:c,children:t.jsx(o,{})})})]},e={args:{defaultValue:"Button 1",onValueChange:o=>{}},render:o=>t.jsx("div",{className:"flex flex-col gap-2",children:u.map(a=>t.jsx(m,{size:a,...o,items:[{label:"Button 1",value:"Button 1"},{label:"Button 2",value:"Button 2"}]},a))})};var i,n,p;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'Button 1',
+    onValueChange: (_value: String) => {}
+  },
+  render: args => <div className="flex flex-col gap-2">
+      {buttonSizes.map(size => <ToggleButtonGroup key={size} size={size} {...args} items={[{
+      label: 'Button 1',
+      value: 'Button 1'
+    }, {
+      label: 'Button 2',
+      value: 'Button 2'
+    }]} />)}
+    </div>
+}`,...(p=(n=e.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const Q=["Default"];export{e as Default,Q as __namedExportsOrder,M as default};
