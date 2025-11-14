e112b9b378ac51962b6f3a91e8e1a312ebdd08fb
Charles Morin <charles.mor@outlook.fr>
2025-11-14 16:22:13 -0500

    feat: sjra-849 fixed date of birth validation and tests (#804)

--- backend/internal/types/patient_batch.go
+++ backend/internal/types/patient_batch.go
@@ -14,26 +14,26 @@ type DateOfBirthType struct {
 
 func (d *DateOfBirthType) UnmarshalJSON(b []byte) (err error) {
 	s := strings.Trim(string(b), `"`)
-	if s == "null" {
-		return
+	if s == "null" || s == "" {
+		return fmt.Errorf("error parsing date of birth")
 	}
 	d.Time, err = time.Parse(DateOfBirthFormat, s)
 	if err != nil {
-		return fmt.Errorf("error parsing date of birth: %w", err)
+		return fmt.Errorf("invalid date format for date of birth, expected YYYY-MM-DD")
 	}
 	return
 }
 
 type PatientBatch struct {
-	OrganizationPatientId     string          `json:"organization_patient_id" binding:"required"`
-	OrganizationPatientIdType string          `json:"organization_patient_id_type" binding:"required"`
-	OrganizationCode          string          `json:"organization_code" binding:"required"`
-	LifeStatusCode            string          `json:"life_status_code" binding:"required,oneof=alive deceased unknown"`
-	FirstName                 string          `json:"first_name,omitempty"`
-	LastName                  string          `json:"last_name,omitempty"`
-	SexCode                   string          `json:"sex_code" binding:"required,oneof=male female unknown"`
-	DateOfBirth               DateOfBirthType `json:"date_of_birth" binding:"required"`
-	Jhn                       string          `json:"jhn,omitempty"`
+	OrganizationPatientId     string           `json:"organization_patient_id" binding:"required"`
+	OrganizationPatientIdType string           `json:"organization_patient_id_type" binding:"required"`
+	OrganizationCode          string           `json:"organization_code" binding:"required"`
+	LifeStatusCode            string           `json:"life_status_code" binding:"required,oneof=alive deceased unknown"`
+	FirstName                 string           `json:"first_name,omitempty"`
+	LastName                  string           `json:"last_name,omitempty"`
+	SexCode                   string           `json:"sex_code" binding:"required,oneof=male female unknown"`
+	DateOfBirth               *DateOfBirthType `json:"date_of_birth" binding:"required"`
+	Jhn                       string           `json:"jhn,omitempty"`
 }
 
 // CreatePatientBatchBody represents the body required to create a patient batch
