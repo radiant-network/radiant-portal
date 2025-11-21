03265f51fdff369cd9fb46f37d922b848e83abf7
jecos <jcostanza@ferlab.bio>
2025-11-21 11:37:33 -0500

    :stethoscope: feat(case-creation): SJRA-833 add a probe for healthcheck

--- backend/cmd/worker/main.go
+++ backend/cmd/worker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
@@ -30,6 +31,9 @@ func main() {
 	repoBatch := repository.NewBatchRepository(dbPostgres)
 	repoPatient := repository.NewPatientsRepository(dbPostgres)
 	repoOrganization := repository.NewOrganizationRepository(dbPostgres)
+
+	StartHealthProbe(dbPostgres)
+
 	glog.Info("Worker started...")
 	for {
 		processBatch(dbPostgres, repoBatch, repoOrganization, repoPatient)
@@ -60,4 +64,39 @@ func processBatch(db *gorm.DB, repoBatch *repository.BatchRepository, repoOrgani
 			processUnexpectedError(nextBatch, notSupportedBatch, repoBatch)
 		}
 	}
+}
+
+func StartHealthProbe(db *gorm.DB) {
+	port := getEnvOrDefault("PROBE_PORT", "9999")
+	if _, err := strconv.Atoi(port); err != nil {
+		glog.Fatalf("Probe port defined in env var PROBE_PORT (%v) must be an integer ", port)
+	}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		sqlDb, err := db.DB()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if err = sqlDb.Ping(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	go func() {
+		glog.Infof("Starting health probe on :%s", port)
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			glog.Fatalf("Failed to start health probe: %v", err)
+		}
+	}()
 }
