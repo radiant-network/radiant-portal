eaddfec684cd017a1f5b498c7afab1c9afa4d6c8
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2025-11-21 16:40:26 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@1a50e425c7c82e86a04c18bba4aa778e74baf7fd 🚀

---
+++ docs/assets/data-table.stories-B-5o17HA.js
@@ -0,0 +1,205 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{a as te}from"./api-Dm3M0Tve.js";import{F as ne}from"./case-exploration-table-filters-BLzSlP7p.js";import{h as oe,i as le,D as s}from"./data-table-B8mlN6X5.js";import{C as ie,A as t}from"./applications-config-DwuB3Ut-.js";import{d as n}from"./table-mock-DUtY5NyB.js";import{B as ce}from"./chunk-PVWAREVJ-BoR99YBq.js";import"./index-CGj_12n1.js";import"./api-Cjk_w81c.js";import"./index-DxlLpgFR.js";import"./data-table-filters-DozZgOAy.js";import"./filter-button-DcjOfpns.js";import"./badge-259Tc8LK.js";import"./utils-D-KgF5mV.js";import"./index-C66Dxnp2.js";import"./separator-IJKoE26K.js";import"./index-B7CJuYpG.js";import"./index-D8dqFcAi.js";import"./index-BBPXtLXU.js";import"./x-CubKniSv.js";import"./createLucideIcon-8Lr1oLzj.js";import"./command-10fy-0UJ.js";import"./index-CosFuvvC.js";import"./index-CcLUv2_A.js";import"./index-CIckazZy.js";import"./index-Ch7hUksi.js";import"./Combination-DwMjbv-J.js";import"./index-CRLeYu_h.js";import"./checkbox-CuLJw5hI.js";import"./index-qxuqJ0RB.js";import"./index-A6VgBoaw.js";import"./check-DRc1RmCY.js";import"./dialog-DwQtXBAc.js";import"./popover-Dw4ngTBa.js";import"./index-DceihmLw.js";import"./tooltip-anNhU4TT.js";import"./index-BDsjCN7N.js";import"./i18n-yWDnI4iT.js";import"./iframe-CTN1xQk4.js";import"./i18next-DOi7g2fS.js";import"./button-gh134uFB.js";import"./action-button-DglMD9AQ.js";import"./dropdown-menu-CxUCUeqn.js";import"./index-C8qycyLa.js";import"./button.variants-Du9eY_ux.js";import"./spinner-BMSZ66Eg.js";import"./search-DqA1hdnz.js";import"./skeleton-Shk8p_SP.js";import"./user-CZ8KFWeE.js";import"./priority-indicator-D65wP9Mz.js";import"./indicator-BmYDS5D0.js";import"./shape-triangle-up-icon-CLEWE8Su.js";import"./refresh-ccw-Bwg-o3xn.js";import"./pen-BVjgPDnY.js";import"./settings-DYVxWNf4.js";import"./number-format-DxX1Gy7s.js";import"./pagination-ywIOo9oc.js";import"./select-WOQZTtP8.js";import"./chevron-down-BLzVWgYU.js";import"./chevron-up-BzG59QGX.js";import"./ellipsis-BM4jpslE.js";import"./empty-BYYXI_d-.js";import"./chart-scatter-5c2GQdAf.js";import"./card-CVaWHAcA.js";import"./circle-alert-DsWOU-5F.js";import"./chevron-right-CKDh57Sc.js";const a=le(),pe={variant_entity:{app_id:t.variant_entity},snv_occurrence:{app_id:t.snv_occurrence,aggregations:[]},cnv_occurrence:{app_id:t.cnv_occurrence,aggregations:[]},admin:{admin_code:"admin",app_id:t.admin},portal:{name:"",navigation:{}}},Vr={title:"Tables/Data Table",component:s,args:{id:"storybook",columns:[a.accessor("firstName",{cell:e=>e.getValue(),header:()=>r.jsx("span",{children:"First Name"})}),a.accessor(e=>e.lastName,{id:"lastName",cell:e=>r.jsx("i",{children:e.getValue()}),header:()=>r.jsx("span",{children:"Last Name"})}),a.accessor("age",{header:()=>"Age",cell:e=>e.renderValue()}),a.accessor("visits",{header:()=>r.jsx("span",{children:"Visits"})}),a.accessor("status",{header:"Status"}),a.accessor("progress",{header:"Profile Progress"})],data:n,serverOptions:{defaultSorting:[{field:"pf_wgs",order:te.Asc}],onSortingChange:e=>{}},defaultColumnSettings:oe([{id:"firstName",visible:!0,label:"First Name"},{id:"lastName",visible:!0,label:"Last Name"},{id:"age",visible:!0,label:"Age"},{id:"visits",visible:!0,label:"firstName"},{id:"status",visible:!0,label:"Status"},{id:"progress",visible:!0,label:"Profile Progress"}]),loadingStates:{total:!1,list:!1},pagination:{type:"server",state:{pageIndex:0,pageSize:10},onPaginationChange:()=>{}},total:10},decorators:[e=>r.jsx(ce,{children:r.jsx(ie,{config:pe,children:r.jsx(e,{})})})]},o={args:{loadingStates:{list:!0,total:!0}},render:e=>r.jsx(s,{...e})},l={args:{loadingStates:{list:!1,total:!1},data:[]},render:e=>r.jsx(s,{...e})},i={args:{loadingStates:{list:!1,total:!1},data:[],hasError:!0},render:e=>r.jsx(s,{...e})},c={args:{data:n.slice(0,10),enableFullscreen:!0,enableColumnOrdering:!0},render:e=>r.jsx(s,{...e})},p={args:{loadingStates:{list:!1,total:!1},data:n,enableColumnOrdering:!1,enableFullscreen:!0,tableIndexResultPosition:"hidden",TableFilters:()=>r.jsx(ne,{loading:!1,setSearchCriteria:()=>{}})},render:e=>r.jsx(s,{...e})},d={args:{enableFullscreen:!0},render:e=>r.jsxs("div",{children:[r.jsx("span",{children:r.jsx("i",{children:'Use "Open in new canvas" button at the top right of the screen a better preview'})}),r.jsx(s,{...e})]})},u={args:{data:n.slice(0,1),total:1,enableFullscreen:!0,enableColumnOrdering:!0},render:e=>r.jsx(s,{...e})},m={args:{data:n.slice(0,1),total:1,TableFilters:()=>r.jsx(ne,{loading:!1,setSearchCriteria:()=>{}}),enableFullscreen:!0,enableColumnOrdering:!0,tableIndexResultPosition:"bottom"},render:e=>r.jsx(s,{...e})},g={args:{pagination:{type:"hidden"}},render:e=>r.jsx(s,{...e})},h={args:{pagination:{type:"locale",state:{pageIndex:0,pageSize:5}}},render:e=>r.jsx(s,{...e})},f={args:{data:n.slice(0,10),enableFullscreen:!0,enableColumnOrdering:!0,columns:[a.accessor("firstName",{id:"firstName",cell:e=>e.getValue(),header:()=>r.jsx("span",{children:"First Name"})}),a.accessor(e=>e.lastName,{id:"lastName",cell:e=>r.jsx("i",{children:e.getValue()}),header:()=>r.jsx("span",{children:"Last Name"})}),a.accessor("age",{id:"age",header:()=>"Age",cell:e=>e.renderValue(),aggregatedCell:({getValue:e})=>r.jsx(r.Fragment,{children:`[${e()[0]}-${e()[1]}]`}),aggregationFn:"extent",enableGrouping:!0}),a.accessor("visits",{id:"visits",header:()=>r.jsx("span",{children:"Visits"}),aggregationFn:"sum"}),a.accessor("status",{id:"status",header:"Status",getGroupingValue:e=>`Group By ${e.status}`,enableGrouping:!0}),a.accessor("progress",{id:"progress",header:"Profile Progress",cell:({getValue:e})=>`${Math.round(e()*100)/100}%`,aggregationFn:"mean",aggregatedCell:({getValue:e})=>`~${Math.round(e()*100)/100}%`})]},render:e=>r.jsxs(r.Fragment,{children:[r.jsxs("span",{children:["You can group by ",r.jsx("b",{children:"status"})]}),r.jsx(s,{...e})]})},b={args:{data:n.slice(0,10),enableFullscreen:!0,enableColumnOrdering:!0,columns:[a.group({id:"group_1",header:()=>r.jsx("span",{children:"Group 1"}),columns:[a.accessor("firstName",{cell:e=>e.getValue(),footer:e=>e.column.id}),a.accessor(e=>e.lastName,{id:"lastName",cell:e=>e.getValue(),header:()=>r.jsx("span",{children:"Last Name"}),footer:e=>e.column.id})]}),a.group({header:"group_2",columns:[a.accessor("age",{header:()=>"Age",footer:e=>e.column.id}),a.group({header:"More Info",columns:[a.accessor("visits",{header:()=>r.jsx("span",{children:"Visits"})}),a.accessor("status",{header:"Status"}),a.accessor("progress",{header:"Profile Progress"})]})]})]},render:e=>r.jsx(s,{...e})},x={args:{columns:[a.accessor("firstName",{cell:e=>e.getValue(),header:()=>r.jsx("span",{children:"First Name"}),footer:()=>r.jsx("span",{children:"First Name"})}),a.accessor(e=>e.lastName,{id:"lastName",cell:e=>r.jsx("i",{children:e.getValue()}),header:()=>r.jsx("span",{children:"Last Name"}),footer:()=>r.jsx("span",{children:"Last Name"})}),a.accessor("age",{header:()=>"Age",cell:e=>e.renderValue(),footer:()=>r.jsx("span",{children:"Age"})}),a.accessor("visits",{header:()=>r.jsx("span",{children:"Visits"}),footer:()=>r.jsx("span",{children:"First Name"})}),a.accessor("status",{header:"Status",footer:()=>r.jsx("span",{children:"Status"})}),a.accessor("progress",{header:"Profile Progress",footer:()=>r.jsx("span",{children:"Profile Progress"})})]},render:e=>r.jsx(s,{...e})};var F,j,N;o.parameters={...o.parameters,docs:{...(F=o.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    loadingStates: {
+      list: true,
+      total: true
+    }
+  },
+  render: args => <DataTable {...args} />
+}`,...(N=(j=o.parameters)==null?void 0:j.docs)==null?void 0:N.source}}};var S,V,v;l.parameters={...l.parameters,docs:{...(S=l.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    loadingStates: {
+      list: false,
+      total: false
+    },
+    data: []
+  },
+  render: args => <DataTable {...args} />
+}`,...(v=(V=l.parameters)==null?void 0:V.docs)==null?void 0:v.source}}};var T,P,C;i.parameters={...i.parameters,docs:{...(T=i.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    loadingStates: {
+      list: false,
+      total: false
+    },
+    data: [],
+    hasError: true
+  },
+  render: args => <DataTable {...args} />
+}`,...(C=(P=i.parameters)==null?void 0:P.docs)==null?void 0:C.source}}};var H,D,y;c.parameters={...c.parameters,docs:{...(H=c.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 10),
+    enableFullscreen: true,
+    enableColumnOrdering: true
+  },
+  render: args => <DataTable {...args} />
+}`,...(y=(D=c.parameters)==null?void 0:D.docs)==null?void 0:y.source}}};var L,_,O;p.parameters={...p.parameters,docs:{...(L=p.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    loadingStates: {
+      list: false,
+      total: false
+    },
+    data,
+    enableColumnOrdering: false,
+    enableFullscreen: true,
+    tableIndexResultPosition: 'hidden',
+    TableFilters: () => <TableFilters loading={false} setSearchCriteria={() => {}} />
+  },
+  render: args => <DataTable {...args} />
+}`,...(O=(_=p.parameters)==null?void 0:_.docs)==null?void 0:O.source}}};var A,G,w;d.parameters={...d.parameters,docs:{...(A=d.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    enableFullscreen: true
+  },
+  render: args => <div>
+      <span>
+        <i>Use &quot;Open in new canvas&quot; button at the top right of the screen a better preview</i>
+      </span>
+      <DataTable {...args} />
+    </div>
+}`,...(w=(G=d.parameters)==null?void 0:G.docs)==null?void 0:w.source}}};var I,R,$;u.parameters={...u.parameters,docs:{...(I=u.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 1),
+    total: 1,
+    enableFullscreen: true,
+    enableColumnOrdering: true
+  },
+  render: args => <DataTable {...args} />
+}`,...($=(R=u.parameters)==null?void 0:R.docs)==null?void 0:$.source}}};var E,M,B;m.parameters={...m.parameters,docs:{...(E=m.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 1),
+    total: 1,
+    TableFilters: () => <TableFilters loading={false} setSearchCriteria={() => {}} />,
+    enableFullscreen: true,
+    enableColumnOrdering: true,
+    tableIndexResultPosition: 'bottom'
+  },
+  render: args => <DataTable {...args} />
+}`,...(B=(M=m.parameters)==null?void 0:M.docs)==null?void 0:B.source}}};var k,z,q;g.parameters={...g.parameters,docs:{...(k=g.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    pagination: {
+      type: 'hidden'
+    }
+  },
+  render: args => <DataTable {...args} />
+}`,...(q=(z=g.parameters)==null?void 0:z.docs)==null?void 0:q.source}}};var U,Y,J;h.parameters={...h.parameters,docs:{...(U=h.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    pagination: {
+      type: 'locale',
+      state: {
+        pageIndex: 0,
+        pageSize: 5
+      }
+    }
+  },
+  render: args => <DataTable {...args} />
+}`,...(J=(Y=h.parameters)==null?void 0:Y.docs)==null?void 0:J.source}}};var K,Q,W;f.parameters={...f.parameters,docs:{...(K=f.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 10),
+    enableFullscreen: true,
+    enableColumnOrdering: true,
+    columns: [columnHelper.accessor('firstName', {
+      id: 'firstName',
+      cell: info => info.getValue(),
+      header: () => <span>First Name</span>
+    }), columnHelper.accessor(row => row.lastName, {
+      id: 'lastName',
+      cell: info => <i>{info.getValue()}</i>,
+      header: () => <span>Last Name</span>
+    }), columnHelper.accessor('age', {
+      id: 'age',
+      header: () => 'Age',
+      cell: info => info.renderValue(),
+      aggregatedCell: ({
+        getValue
+      }) => <>{\`[\${getValue()[0]}-\${getValue()[1]}]\`}</>,
+      aggregationFn: 'extent',
+      enableGrouping: true
+    }), columnHelper.accessor('visits', {
+      id: 'visits',
+      header: () => <span>Visits</span>,
+      aggregationFn: 'sum'
+    }), columnHelper.accessor('status', {
+      id: 'status',
+      header: 'Status',
+      getGroupingValue: row => \`Group By \${row.status}\`,
+      enableGrouping: true
+    }), columnHelper.accessor('progress', {
+      id: 'progress',
+      header: 'Profile Progress',
+      cell: ({
+        getValue
+      }) => \`\${Math.round(getValue<number>() * 100) / 100}%\`,
+      aggregationFn: 'mean',
+      aggregatedCell: ({
+        getValue
+      }) => \`~\${Math.round(getValue<number>() * 100) / 100}%\`
+    })] as TableColumnDef<MockData, any>[]
+  },
+  render: args => <>
+      <span>
+        You can group by <b>status</b>
+      </span>
+      <DataTable {...args} />
+    </>
+}`,...(W=(Q=f.parameters)==null?void 0:Q.docs)==null?void 0:W.source}}};var X,Z,ee;b.parameters={...b.parameters,docs:{...(X=b.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 10),
+    enableFullscreen: true,
+    enableColumnOrdering: true,
+    columns: [columnHelper.group({
+      id: 'group_1',
+      header: () => <span>Group 1</span>,
+      columns: [columnHelper.accessor('firstName', {
+        cell: info => info.getValue(),
+        footer: props => props.column.id
+      }), columnHelper.accessor(row => row.lastName, {
+        id: 'lastName',
+        cell: info => info.getValue(),
+        header: () => <span>Last Name</span>,
+        footer: props => props.column.id
+      })]
+    }), columnHelper.group({
+      header: 'group_2',
+      columns: [columnHelper.accessor('age', {
+        header: () => 'Age',
+        footer: props => props.column.id
+      }), columnHelper.group({
+        header: 'More Info',
+        columns: [columnHelper.accessor('visits', {
+          header: () => <span>Visits</span>
+        }), columnHelper.accessor('status', {
+          header: 'Status'
+        }), columnHelper.accessor('progress', {
+          header: 'Profile Progress'
+        })]
+      })]
+    })] as TableColumnDef<MockData, any>[]
+  },
+  render: args => <DataTable {...args} />
+}`,...(ee=(Z=b.parameters)==null?void 0:Z.docs)==null?void 0:ee.source}}};var re,ae,se;x.parameters={...x.parameters,docs:{...(re=x.parameters)==null?void 0:re.docs,source:{originalSource:`{
+  args: {
+    columns: [columnHelper.accessor('firstName', {
+      cell: info => info.getValue(),
+      header: () => <span>First Name</span>,
+      footer: () => <span>First Name</span>
+    }), columnHelper.accessor(row => row.lastName, {
+      id: 'lastName',
+      cell: info => <i>{info.getValue()}</i>,
+      header: () => <span>Last Name</span>,
+      footer: () => <span>Last Name</span>
+    }), columnHelper.accessor('age', {
+      header: () => 'Age',
+      cell: info => info.renderValue(),
+      footer: () => <span>Age</span>
+    }), columnHelper.accessor('visits', {
+      header: () => <span>Visits</span>,
+      footer: () => <span>First Name</span>
+    }), columnHelper.accessor('status', {
+      header: 'Status',
+      footer: () => <span>Status</span>
+    }), columnHelper.accessor('progress', {
+      header: 'Profile Progress',
+      footer: () => <span>Profile Progress</span>
+    })]
+  },
+  render: args => <DataTable {...args} />
+}`,...(se=(ae=x.parameters)==null?void 0:ae.docs)==null?void 0:se.source}}};const vr=["Loading","Empty","Error","Default","DefaultTableFilters","Fullscreen","LessThan10Results","DataTableFiltersAndLessThan10Results","PaginationHidden","PaginationLocale","GroupBy","HeaderGroups","Footer"];export{m as DataTableFiltersAndLessThan10Results,c as Default,p as DefaultTableFilters,l as Empty,i as Error,x as Footer,d as Fullscreen,f as GroupBy,b as HeaderGroups,u as LessThan10Results,o as Loading,g as PaginationHidden,h as PaginationLocale,vr as __namedExportsOrder,Vr as default};
