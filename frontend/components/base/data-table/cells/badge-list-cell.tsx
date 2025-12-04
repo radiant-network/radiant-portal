904f9f22ffac92f9521d99333df2b19c4bb9e8ca
GaelleA <galtefrohne@ferlab.bio>
2025-12-04 10:41:20 -0500

    feat(dependencies): SJRA-917 update dep to resolve critical alert

--- frontend/components/base/data-table/cells/badge-list-cell.tsx
+++ frontend/components/base/data-table/cells/badge-list-cell.tsx
@@ -1,20 +1,22 @@
+import { Badge, BadgeProps } from '../../ui/badge';
 
-import { Badge, BadgeProps } from "../../ui/badge";
-import EmptyCell from "./empty-cell";
+import EmptyCell from './empty-cell';
 
 type BadgeListProps = BadgeProps & {
   badges?: string[];
 };
 function BadgeListCell({ badges, ...props }: BadgeListProps) {
-
   if (!badges || badges?.length === 0) return <EmptyCell />;
 
   return (
     <div className="flex gap-1">
-      {badges.map(badge => (
-        <Badge className="capitalize" {...props}>{badge}</Badge>
+      {badges.map((badge, index) => (
+        <Badge key={index} className="capitalize" {...props}>
+          {badge}
+        </Badge>
       ))}
     </div>
   );
-};
+}
+
 export default BadgeListCell;
