2a7659979e8464b5d3393e9971151f16621c973f
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-12-08 10:37:22 -0500

    feat: SJRA-935 Adjust Cypress tests related to recent changes

--- cypress/api/Samples/Batch/ProcessWorker/Sample006.cy.ts
+++ cypress/api/Samples/Batch/ProcessWorker/Sample006.cy.ts
@@ -43,19 +43,19 @@ describe('Samples - Batch - Process worker - Sample006', () => {
     cy.validateSummary(response, 0 /*created*/, 0 /*updated*/, 0 /*skipped*/, 1 /*errors*/);
   });
 
-  it('Validate report error count [SJRA-912]', () => {
+  it('Validate report error count', () => {
     expect(Object.keys(response.body.report.error)).to.have.lengthOf(3);
   });
 
-  it('Validate report sample[0] tissue_site [SJRA-912]', () => {
-    cy.validateReport(response, 'error', 'SAMPLE-006', apiMessages.ProcessWorkerErrorRegExp('sample', 'tissue_site', 'CQGC', 'dna!'), 'sample[0].tissue_site');
+  it('Validate report sample[0] tissue_site', () => {
+    cy.validateReport(response, 'error', 'SAMPLE-006', apiMessages.ProcessWorkerErrorRegExpTissue('sample', 'tissue_site', 'CQGC', '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890!'), 'sample[0].tissue_site');
   });
 
-  it('Validate report sample[0] submitter_sample_id 1 [SJRA-912]', () => {
-    cy.validateReport(response, 'error', 'SAMPLE-006', apiMessages.ProcessWorkerErrorRegExp('sample', 'submitter_sample_id', 'CQGC', '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890!'), 'sample[0].submitter_sample_id');
+  it('Validate report sample[0] submitter_sample_id 1', () => {
+    cy.validateReport(response, 'error', 'SAMPLE-006', apiMessages.ProcessWorkerErrorRegExpId('sample', 'submitter_sample_id', 'CQGC', '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890!'), 'sample[0].submitter_sample_id');
   });
 
-  it('Validate report sample[0] submitter_sample_id 2 [SJRA-912]', () => {
+  it('Validate report sample[0] submitter_sample_id 2', () => {
     cy.validateReport(response, 'error', 'SAMPLE-006', apiMessages.ProcessWorkerErrorTooLong('sample', 'submitter_sample_id', 'CQGC', '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890!'), 'sample[0].submitter_sample_id');
   });
 });
