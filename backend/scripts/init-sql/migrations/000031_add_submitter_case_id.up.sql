28a66616dd48ad329bf868d4acce456bfeb4366d
Charles Morin <charles.mor@outlook.fr>
2025-12-15 15:33:49 -0500

    feat: SJRA-975 added unique constraint to submitter case id

--- backend/scripts/init-sql/migrations/000031_add_submitter_case_id.up.sql
+++ backend/scripts/init-sql/migrations/000031_add_submitter_case_id.up.sql
@@ -3,3 +3,5 @@ ALTER TABLE cases ADD COLUMN submitter_case_id TEXT;
 UPDATE cases SET submitter_case_id = CONCAT(project_id, ':', id);
 
 ALTER TABLE cases ALTER COLUMN submitter_case_id SET NOT NULL;
+
+ALTER TABLE cases ADD CONSTRAINT UC_cases_submitter_case_id UNIQUE (submitter_case_id);
