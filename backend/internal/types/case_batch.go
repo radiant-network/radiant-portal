75589110cf854aec0adaa49535888d97e273bd3f
Charles Morin <charles.mor@outlook.fr>
2025-12-16 15:46:34 -0500

    feat: SJRA-966 added case batch struct

---
+++ backend/internal/types/case_batch.go
@@ -0,0 +1,80 @@
+package types
+
+type CaseBatch struct {
+	SubmitterCaseId            string                           `json:"submitter_case_id" binding:"required"`
+	Type                       string                           `json:"type" binding:"required,oneof=germline somatic"`
+	StatusCode                 string                           `json:"status_code" binding:"required"`
+	ProjectCode                string                           `json:"project_code" binding:"required"`
+	DiagnosticLabCode          string                           `json:"diagnostic_lab_code,omitempty"`
+	PrimaryConditionCodeSystem string                           `json:"primary_condition_code_system,omitempty"`
+	PrimaryConditionValue      string                           `json:"primary_condition_value,omitempty"`
+	PriorityCode               string                           `json:"priority_code,omitempty"`
+	CategoryCode               string                           `json:"category_code" binding:"required,oneof=prenatal postnatal"`
+	AnalysisCode               string                           `json:"analysis_code,omitempty"`
+	ResolutionStatusCode       string                           `json:"resolution_status_code,omitempty"`
+	Note                       string                           `json:"note,omitempty"`
+	OrderingPhysician          string                           `json:"ordering_physician,omitempty"`
+	OrderingOrganizationCode   string                           `json:"ordering_organization_code,omitempty"`
+	Patients                   []*CasePatientBatch              `json:"patients" binding:"required,dive"`
+	SequencingExperiments      []*CaseSequencingExperimentBatch `json:"sequencing_experiments" binding:"required,dive"`
+	Tasks                      []*CaseTaskBatch                 `json:"tasks" binding:"required,dive"`
+}
+
+type CasePatientBatch struct {
+	AffectedStatusCode      string                         `json:"affected_status_code" binding:"required"`
+	FamilyHistory           []*FamilyHistoryBatch          `json:"family_history,omitempty" binding:"dive"`
+	ObservationsCategorical []*ObservationCategoricalBatch `json:"observations_categorical,omitempty" binding:"dive"`
+	ObservationsText        []*ObservationTextBatch        `json:"observations_text,omitempty" binding:"dive"`
+	SubmitterPatientId      string                         `json:"submitter_patient_id" binding:"required"`
+	PatientOrganizationCode string                         `json:"patient_organization_code" binding:"required"`
+	RelationToProbandCode   string                         `json:"relation_to_proband_code" binding:"required"`
+}
+
+type FamilyHistoryBatch struct {
+	FamilyMemberCode string `json:"family_member_code" binding:"required"`
+	Condition        string `json:"condition" binding:"required"`
+}
+
+type ObservationCategoricalBatch struct {
+	Code               string `json:"code" binding:"required"`
+	System             string `json:"system" binding:"required"`
+	Value              string `json:"value" binding:"required"`
+	OnsetCode          string `json:"onset_code" binding:"required"`
+	InterpretationCode string `json:"interpretation_code" binding:"required"`
+	Note               string `json:"note,omitempty"`
+}
+
+type ObservationTextBatch struct {
+	Code string `json:"code" binding:"required"`
+	Note string `json:"note" binding:"required"`
+}
+
+type CaseSequencingExperimentBatch struct {
+	Aliquot                string `json:"aliquot" binding:"required"`
+	SampleOrganizationCode string `json:"sample_organization_code" binding:"required"`
+	SubmitterSampleId      string `json:"submitter_sample_id" binding:"required"`
+}
+
+type CaseTaskBatch struct {
+	TypeCode        string                 `json:"type_code" binding:"required"`
+	Aliquot         string                 `json:"aliquot,omitempty"`
+	InputDocuments  []*InputDocumentBatch  `json:"input_documents,omitempty" binding:"dive"`
+	OutputDocuments []*OutputDocumentBatch `json:"output_documents" binding:"required,dive"`
+	PipelineName    string                 `json:"pipeline_name,omitempty"`
+	PipelineVersion string                 `json:"pipeline_version" binding:"required"`
+	GenomeBuild     string                 `json:"genome_build,omitempty"`
+}
+
+type InputDocumentBatch struct {
+	Url string `json:"url" binding:"required"`
+}
+
+type OutputDocumentBatch struct {
+	DataCategoryCode string `json:"data_category_code" binding:"required"`
+	DataTypeCode     string `json:"data_type_code" binding:"required"`
+	FormatCode       string `json:"format_code" binding:"required"`
+	Hash             string `json:"hash,omitempty"`
+	Name             string `json:"name" binding:"required"`
+	Size             int64  `json:"size" binding:"required"`
+	Url              string `json:"url" binding:"required"`
+}
