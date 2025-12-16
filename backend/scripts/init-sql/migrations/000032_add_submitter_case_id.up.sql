606856ec8428dd5cdb8fb476583522299c24fa18
Charles Morin <charles.mor@outlook.fr>
2025-12-16 15:30:11 -0500

    feat: SJRA-975 fixed unique constraint

--- backend/scripts/init-sql/migrations/000032_add_submitter_case_id.up.sql
+++ backend/scripts/init-sql/migrations/000032_add_submitter_case_id.up.sql
@@ -4,4 +4,4 @@ UPDATE cases SET submitter_case_id = CONCAT(project_id, ':', id);
 
 ALTER TABLE cases ALTER COLUMN submitter_case_id SET NOT NULL;
 
-ALTER TABLE cases ADD CONSTRAINT UC_cases_submitter_case_id UNIQUE (submitter_case_id);
+ALTER TABLE cases ADD CONSTRAINT UC_cases_submitter_case_id UNIQUE (project_id, submitter_case_id);
