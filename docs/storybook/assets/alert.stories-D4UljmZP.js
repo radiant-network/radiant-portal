839ee294136a7e35bd9eb53e4e5fd9c4fb8edff8
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2025-12-24 15:37:55 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@c24b08f4e85b5a01cb47a7dfa2ccf97e8abf316f 🚀

---
+++ docs/storybook/assets/alert.stories-D4UljmZP.js
@@ -0,0 +1,181 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as r,a as s,b as t,c as n,d as l,e as o,f as a}from"./alert-BuxHswgz.js";import{B as i}from"./button-yJUz8gb1.js";import{C as c}from"./circle-alert-DTyzftz0.js";import"./index-CBYaBgW8.js";import"./index-C66Dxnp2.js";import"./utils-CDN07tui.js";import"./x-4HkHZ1eq.js";import"./createLucideIcon-B119WVF5.js";import"./index-C-d7IIsQ.js";import"./index-Dy6y0jaD.js";import"./action-button-9r-08jXC.js";import"./dropdown-menu-DPm_FhUX.js";import"./index-D9mtqW9-.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfgEC-S9.js";import"./index-DnEzm5An.js";import"./check-DSe_yRo5.js";import"./separator-ChZWIdMg.js";import"./button.variants-Du9eY_ux.js";import"./spinner-D7tBPZCQ.js";import"./tooltip-0vX-MTK3.js";import"./index-CfXWnpL9.js";import"./i18n-HRB0heoW.js";import"./iframe-7V1G9me2.js";import"./i18next-CYn7LYXT.js";const Ae={title:"Alerts/Alert",component:i},A={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"default",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Default Alert"}),e.jsx(l,{children:"This is a default alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"default",children:e.jsxs(t,{children:[e.jsx(n,{children:"Default Alert"}),e.jsx(l,{children:"This is a default alert message."})]})}),e.jsxs(r,{variant:"default",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is a default alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},d={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"info",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Info Alert"}),e.jsx(l,{children:"This is an informational alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"info",children:e.jsxs(t,{children:[e.jsx(n,{children:"Info Alert"}),e.jsx(l,{children:"This is an informational alert message."})]})}),e.jsxs(r,{variant:"info",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is an informational alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},h={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"warning",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Warning Alert"}),e.jsx(l,{children:"This is a warning alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"warning",children:e.jsxs(t,{children:[e.jsx(n,{children:"Warning Alert"}),e.jsx(l,{children:"This is a warning alert message."})]})}),e.jsxs(r,{variant:"warning",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is a warning alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},x={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"error",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Error Alert"}),e.jsx(l,{children:"This is an error alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"error",children:e.jsxs(t,{children:[e.jsx(n,{children:"Error Alert"}),e.jsx(l,{children:"This is an error alert message."})]})}),e.jsxs(r,{variant:"error",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is an error alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})},m={render:()=>e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsxs(r,{variant:"success",bordered:!0,children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsxs(t,{children:[e.jsx(n,{children:"Success Alert"}),e.jsx(l,{children:"This is a success alert message."}),e.jsxs(o,{children:[e.jsx(i,{variant:"default",size:"sm",onClick:()=>alert("Main action clicked"),children:"Main Action"}),e.jsx(i,{variant:"ghost",size:"sm",onClick:()=>alert("Other action clicked"),children:"Action"})]})]}),e.jsx(a,{onClick:()=>alert("Alert closed")})]}),e.jsx(r,{variant:"success",children:e.jsxs(t,{children:[e.jsx(n,{children:"Success Alert"}),e.jsx(l,{children:"This is a success alert message."})]})}),e.jsxs(r,{variant:"success",children:[e.jsx(s,{children:e.jsx(c,{})}),e.jsx(t,{children:e.jsx(l,{children:"This is a success alert message."})}),e.jsx(a,{onClick:()=>alert("Alert closed")})]})]})};var j,p,C;A.parameters={...A.parameters,docs:{...(j=A.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="default" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Default Alert</AlertTitle>
+          <AlertDescription>This is a default alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="default">
+        <AlertContent>
+          <AlertTitle>Default Alert</AlertTitle>
+          <AlertDescription>This is a default alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="default">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is a default alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(C=(p=A.parameters)==null?void 0:p.docs)==null?void 0:C.source}}};var u,g,f;d.parameters={...d.parameters,docs:{...(u=d.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="info" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Info Alert</AlertTitle>
+          <AlertDescription>This is an informational alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="info">
+        <AlertContent>
+          <AlertTitle>Info Alert</AlertTitle>
+          <AlertDescription>This is an informational alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="info">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is an informational alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(f=(g=d.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var v,k,T;h.parameters={...h.parameters,docs:{...(v=h.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="warning" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Warning Alert</AlertTitle>
+          <AlertDescription>This is a warning alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="warning">
+        <AlertContent>
+          <AlertTitle>Warning Alert</AlertTitle>
+          <AlertDescription>This is a warning alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="warning">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is a warning alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(T=(k=h.parameters)==null?void 0:k.docs)==null?void 0:T.source}}};var I,D,b;x.parameters={...x.parameters,docs:{...(I=x.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="error" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Error Alert</AlertTitle>
+          <AlertDescription>This is an error alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="error">
+        <AlertContent>
+          <AlertTitle>Error Alert</AlertTitle>
+          <AlertDescription>This is an error alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="error">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is an error alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(b=(D=x.parameters)==null?void 0:D.docs)==null?void 0:b.source}}};var B,z,M;m.parameters={...m.parameters,docs:{...(B=m.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-3">
+      <Alert variant="success" bordered>
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertTitle>Success Alert</AlertTitle>
+          <AlertDescription>This is a success alert message.</AlertDescription>
+          <AlertActions>
+            <Button variant="default" size="sm" onClick={() => alert('Main action clicked')}>
+              Main Action
+            </Button>
+            <Button variant="ghost" size="sm" onClick={() => alert('Other action clicked')}>
+              Action
+            </Button>
+          </AlertActions>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+      <Alert variant="success">
+        <AlertContent>
+          <AlertTitle>Success Alert</AlertTitle>
+          <AlertDescription>This is a success alert message.</AlertDescription>
+        </AlertContent>
+      </Alert>
+      <Alert variant="success">
+        <AlertIcon>
+          <CircleAlert />
+        </AlertIcon>
+        <AlertContent>
+          <AlertDescription>This is a success alert message.</AlertDescription>
+        </AlertContent>
+        <AlertClosableIcon onClick={() => alert('Alert closed')} />
+      </Alert>
+    </div>
+}`,...(M=(z=m.parameters)==null?void 0:z.docs)==null?void 0:M.source}}};const de=["Default","Info","Warning","Error","Success"];export{A as Default,x as Error,d as Info,m as Success,h as Warning,de as __namedExportsOrder,Ae as default};
---
+++ docs/storybook/assets/classification-badge.stories-Bg49G6hU.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{C as a,a as c}from"./classification-badge-CnZDMsLS.js";import"./conditional-wrapper-BBX8pIPQ.js";import"./badge-DXI8q3e3.js";import"./index-C66Dxnp2.js";import"./utils-CDN07tui.js";import"./separator-ChZWIdMg.js";import"./index-CBYaBgW8.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-C-d7IIsQ.js";import"./index-Dy6y0jaD.js";import"./x-4HkHZ1eq.js";import"./createLucideIcon-B119WVF5.js";import"./tooltip-0vX-MTK3.js";import"./index-D9mtqW9-.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./index-DnEzm5An.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfXWnpL9.js";import"./i18n-HRB0heoW.js";import"./iframe-7V1G9me2.js";import"./i18next-CYn7LYXT.js";const P={title:"Badges/Classification Badge",component:a,args:{value:"other",variant:"neutral"}},t={render:()=>e.jsx("div",{className:"flex flex-col gap-2 items-start",children:Object.keys(c).map((i,s)=>e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(a,{value:i}),e.jsx(a,{value:i,abbreviated:!0}),e.jsx(a,{value:i,count:s}),e.jsx(a,{value:i,abbreviated:!0,count:s})]},i))})},l={render:()=>e.jsxs("div",{className:"flex flex-col gap-2 items-start",children:[e.jsx("div",{children:"Key without space or underscore e.g. likelybenign"}),Object.keys(c).map((i,s)=>e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(a,{value:i.replaceAll("_","")}),e.jsx(a,{value:i.replaceAll("_",""),abbreviated:!0}),e.jsx(a,{value:i.replaceAll("_",""),count:s}),e.jsx(a,{value:i.replaceAll("_",""),abbreviated:!0,count:s})]},i))]})},r={render:()=>e.jsxs("div",{className:"flex flex-col gap-2 items-start",children:[e.jsx("div",{children:"Key without space or underscore e.g. _low_penetrance"}),Object.keys(c).map((i,s)=>e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(a,{value:"_"+i}),e.jsx(a,{value:"_"+i,abbreviated:!0}),e.jsx(a,{value:"_"+i,count:s}),e.jsx(a,{value:"_"+i,abbreviated:!0,count:s})]},i))]})},n={render:()=>e.jsxs("div",{className:"flex flex-col gap-2 items-start",children:["With an empty string",e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(a,{value:""}),e.jsx(a,{value:"",abbreviated:!0}),e.jsx(a,{value:"",count:1}),e.jsx(a,{value:"",abbreviated:!0,count:1})]}),"With an null value",e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(a,{value:null}),e.jsx(a,{value:null,abbreviated:!0}),e.jsx(a,{value:null,count:1}),e.jsx(a,{value:null,abbreviated:!0,count:1})]})]})};var o,d,u;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-2 items-start">
+      {Object.keys(ClassificationValueMap).map((key, index) => <div className="flex items-center gap-2" key={key}>
+          <ClassificationBadge value={key} />
+          <ClassificationBadge value={key} abbreviated />
+          <ClassificationBadge value={key} count={index} />
+          <ClassificationBadge value={key} abbreviated count={index} />
+        </div>)}
+    </div>
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var v,m,p;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-2 items-start">
+      <div>Key without space or underscore e.g. likelybenign</div>
+      {Object.keys(ClassificationValueMap).map((key, index) => <div className="flex items-center gap-2" key={key}>
+          <ClassificationBadge value={key.replaceAll('_', '')} />
+          <ClassificationBadge value={key.replaceAll('_', '')} abbreviated />
+          <ClassificationBadge value={key.replaceAll('_', '')} count={index} />
+          <ClassificationBadge value={key.replaceAll('_', '')} abbreviated count={index} />
+        </div>)}
+    </div>
+}`,...(p=(m=l.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var x,f,g;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-2 items-start">
+      <div>Key without space or underscore e.g. _low_penetrance</div>
+      {Object.keys(ClassificationValueMap).map((key, index) => <div className="flex items-center gap-2" key={key}>
+          <ClassificationBadge value={'_' + key} />
+          <ClassificationBadge value={'_' + key} abbreviated />
+          <ClassificationBadge value={'_' + key} count={index} />
+          <ClassificationBadge value={'_' + key} abbreviated count={index} />
+        </div>)}
+    </div>
+}`,...(g=(f=r.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var b,j,C;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: () => <div className="flex flex-col gap-2 items-start">
+      With an empty string
+      <div className="flex items-center gap-2">
+        <ClassificationBadge value="" />
+        <ClassificationBadge value="" abbreviated />
+        <ClassificationBadge value="" count={1} />
+        <ClassificationBadge value="" abbreviated count={1} />
+      </div>
+      With an null value
+      <div className="flex items-center gap-2">
+        <ClassificationBadge value={null} />
+        <ClassificationBadge value={null} abbreviated />
+        <ClassificationBadge value={null} count={1} />
+        <ClassificationBadge value={null} abbreviated count={1} />
+      </div>
+    </div>
+}`,...(C=(j=n.parameters)==null?void 0:j.docs)==null?void 0:C.source}}};const Q=["Default","ClassificationValueWithoutUnderscore","ClassificationValueStartingWithAnUnderscore","NoData"];export{r as ClassificationValueStartingWithAnUnderscore,l as ClassificationValueWithoutUnderscore,t as Default,n as NoData,Q as __namedExportsOrder,P as default};
