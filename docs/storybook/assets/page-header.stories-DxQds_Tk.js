8bc6fc61d01c8a0fd16a6eacb23ac7e54b082948
lflangis <65532894+lflangis@users.noreply.github.com>
2026-01-07 14:32:45 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@bc5154a8fb393628e04fcbc079335dce706e830c 🚀

---
+++ docs/storybook/assets/page-header.stories-DxQds_Tk.js
@@ -0,0 +1,33 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{P as o}from"./page-header-DOuXx5JF.js";import{U as c}from"./users-BCnyYR8e.js";import{B as l}from"./chunk-WWGJGFF6-0IpzJ_3T.js";import"./index-C66Dxnp2.js";import"./badge-DXI8q3e3.js";import"./utils-CDN07tui.js";import"./separator-ChZWIdMg.js";import"./index-CBYaBgW8.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-C-d7IIsQ.js";import"./index-Dy6y0jaD.js";import"./x-4HkHZ1eq.js";import"./createLucideIcon-B119WVF5.js";import"./button-3CxuszzO.js";import"./action-button-9r-08jXC.js";import"./dropdown-menu-DPm_FhUX.js";import"./index-D9mtqW9-.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfgEC-S9.js";import"./index-DnEzm5An.js";import"./check-DSe_yRo5.js";import"./button.variants-Du9eY_ux.js";import"./spinner-D7tBPZCQ.js";import"./tooltip-0vX-MTK3.js";import"./index-CfXWnpL9.js";import"./i18n-D3pIa3my.js";import"./iframe-D1KVRCYH.js";import"./i18next-CYn7LYXT.js";import"./skeleton-_T1otFf0.js";const X={title:"Headers/Page-Header",component:o,args:{}},e={args:{},render:()=>r.jsx(o,{})},t={args:{isLoading:!1,title:"Title",badges:[{variant:"secondary",children:r.jsxs(r.Fragment,{children:[r.jsx(c,{}),"Icon"]})},{variant:"outline",children:r.jsx(r.Fragment,{children:"outline"})}],buttons:[{children:"primary"},{variant:"secondary",children:"Secondary"}],description:"Optional description text…"},render:d=>r.jsx(l,{children:r.jsx(o,{...d})})};var n,i,a;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    return <PageHeader />;
+  }
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var s,m,p;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    isLoading: false,
+    title: 'Title',
+    badges: [{
+      variant: 'secondary',
+      children: <>
+            <Users />
+            Icon
+          </>
+    }, {
+      variant: 'outline',
+      children: <>outline</>
+    }],
+    buttons: [{
+      children: 'primary'
+    }, {
+      variant: 'secondary',
+      children: 'Secondary'
+    }],
+    description: 'Optional description text…'
+  },
+  render: args => {
+    return <BrowserRouter>
+        <PageHeader {...args} />
+      </BrowserRouter>;
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const Y=["Loading","Default"];export{t as Default,e as Loading,Y as __namedExportsOrder,X as default};
---
+++ docs/storybook/assets/tooltip.stories-DYgL7wKq.js
@@ -0,0 +1,26 @@
+import{j as i}from"./jsx-runtime-D_zvdyIk.js";import{B as o}from"./button-3CxuszzO.js";import{b as t,T as r,a as s}from"./tooltip-0vX-MTK3.js";import"./index-C-d7IIsQ.js";import"./index-CBYaBgW8.js";import"./index-Dy6y0jaD.js";import"./action-button-9r-08jXC.js";import"./dropdown-menu-DPm_FhUX.js";import"./index-D9mtqW9-.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfgEC-S9.js";import"./createLucideIcon-B119WVF5.js";import"./index-DnEzm5An.js";import"./utils-CDN07tui.js";import"./check-DSe_yRo5.js";import"./separator-ChZWIdMg.js";import"./button.variants-Du9eY_ux.js";import"./index-C66Dxnp2.js";import"./spinner-D7tBPZCQ.js";import"./i18n-D3pIa3my.js";import"./iframe-D1KVRCYH.js";import"./i18next-CYn7LYXT.js";import"./index-CfXWnpL9.js";const w={title:"Tooltips/Tooltip",component:t,args:{children:"Tooltip Content",sideOffset:8,side:"top"}},e={render:m=>i.jsxs("div",{className:"flex flex-col gap-3 justify-center p-24",children:[i.jsx("div",{className:"flex justify-center",children:i.jsxs(r,{children:[i.jsx(s,{asChild:!0,children:i.jsx(o,{color:"primary",children:"Hover Me"})}),i.jsx(t,{...m})]})}),i.jsx("div",{className:"flex justify-center",children:i.jsxs(r,{children:[i.jsx(s,{asChild:!0,children:i.jsx(o,{color:"primary",children:"Hover Me (long text)"})}),i.jsx(t,{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a dolor nisl. Praesent varius vestibulum dui vitae aliquam. Suspendisse ac commodo nisl, non congue diam. Aliquam elementum, lectus sit amet viverra pharetra, leo magna iaculis ex, ut volutpat nulla nisl quis lacus. Phasellus vestibulum sit amet justo non facilisis. Proin suscipit condimentum orci eu eleifend. In porttitor libero sed arcu semper, non porta neque imperdiet."})]})})]})};var n,l,a;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => <div className="flex flex-col gap-3 justify-center p-24">
+      <div className="flex justify-center">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button color="primary">Hover Me</Button>
+          </TooltipTrigger>
+          <TooltipContent {...args} />
+        </Tooltip>
+      </div>
+      <div className="flex justify-center">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button color="primary">Hover Me (long text)</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a dolor nisl. Praesent varius vestibulum dui
+            vitae aliquam. Suspendisse ac commodo nisl, non congue diam. Aliquam elementum, lectus sit amet viverra
+            pharetra, leo magna iaculis ex, ut volutpat nulla nisl quis lacus. Phasellus vestibulum sit amet justo non
+            facilisis. Proin suscipit condimentum orci eu eleifend. In porttitor libero sed arcu semper, non porta neque
+            imperdiet.
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </div>
+}`,...(a=(l=e.parameters)==null?void 0:l.docs)==null?void 0:a.source}}};const z=["Default"];export{e as Default,z as __namedExportsOrder,w as default};
