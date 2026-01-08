4bb4fb14f36f1642d89cc8f0b3708c0d02b521ac
Celine Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2026-01-08 15:14:19 -0500

    feat: SJRA-1052 add schema again

--- backend/scripts/init-sql/migrations/000001_init.up.sql
+++ backend/scripts/init-sql/migrations/000001_init.up.sql
@@ -12,8 +12,19 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+--
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
 
 --
 -- Name: tp_history_func(); Type: FUNCTION; Schema: public; Owner: radiant
