ff44e31ac1805139b75b3498c162bbf8fe5fbdac
Celine Pelletier <celine.pelletier.hsj@ssss.gouv.qc.ca>
2026-01-08 15:07:06 -0500

    feat: SJRA-1052 try to fix migration in chop env

--- backend/scripts/init-sql/migrations/000001_init.up.sql
+++ backend/scripts/init-sql/migrations/000001_init.up.sql
@@ -12,33 +12,8 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
-
-
---
--- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
-
-
---
--- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
-
-
---
--- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
-
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 --
 -- Name: tp_history_func(); Type: FUNCTION; Schema: public; Owner: radiant
