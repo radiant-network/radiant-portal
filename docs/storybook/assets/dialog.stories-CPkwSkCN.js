d4a5f4d11c9ed73d54ad359b165c7490dd5a4c65
lflangis <65532894+lflangis@users.noreply.github.com>
2026-01-12 19:13:48 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@63fd3b471fe397b47a4f7ef613f385fce88e7adf 🚀

---
+++ docs/storybook/assets/dialog.stories-CPkwSkCN.js
@@ -0,0 +1,243 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r as t}from"./index-CBYaBgW8.js";import{B as o}from"./button-Dg86JJyb.js";import{D as r,a as s,b as a,c as l,d as c,e as d,f as g,g as p}from"./dialog-TAP2eCwF.js";import{c as K}from"./createLucideIcon-B119WVF5.js";import"./index-C-d7IIsQ.js";import"./index-Dy6y0jaD.js";import"./action-button-9r-08jXC.js";import"./dropdown-menu-DPm_FhUX.js";import"./index-D9mtqW9-.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfgEC-S9.js";import"./index-DnEzm5An.js";import"./utils-CDN07tui.js";import"./check-DSe_yRo5.js";import"./separator-ChZWIdMg.js";import"./button.variants-Du9eY_ux.js";import"./index-C66Dxnp2.js";import"./spinner-D7tBPZCQ.js";import"./tooltip-0vX-MTK3.js";import"./index-CfXWnpL9.js";import"./i18n-d0VgJIIQ.js";import"./iframe-DQpbosYN.js";import"./i18next-CYn7LYXT.js";import"./index-Cq0TEE3l.js";import"./x-4HkHZ1eq.js";/**
+ * @license lucide-react v0.482.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=[["path",{d:"M5.7 21a2 2 0 0 1-3.5-2l8.6-14a6 6 0 0 1 10.4 6 2 2 0 1 1-3.464-2 2 2 0 1 0-3.464-2Z",key:"isaq8g"}],["path",{d:"M17.75 7 15 2.1",key:"12x7e8"}],["path",{d:"M10.9 4.8 13 9",key:"100a87"}],["path",{d:"m7.9 9.7 2 4.4",key:"ntfhaj"}],["path",{d:"M4.9 14.7 7 18.9",key:"1x43jy"}]],Q=K("CandyCane",P),we={title:"Modals/Dialog",component:r,args:{}},h={args:{},render:D=>{const[i,n]=t.useState(!1);return e.jsxs(r,{open:i,onOpenChange:n,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>n(!0),children:"Open"})}),e.jsxs(a,{children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>n(!1),children:"Close"})})]})]})}},u={args:{},render:D=>{const[i,n]=t.useState(!1),[R,C]=t.useState(!1),[Z,f]=t.useState(!1),[A,b]=t.useState(!1),[J,O]=t.useState(!1);return e.jsxs("div",{className:"flex gap-4",children:[e.jsxs(r,{open:i,onOpenChange:n,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>n(!0),children:"Open SM"})}),e.jsxs(a,{size:"sm",children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title SM"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>n(!1),children:"Close"})})]})]}),e.jsxs(r,{open:R,onOpenChange:C,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>C(!0),children:"Open MD / default"})}),e.jsxs(a,{size:"md",children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title MD"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>C(!1),children:"Close"})})]})]}),e.jsxs(r,{open:Z,onOpenChange:f,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>f(!0),children:"Open LG"})}),e.jsxs(a,{size:"lg",children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title LG"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>f(!1),children:"Close"})})]})]}),e.jsxs(r,{open:A,onOpenChange:b,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>b(!0),children:"Open XL"})}),e.jsxs(a,{size:"xl",children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title XL"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>b(!1),children:"Close"})})]})]}),e.jsxs(r,{open:J,onOpenChange:O,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>O(!0),children:"Open Full"})}),e.jsxs(a,{size:"full",children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title Full"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>O(!1),children:"Close"})})]})]})]})}},m={args:{},render:D=>{const[i,n]=t.useState(!1);return e.jsxs(r,{open:i,onOpenChange:n,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>n(!0),children:"Open"})}),e.jsxs(a,{variant:"stickyHeader",className:"h-[300px]",children:[e.jsxs(l,{className:"sticky top-0",children:[e.jsx(c,{children:"Dialog Title"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsxs("div",{className:"overflow-auto",children:[e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>n(!1),children:"Close"})})]})]})]})}},x={args:{},render:D=>{const[i,n]=t.useState(!1);return e.jsxs(r,{open:i,onOpenChange:n,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>n(!0),children:"Open"})}),e.jsxs(a,{variant:"stickyFooter",className:"h-[300px]",children:[e.jsxs("div",{className:"overflow-auto",children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-56",children:"Slot (swap it with your content)"})})]}),e.jsx(p,{children:e.jsx(o,{onClick:()=>n(!1),children:"Close"})})]})]})}},j={args:{},render:D=>{const[i,n]=t.useState(!1);return e.jsxs(r,{open:i,onOpenChange:n,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>n(!0),children:"Open"})}),e.jsxs(a,{variant:"stickyBoth",className:"h-[300px]",children:[e.jsxs(l,{children:[e.jsx(c,{children:"Dialog Title"}),e.jsx(d,{children:"Dialog Description"})]}),e.jsx("div",{className:"overflow-auto px-6 py-4",children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-56",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>n(!1),children:"Close"})})]})]})}},y={args:{},render:D=>{const[i,n]=t.useState(!1);return e.jsxs(r,{open:i,onOpenChange:n,children:[e.jsx(s,{children:e.jsx(o,{onClick:()=>n(!0),children:"Open"})}),e.jsxs(a,{children:[e.jsxs(l,{icon:e.jsx(Q,{}),children:[e.jsx(c,{children:"Dialog Title"}),e.jsx(d,{children:"Dialog Description Description Description Description Description Description Description Description Description Description Description Description"})]}),e.jsx(g,{children:e.jsx("div",{className:"flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40",children:"Slot (swap it with your content)"})}),e.jsx(p,{children:e.jsx(o,{onClick:()=>n(!1),children:"Close"})})]})]})}};var S,B,T;h.parameters={...h.parameters,docs:{...(S=h.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {},
+  render: _args => {
+    const [open, setOpen] = useState<boolean>(false);
+    return <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <Button onClick={() => setOpen(true)}>Open</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Dialog Title</DialogTitle>
+            <DialogDescription>Dialog Description</DialogDescription>
+          </DialogHeader>
+          <DialogBody>
+            <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+              Slot (swap it with your content)
+            </div>
+          </DialogBody>
+          <DialogFooter>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>;
+  }
+}`,...(T=(B=h.parameters)==null?void 0:B.docs)==null?void 0:T.source}}};var k,v,w;u.parameters={...u.parameters,docs:{...(k=u.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {},
+  render: _args => {
+    const [openSm, setOpenSm] = useState<boolean>(false);
+    const [openMd, setOpenMd] = useState<boolean>(false);
+    const [openLg, setOpenLg] = useState<boolean>(false);
+    const [openXl, setOpenXl] = useState<boolean>(false);
+    const [openFull, setOpenFull] = useState<boolean>(false);
+    return <div className="flex gap-4">
+        <Dialog open={openSm} onOpenChange={setOpenSm}>
+          <DialogTrigger>
+            <Button onClick={() => setOpenSm(true)}>Open SM</Button>
+          </DialogTrigger>
+          <DialogContent size="sm">
+            <DialogHeader>
+              <DialogTitle>Dialog Title SM</DialogTitle>
+              <DialogDescription>Dialog Description</DialogDescription>
+            </DialogHeader>
+            <DialogBody>
+              <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+                Slot (swap it with your content)
+              </div>
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => setOpenSm(false)}>Close</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog open={openMd} onOpenChange={setOpenMd}>
+          <DialogTrigger>
+            <Button onClick={() => setOpenMd(true)}>Open MD / default</Button>
+          </DialogTrigger>
+          <DialogContent size="md">
+            <DialogHeader>
+              <DialogTitle>Dialog Title MD</DialogTitle>
+              <DialogDescription>Dialog Description</DialogDescription>
+            </DialogHeader>
+            <DialogBody>
+              <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+                Slot (swap it with your content)
+              </div>
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => setOpenMd(false)}>Close</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog open={openLg} onOpenChange={setOpenLg}>
+          <DialogTrigger>
+            <Button onClick={() => setOpenLg(true)}>Open LG</Button>
+          </DialogTrigger>
+          <DialogContent size="lg">
+            <DialogHeader>
+              <DialogTitle>Dialog Title LG</DialogTitle>
+              <DialogDescription>Dialog Description</DialogDescription>
+            </DialogHeader>
+            <DialogBody>
+              <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+                Slot (swap it with your content)
+              </div>
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => setOpenLg(false)}>Close</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog open={openXl} onOpenChange={setOpenXl}>
+          <DialogTrigger>
+            <Button onClick={() => setOpenXl(true)}>Open XL</Button>
+          </DialogTrigger>
+          <DialogContent size="xl">
+            <DialogHeader>
+              <DialogTitle>Dialog Title XL</DialogTitle>
+              <DialogDescription>Dialog Description</DialogDescription>
+            </DialogHeader>
+            <DialogBody>
+              <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+                Slot (swap it with your content)
+              </div>
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => setOpenXl(false)}>Close</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog open={openFull} onOpenChange={setOpenFull}>
+          <DialogTrigger>
+            <Button onClick={() => setOpenFull(true)}>Open Full</Button>
+          </DialogTrigger>
+          <DialogContent size="full">
+            <DialogHeader>
+              <DialogTitle>Dialog Title Full</DialogTitle>
+              <DialogDescription>Dialog Description</DialogDescription>
+            </DialogHeader>
+            <DialogBody>
+              <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+                Slot (swap it with your content)
+              </div>
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => setOpenFull(false)}>Close</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>;
+  }
+}`,...(w=(v=u.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};var F,N,H;m.parameters={...m.parameters,docs:{...(F=m.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {},
+  render: _args => {
+    const [open, setOpen] = useState<boolean>(false);
+    return <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <Button onClick={() => setOpen(true)}>Open</Button>
+        </DialogTrigger>
+        <DialogContent variant="stickyHeader" className="h-[300px]">
+          <DialogHeader className="sticky top-0">
+            <DialogTitle>Dialog Title</DialogTitle>
+            <DialogDescription>Dialog Description</DialogDescription>
+          </DialogHeader>
+          <div className="overflow-auto">
+            <DialogBody>
+              <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+                Slot (swap it with your content)
+              </div>
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => setOpen(false)}>Close</Button>
+            </DialogFooter>
+          </div>
+        </DialogContent>
+      </Dialog>;
+  }
+}`,...(H=(N=m.parameters)==null?void 0:N.docs)==null?void 0:H.source}}};var M,L,_;x.parameters={...x.parameters,docs:{...(M=x.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {},
+  render: _args => {
+    const [open, setOpen] = useState<boolean>(false);
+    return <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <Button onClick={() => setOpen(true)}>Open</Button>
+        </DialogTrigger>
+        <DialogContent variant="stickyFooter" className="h-[300px]">
+          <div className="overflow-auto">
+            <DialogHeader>
+              <DialogTitle>Dialog Title</DialogTitle>
+              <DialogDescription>Dialog Description</DialogDescription>
+            </DialogHeader>
+            <DialogBody>
+              <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-56">
+                Slot (swap it with your content)
+              </div>
+            </DialogBody>
+          </div>
+          <DialogFooter>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>;
+  }
+}`,...(_=(L=x.parameters)==null?void 0:L.docs)==null?void 0:_.source}}};var z,X,G;j.parameters={...j.parameters,docs:{...(z=j.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {},
+  render: _args => {
+    const [open, setOpen] = useState<boolean>(false);
+    return <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <Button onClick={() => setOpen(true)}>Open</Button>
+        </DialogTrigger>
+        <DialogContent variant="stickyBoth" className="h-[300px]">
+          <DialogHeader>
+            <DialogTitle>Dialog Title</DialogTitle>
+            <DialogDescription>Dialog Description</DialogDescription>
+          </DialogHeader>
+          <div className="overflow-auto px-6 py-4">
+            <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-56">
+              Slot (swap it with your content)
+            </div>
+          </div>
+          <DialogFooter>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>;
+  }
+}`,...(G=(X=j.parameters)==null?void 0:X.docs)==null?void 0:G.source}}};var E,I,q;y.parameters={...y.parameters,docs:{...(E=y.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {},
+  render: _args => {
+    const [open, setOpen] = useState<boolean>(false);
+    return <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <Button onClick={() => setOpen(true)}>Open</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader icon={<CandyCane />}>
+            <DialogTitle>Dialog Title</DialogTitle>
+            <DialogDescription>
+              Dialog Description Description Description Description Description Description Description Description
+              Description Description Description Description
+            </DialogDescription>
+          </DialogHeader>
+          <DialogBody>
+            <div className="flex items-center justify-center text-sm border border-dashed border-primary bg-primary/10 rounded h-40">
+              Slot (swap it with your content)
+            </div>
+          </DialogBody>
+          <DialogFooter>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>;
+  }
+}`,...(q=(I=y.parameters)==null?void 0:I.docs)==null?void 0:q.source}}};const Fe=["Default","Sizes","StickyHeader","StickyFooter","StickyBoth","HeaderIcon"];export{h as Default,y as HeaderIcon,u as Sizes,j as StickyBoth,x as StickyFooter,m as StickyHeader,Fe as __namedExportsOrder,we as default};
