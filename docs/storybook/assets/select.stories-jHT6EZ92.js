37f4076798e338aafd5f40a14a68666e592649ec
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2026-01-19 15:52:05 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@9b76e761485c9b01faf63a421566e80d796d1330 🚀

---
+++ docs/storybook/assets/select.stories-jHT6EZ92.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{S as p,a as s,b as o,c as m,e as d,f as u,d as t}from"./select-DnbErV3f.js";import{C as S,A as a}from"./applications-config-3OOAo44D.js";import{a as x}from"./utils-Be9R-1lk.js";import{B as v}from"./chunk-EPOLDU6W-BBQlfikL.js";import"./index-CBYaBgW8.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-D9mtqW9-.js";import"./index-Dy6y0jaD.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-DnEzm5An.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-SF2qmtPV.js";import"./index-CfXWnpL9.js";import"./index-C66Dxnp2.js";import"./utils-CDN07tui.js";import"./chevron-down-DOuPo75j.js";import"./createLucideIcon-B119WVF5.js";import"./check-DSe_yRo5.js";import"./chevron-up-C0Hb7JXF.js";const g={variant_entity:{app_id:a.variant_entity},snv_occurrence:{app_id:a.snv_occurrence,aggregations:[]},cnv_occurrence:{app_id:a.cnv_occurrence,aggregations:[]},admin:{admin_code:"admin",app_id:a.admin},portal:{name:"",navigation:{}}},H={title:"Inputs/Select",component:p,args:{},decorators:[r=>e.jsx(v,{children:e.jsx(S,{config:g,children:e.jsx(r,{})})})]},l={args:{},render:()=>e.jsx("div",{className:"w-[180px] flex flex-col gap-2",children:x.map(r=>e.jsxs("div",{children:[e.jsx("span",{children:r}),e.jsxs(p,{children:[e.jsx(s,{size:r,children:e.jsx(o,{placeholder:"Select a fruit"})}),e.jsx(m,{children:e.jsxs(d,{children:[e.jsx(u,{children:"Fruits"}),e.jsx(t,{value:"apple",children:"Apple"}),e.jsx(t,{value:"banana",children:"Banana"}),e.jsx(t,{value:"blueberry",children:"Blueberry"}),e.jsx(t,{value:"grapes",children:"Grapes"}),e.jsx(t,{value:"pineapple",children:"Pineapple"})]})})]})]},r))})};var n,i,c;l.parameters={...l.parameters,docs:{...(n=l.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  render: () => <div className="w-[180px] flex flex-col gap-2">
+      {selectSizes.map(size => <div key={size}>
+          <span>{size}</span>
+          <Select>
+            <SelectTrigger size={size}>
+              <SelectValue placeholder="Select a fruit" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Fruits</SelectLabel>
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>)}
+    </div>
+}`,...(c=(i=l.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const J=["Default"];export{l as Default,J as __namedExportsOrder,H as default};
---
+++ docs/storybook/assets/spinner-DMuui_2m.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{c as s}from"./utils-CDN07tui.js";import{c as n}from"./createLucideIcon-B119WVF5.js";/**
+ * @license lucide-react v0.482.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],t=n("LoaderCircle",i),c=({size:e=16,className:a,...o})=>r.jsx(t,{size:e,className:s("animate-spin",a),...o});c.__docgenInfo={description:"",methods:[],displayName:"Spinner",props:{size:{required:!1,tsType:{name:"number"},description:"",defaultValue:{value:"16",computed:!1}}}};export{t as L,c as S};
