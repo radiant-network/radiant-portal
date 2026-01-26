b8c8cf18bf3391d059ddd02e2ac30fc3b6a5f2bd
Karl Ritchie <ritchie.karl@gmail.com>
2026-01-26 15:34:15 -0500

    fix: SJRA-1140 add missing consanguinity struct

--- backend/internal/types/consanguinity.go
+++ backend/internal/types/consanguinity.go
@@ -1,5 +1,9 @@
 package types
 
+type Consanguinity struct {
+	ValueSet
+}
+
 var ConsanguinityTable = Table{
 	Name:           "consanguinity",
 	FederationName: "radiant_jdbc.public.consanguinity",
