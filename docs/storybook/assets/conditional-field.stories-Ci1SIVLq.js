2fb2e43b2cff074f9dd91e95f7b9b2012c79821c
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2026-01-30 17:09:04 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@f75e9ac876934da1da84225b0993446a573b6c40 🚀

---
+++ docs/storybook/assets/conditional-field.stories-Ci1SIVLq.js
@@ -0,0 +1,10 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{E as c}from"./empty-field-C1VzDKLJ.js";import{C as l,A as e}from"./applications-config-D6sxpq9M.js";import{B as p}from"./chunk-EPOLDU6W-BBQlfikL.js";import"./index-CBYaBgW8.js";function r({condition:i,children:s}){return i?n.jsx(n.Fragment,{children:s}):n.jsx(c,{})}r.__docgenInfo={description:"",methods:[],displayName:"ConditionalField",props:{condition:{required:!0,tsType:{name:"boolean"},description:""},children:{required:!1,tsType:{name:"any"},description:""}}};const m={variant_entity:{app_id:e.variant_entity},snv_occurrence:{app_id:e.snv_occurrence,aggregations:[]},cnv_occurrence:{app_id:e.cnv_occurrence,aggregations:[]},admin:{admin_code:"admin",app_id:e.admin},portal:{name:"",navigation:{}}},_={title:"Informations/ConditionalField",component:r,args:{},decorators:[i=>n.jsx(p,{children:n.jsx(l,{config:m,children:n.jsx(i,{})})})]},o={args:{condition:!0,children:n.jsx(n.Fragment,{children:"This Condition is True"})},render:i=>n.jsxs("div",{className:"flex flex-col gap-6",children:[n.jsx(r,{condition:i.condition,children:i.children}),n.jsx(r,{condition:!1,children:i.children})]})};var a,d,t;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    condition: true,
+    children: <>This Condition is True</>
+  },
+  render: args => <div className="flex flex-col gap-6">
+      <ConditionalField condition={args.condition}>{args.children}</ConditionalField>
+      <ConditionalField condition={false}>{args.children}</ConditionalField>
+    </div>
+}`,...(t=(d=o.parameters)==null?void 0:d.docs)==null?void 0:t.source}}};const j=["Default"];export{o as Default,j as __namedExportsOrder,_ as default};
---
+++ docs/storybook/assets/information-field.stories-DyTb3igG.js
@@ -0,0 +1,30 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{I as o}from"./information-field-CsNmTR2h.js";import{C as j,A as a}from"./applications-config-D6sxpq9M.js";import{B as v}from"./chunk-EPOLDU6W-BBQlfikL.js";import"./tooltip-0vX-MTK3.js";import"./index-CBYaBgW8.js";import"./index-D9mtqW9-.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-Dy6y0jaD.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./index-DnEzm5An.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfXWnpL9.js";import"./utils-CDN07tui.js";import"./empty-field-C1VzDKLJ.js";const W={variant_entity:{app_id:a.variant_entity},snv_occurrence:{app_id:a.snv_occurrence,aggregations:[]},cnv_occurrence:{app_id:a.cnv_occurrence,aggregations:[]},admin:{admin_code:"admin",app_id:a.admin},portal:{name:"",navigation:{}}},N={title:"Informations/InformationField",component:o,args:{},decorators:[e=>r.jsx(v,{children:r.jsx(j,{config:W,children:r.jsx(e,{})})})]},t={args:{label:"Label"},render:e=>r.jsx(o,{...e,children:"InformationField"})},n={args:{label:"Label",labelTooltipText:"Label Tooltip"},render:e=>r.jsx(o,{...e,children:"InformationField"})},i={args:{label:"Label",tooltipText:"Tooltip Text"},render:e=>r.jsx(o,{...e,children:"InformationField"})},l={args:{label:"Label",labelTooltipText:"Label Tooltip",tooltipText:"Tooltip Text"},render:e=>r.jsx(o,{...e,children:"InformationField"})},s={args:{label:"Label"},render:e=>r.jsx(o,{...e,children:void 0})};var p,m,d;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: 'Label'
+  },
+  render: args => <InformationField {...args}>InformationField</InformationField>
+}`,...(d=(m=t.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var c,g,T;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    labelTooltipText: 'Label Tooltip'
+  },
+  render: args => <InformationField {...args}>InformationField</InformationField>
+}`,...(T=(g=n.parameters)==null?void 0:g.docs)==null?void 0:T.source}}};var b,f,x;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    tooltipText: 'Tooltip Text'
+  },
+  render: args => <InformationField {...args}>InformationField</InformationField>
+}`,...(x=(f=i.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var u,I,F;l.parameters={...l.parameters,docs:{...(u=l.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    labelTooltipText: 'Label Tooltip',
+    tooltipText: 'Tooltip Text'
+  },
+  render: args => <InformationField {...args}>InformationField</InformationField>
+}`,...(F=(I=l.parameters)==null?void 0:I.docs)==null?void 0:F.source}}};var L,h,_;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    label: 'Label'
+  },
+  render: args => <InformationField {...args}>{undefined}</InformationField>
+}`,...(_=(h=s.parameters)==null?void 0:h.docs)==null?void 0:_.source}}};const Q=["Default","WithLabelTooltip","WithTooltipText","WithLabelAndTooltipText","Empty"];export{t as Default,s as Empty,l as WithLabelAndTooltipText,n as WithLabelTooltip,i as WithTooltipText,Q as __namedExportsOrder,N as default};
