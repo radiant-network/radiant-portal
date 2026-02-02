6355960a992e79c0b315244303277300a65cb5dd
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2026-02-02 19:06:20 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@85ef386bab6273daa4ee1476e8909453ebbe33d0 🚀

---
+++ docs/storybook/assets/list-item-with-action.stories-rfOPqG2t.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{a as o}from"./index-B-lxVbXh.js";import{L as i,a}from"./list-item-with-action-DguVd6CA.js";import"./v4-CtRu48qb.js";import"./index-C66Dxnp2.js";import"./button-WHQ7Zt6J.js";import"./index-C-d7IIsQ.js";import"./index-CBYaBgW8.js";import"./index-Dy6y0jaD.js";import"./action-button-9r-08jXC.js";import"./dropdown-menu-DPm_FhUX.js";import"./index-D9mtqW9-.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfgEC-S9.js";import"./createLucideIcon-B119WVF5.js";import"./index-DnEzm5An.js";import"./utils-CDN07tui.js";import"./check-DSe_yRo5.js";import"./separator-ChZWIdMg.js";import"./button.variants-Du9eY_ux.js";import"./spinner-DMuui_2m.js";import"./tooltip-0vX-MTK3.js";import"./index-CfXWnpL9.js";import"./i18n-Z7U3RexS.js";import"./iframe-kQnH2Fnl.js";import"./i18next-CYn7LYXT.js";import"./conditional-wrapper-BBX8pIPQ.js";import"./pen-BSZDi--G.js";import"./trash-EX7KNg_H.js";const P={title:"Lists/ListItem with action",component:i,args:{onEdit:o("onEdit"),onDelete:o("onDelete"),onShare:o("onShare"),onClick:o("onClick")}},m={args:{title:"Title",description:"Description"},render:r=>t.jsx("div",{className:"max-w-[450px]",children:t.jsxs(a,{bordered:!0,children:[t.jsx(i,{...r}),t.jsx(i,{...r}),t.jsx(i,{...r}),t.jsx(i,{...r})]})})};var e,p,s;m.parameters={...m.parameters,docs:{...(e=m.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    title: 'Title',
+    description: 'Description'
+  },
+  render: args => <div className="max-w-[450px]">
+      <List bordered>
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+        <ListItemWithAction {...args} />
+      </List>
+    </div>
+}`,...(s=(p=m.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const Q=["Default"];export{m as Default,Q as __namedExportsOrder,P as default};
