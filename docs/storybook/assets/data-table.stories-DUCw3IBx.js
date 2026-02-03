0e40cb02007900263c8a1e64c1dc084a8b289c0a
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2026-02-03 13:53:31 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@c1a4c69e55ecf76c370be56698f6c9321e09d8a4 🚀

---
+++ docs/storybook/assets/data-table.stories-DUCw3IBx.js
@@ -0,0 +1,219 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{a as X}from"./api-DPRnJls5.js";import{F as Z}from"./case-exploration-table-filters-t4KniQqY.js";import{D as s,i as ee}from"./data-table-CMd7xxiI.js";import{C as t,d as n}from"./card-Cyh3E19J.js";import{C as ae,A as l}from"./applications-config-D6sxpq9M.js";import{m as re,d as o,a as se}from"./table-mock-RpycLWxZ.js";import{B as te}from"./chunk-EPOLDU6W-BBQlfikL.js";import"./index-CBYaBgW8.js";import"./filter-button-sRTjazzq.js";import"./badge-B8JYzoyf.js";import"./index-C66Dxnp2.js";import"./utils-CDN07tui.js";import"./separator-ChZWIdMg.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-C-d7IIsQ.js";import"./index-Dy6y0jaD.js";import"./x-4HkHZ1eq.js";import"./createLucideIcon-B119WVF5.js";import"./button-O8T4L_tu.js";import"./action-button-9r-08jXC.js";import"./dropdown-menu-DPm_FhUX.js";import"./index-D9mtqW9-.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfgEC-S9.js";import"./index-DnEzm5An.js";import"./check-DSe_yRo5.js";import"./button.variants-Du9eY_ux.js";import"./spinner-DMuui_2m.js";import"./tooltip-0vX-MTK3.js";import"./index-CfXWnpL9.js";import"./i18n-BbKbA56i.js";import"./iframe-SafmCMQQ.js";import"./i18next-CYn7LYXT.js";import"./checkbox-BUFo-vqr.js";import"./index-SF2qmtPV.js";import"./command-CqDCn1-N.js";import"./index-Cq0TEE3l.js";import"./dialog-TAP2eCwF.js";import"./popover-CV3rAFUt.js";import"./search-DKmUqS9g.js";import"./skeleton-_T1otFf0.js";import"./test-tube-diagonal-Dy9u_yEx.js";import"./user-3oWHM7_v.js";import"./priority-indicator-BHQQLBKV.js";import"./indicator-CdtPwwNK.js";import"./shape-triangle-up-icon-CLEWE8Su.js";import"./refresh-ccw-CVzmjQM2.js";import"./pen-BSZDi--G.js";import"./isEqual-DJddVrXl.js";import"./settings-Dw4TSVKU.js";import"./number-format-D6gxQ_H-.js";import"./pagination-Dg2Fl1gh.js";import"./select-DnbErV3f.js";import"./chevron-down-DOuPo75j.js";import"./chevron-up-C0Hb7JXF.js";import"./ellipsis-RxOQoOKc.js";import"./empty-LncwPSrD.js";import"./chart-scatter-DVTaxjlK.js";import"./index-lnksFm0-.js";import"./circle-alert-DTyzftz0.js";import"./chevron-right-BONyyZTy.js";const r=ee(),ne={variant_entity:{app_id:l.variant_entity},snv_occurrence:{app_id:l.snv_occurrence,aggregations:[]},cnv_occurrence:{app_id:l.cnv_occurrence,aggregations:[]},admin:{admin_code:"admin",app_id:l.admin},portal:{name:"",navigation:{}}},va={title:"Tables/Data Table",component:s,args:{id:"storybook",columns:se,data:o,serverOptions:{defaultSorting:[{field:"pf_wgs",order:X.Asc}],onSortingChange:a=>{}},defaultColumnSettings:re,loadingStates:{total:!1,list:!1},pagination:{type:"server",state:{pageIndex:0,pageSize:10},onPaginationChange:()=>{}},total:10},decorators:[a=>e.jsx(te,{children:e.jsx(ae,{config:ne,children:e.jsx(a,{})})})]},i={args:{loadingStates:{list:!0,total:!0}},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},c={args:{loadingStates:{list:!1,total:!1},data:[]},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},d={args:{loadingStates:{list:!1,total:!1},data:[],hasError:!0},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},u={args:{data:o.slice(0,10),enableFullscreen:!0,enableColumnOrdering:!0},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},m={args:{enableFullscreen:!0},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsxs(n,{children:[e.jsx("span",{children:e.jsx("i",{children:'Use "Open in new canvas" button at the top right of the screen a better preview'})}),e.jsx(s,{...a})]})})})},p={args:{data:o.slice(0,1),total:1,enableFullscreen:!0,enableColumnOrdering:!0},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},g={args:{data:o.slice(0,1),total:1,TableFilters:()=>e.jsx(Z,{loading:!1,setSearchCriteria:()=>{}}),enableFullscreen:!0,enableColumnOrdering:!0,tableIndexResultPosition:"bottom"},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},h={args:{pagination:{type:"hidden"}},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},f={args:{pagination:{type:"locale",state:{pageIndex:0,pageSize:5}}},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})},x={args:{data:o.slice(0,10),enableFullscreen:!0,enableColumnOrdering:!0,columns:[r.accessor("firstName",{id:"firstName",cell:a=>a.getValue(),header:()=>e.jsx("span",{children:"First Name"})}),r.accessor(a=>a.lastName,{id:"lastName",cell:a=>e.jsx("i",{children:a.getValue()}),header:()=>e.jsx("span",{children:"Last Name"})}),r.accessor("age",{id:"age",header:()=>"Age",cell:a=>a.renderValue(),aggregatedCell:({getValue:a})=>e.jsx(e.Fragment,{children:`[${a()[0]}-${a()[1]}]`}),aggregationFn:"extent",enableGrouping:!0}),r.accessor("visits",{id:"visits",header:()=>e.jsx("span",{children:"Visits"}),aggregationFn:"sum"}),r.accessor("status",{id:"status",header:"Status",getGroupingValue:a=>`Group By ${a.status}`,enableGrouping:!0}),r.accessor("progress",{id:"progress",header:"Profile Progress",cell:({getValue:a})=>`${Math.round(a()*100)/100}%`,aggregationFn:"mean",aggregatedCell:({getValue:a})=>`~${Math.round(a()*100)/100}%`})]},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsxs(n,{children:[e.jsxs("span",{children:["You can group by ",e.jsx("b",{children:"status"})]}),e.jsx(s,{...a})]})})})},C={args:{columns:[r.accessor("firstName",{cell:a=>a.getValue(),header:()=>e.jsx("span",{children:"First Name"}),footer:()=>e.jsx("span",{children:"First Name"})}),r.accessor(a=>a.lastName,{id:"lastName",cell:a=>e.jsx("i",{children:a.getValue()}),header:()=>e.jsx("span",{children:"Last Name"}),footer:()=>e.jsx("span",{children:"Last Name"})}),r.accessor("age",{header:()=>"Age",cell:a=>a.renderValue(),footer:()=>e.jsx("span",{children:"Age"})}),r.accessor("visits",{header:()=>e.jsx("span",{children:"Visits"}),footer:()=>e.jsx("span",{children:"First Name"})}),r.accessor("status",{header:"Status",footer:()=>e.jsx("span",{children:"Status"})}),r.accessor("progress",{header:"Profile Progress",footer:()=>e.jsx("span",{children:"Profile Progress"})})]},render:a=>e.jsx("div",{className:"bg-muted w-full size-auto h-screen overflow-auto p-3",children:e.jsx(t,{className:"h-auto size-max w-full",children:e.jsx(n,{children:e.jsx(s,{...a})})})})};var b,w,v;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    loadingStates: {
+      list: true,
+      total: true
+    }
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(v=(w=i.parameters)==null?void 0:w.docs)==null?void 0:v.source}}};var N,j,z;c.parameters={...c.parameters,docs:{...(N=c.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    loadingStates: {
+      list: false,
+      total: false
+    },
+    data: []
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(z=(j=c.parameters)==null?void 0:j.docs)==null?void 0:z.source}}};var F,S,T;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    loadingStates: {
+      list: false,
+      total: false
+    },
+    data: [],
+    hasError: true
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(T=(S=d.parameters)==null?void 0:S.docs)==null?void 0:T.source}}};var V,D,P;u.parameters={...u.parameters,docs:{...(V=u.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 10),
+    enableFullscreen: true,
+    enableColumnOrdering: true
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(P=(D=u.parameters)==null?void 0:D.docs)==null?void 0:P.source}}};var y,H,L;m.parameters={...m.parameters,docs:{...(y=m.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    enableFullscreen: true
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <span>
+            <i>Use &quot;Open in new canvas&quot; button at the top right of the screen a better preview</i>
+          </span>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(L=(H=m.parameters)==null?void 0:H.docs)==null?void 0:L.source}}};var _,O,A;p.parameters={...p.parameters,docs:{...(_=p.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 1),
+    total: 1,
+    enableFullscreen: true,
+    enableColumnOrdering: true
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(A=(O=p.parameters)==null?void 0:O.docs)==null?void 0:A.source}}};var G,$,E;g.parameters={...g.parameters,docs:{...(G=g.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 1),
+    total: 1,
+    TableFilters: () => <TableFilters loading={false} setSearchCriteria={() => {}} />,
+    enableFullscreen: true,
+    enableColumnOrdering: true,
+    tableIndexResultPosition: 'bottom'
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(E=($=g.parameters)==null?void 0:$.docs)==null?void 0:E.source}}};var R,B,I;h.parameters={...h.parameters,docs:{...(R=h.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    pagination: {
+      type: 'hidden'
+    }
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(I=(B=h.parameters)==null?void 0:B.docs)==null?void 0:I.source}}};var M,k,q;f.parameters={...f.parameters,docs:{...(M=f.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    pagination: {
+      type: 'locale',
+      state: {
+        pageIndex: 0,
+        pageSize: 5
+      }
+    }
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(q=(k=f.parameters)==null?void 0:k.docs)==null?void 0:q.source}}};var U,Y,J;x.parameters={...x.parameters,docs:{...(U=x.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    data: data.slice(0, 10),
+    enableFullscreen: true,
+    enableColumnOrdering: true,
+    columns: [columnHelper.accessor('firstName', {
+      id: 'firstName',
+      cell: info => info.getValue(),
+      header: () => <span>First Name</span>
+    }), columnHelper.accessor(row => row.lastName, {
+      id: 'lastName',
+      cell: info => <i>{info.getValue()}</i>,
+      header: () => <span>Last Name</span>
+    }), columnHelper.accessor('age', {
+      id: 'age',
+      header: () => 'Age',
+      cell: info => info.renderValue(),
+      aggregatedCell: ({
+        getValue
+      }) => <>{\`[\${getValue()[0]}-\${getValue()[1]}]\`}</>,
+      aggregationFn: 'extent',
+      enableGrouping: true
+    }), columnHelper.accessor('visits', {
+      id: 'visits',
+      header: () => <span>Visits</span>,
+      aggregationFn: 'sum'
+    }), columnHelper.accessor('status', {
+      id: 'status',
+      header: 'Status',
+      getGroupingValue: row => \`Group By \${row.status}\`,
+      enableGrouping: true
+    }), columnHelper.accessor('progress', {
+      id: 'progress',
+      header: 'Profile Progress',
+      cell: ({
+        getValue
+      }) => \`\${Math.round(getValue<number>() * 100) / 100}%\`,
+      aggregationFn: 'mean',
+      aggregatedCell: ({
+        getValue
+      }) => \`~\${Math.round(getValue<number>() * 100) / 100}%\`
+    })] as TableColumnDef<MockData, any>[]
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <span>
+            You can group by <b>status</b>
+          </span>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(J=(Y=x.parameters)==null?void 0:Y.docs)==null?void 0:J.source}}};var K,Q,W;C.parameters={...C.parameters,docs:{...(K=C.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    columns: [columnHelper.accessor('firstName', {
+      cell: info => info.getValue(),
+      header: () => <span>First Name</span>,
+      footer: () => <span>First Name</span>
+    }), columnHelper.accessor(row => row.lastName, {
+      id: 'lastName',
+      cell: info => <i>{info.getValue()}</i>,
+      header: () => <span>Last Name</span>,
+      footer: () => <span>Last Name</span>
+    }), columnHelper.accessor('age', {
+      header: () => 'Age',
+      cell: info => info.renderValue(),
+      footer: () => <span>Age</span>
+    }), columnHelper.accessor('visits', {
+      header: () => <span>Visits</span>,
+      footer: () => <span>First Name</span>
+    }), columnHelper.accessor('status', {
+      header: 'Status',
+      footer: () => <span>Status</span>
+    }), columnHelper.accessor('progress', {
+      header: 'Profile Progress',
+      footer: () => <span>Profile Progress</span>
+    })]
+  },
+  render: args => <div className="bg-muted w-full size-auto h-screen overflow-auto p-3">
+      <Card className="h-auto size-max w-full">
+        <CardContent>
+          <DataTable {...args} />
+        </CardContent>
+      </Card>
+    </div>
+}`,...(W=(Q=C.parameters)==null?void 0:Q.docs)==null?void 0:W.source}}};const Na=["Loading","Empty","Error","Default","Fullscreen","LessThan10Results","DataTableFiltersAndLessThan10Results","PaginationHidden","PaginationLocale","GroupBy","Footer"];export{g as DataTableFiltersAndLessThan10Results,u as Default,c as Empty,d as Error,C as Footer,m as Fullscreen,x as GroupBy,p as LessThan10Results,i as Loading,h as PaginationHidden,f as PaginationLocale,Na as __namedExportsOrder,va as default};
