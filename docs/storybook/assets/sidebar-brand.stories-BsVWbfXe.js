d874f8536252da934dfba9afae6a4649021945f4
lflangis <65532894+lflangis@users.noreply.github.com>
2026-02-05 13:03:27 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@16da229316de6e59ff8802cde89e85bcef76ed08 🚀

---
+++ docs/storybook/assets/sidebar-brand.stories-BsVWbfXe.js
@@ -0,0 +1,163 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{S as p}from"./sidebar-groups-CLWftXSx.js";import{S as l}from"./sidebar-D8Ey6VHe.js";import{F as e}from"./applications-config-D6sxpq9M.js";import{R as n}from"./sqon-DDFmFWEM.js";import"./index-CBYaBgW8.js";import"./index-C66Dxnp2.js";import"./variant-icon-C_DRBYBR.js";import"./createLucideIcon-B119WVF5.js";import"./button-JKxddmHK.js";import"./index-C-d7IIsQ.js";import"./index-Dy6y0jaD.js";import"./action-button-9r-08jXC.js";import"./dropdown-menu-DPm_FhUX.js";import"./index-D9mtqW9-.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-BCzuw4Jg.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./Combination-CrkgvPnV.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfgEC-S9.js";import"./index-DnEzm5An.js";import"./utils-CDN07tui.js";import"./check-DSe_yRo5.js";import"./separator-ChZWIdMg.js";import"./button.variants-Du9eY_ux.js";import"./spinner-DMuui_2m.js";import"./tooltip-0vX-MTK3.js";import"./index-CfXWnpL9.js";import"./i18n-CqMj48hY.js";import"./iframe-h4AkwOaq.js";import"./i18next-CYn7LYXT.js";import"./panel-left-open-BsQqi0Jz.js";import"./index-C2vczdB5.js";import"./input-Bj-MPxry.js";import"./index-Cq0TEE3l.js";import"./x-4HkHZ1eq.js";import"./skeleton-_T1otFf0.js";import"./api-DPRnJls5.js";const $={title:"Sidebar/Sidebar Brand",component:p,args:{},decorators:[r=>a.jsx(l,{className:"m-[-16px] w-50",children:a.jsx("div",{className:"w-50",children:a.jsx(r,{})})})]},t={args:{aggregationGroups:{variant:{items:[{key:"type",translation_key:"variant_type",type:e.MULTIPLE},{key:"cn",translation_key:"cn",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.GreaterThan}},{key:"length",translation_key:"cnv_length",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.GreaterThan}},{key:"chromosome",translation_key:"chromosome",type:e.MULTIPLE},{key:"start",translation_key:"cnv_start",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.Between}},{key:"end",translation_key:"cnv_end",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.LessThan}},{key:"nb_snv",translation_key:"nb_snv",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.GreaterThan}}]},gene:{items:[{key:"cytoband",translation_key:"cytoband",type:e.MULTIPLE},{key:"gene_divider",translation_key:"gene_divider",type:e.DIVIDER},{key:"hpo_gene_panel",translation_key:"hpo_gene_panel",type:e.MULTIPLE},{key:"orphanet_gene_panel",translation_key:"orphanet_gene_panel",type:e.MULTIPLE},{key:"omim_gene_panel",translation_key:"omim_gene_panel",type:e.MULTIPLE},{key:"ddd_gene_panel",translation_key:"ddd_gene_panel",type:e.MULTIPLE},{key:"cosmic_gene_panel",translation_key:"cosmic_gene_panel",type:e.MULTIPLE}]},frequency:{items:[{key:"frequency_divider_pulic_cohorts",translation_key:"frequency_divider_pulic_cohorts",type:e.DIVIDER},{key:"gnomad_sf",translation_key:"gnomad_sf",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.LessThan,defaultMin:0,defaultMax:100}},{key:"gnomad_sc",translation_key:"gnomad_sc",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.LessThan,defaultMin:0,defaultMax:100}}]},metric_qc:{items:[{key:"filter",translation_key:"filter",type:e.MULTIPLE},{key:"quality",translation_key:"cnv_quality",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.GreaterThan,defaultMin:0,defaultMax:100}},{key:"pe",translation_key:"pe",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.LessThan,defaultMin:0,defaultMax:100}},{key:"sm",translation_key:"sm",type:e.NUMERICAL,defaults:{min:0,max:100,defaultOperator:n.LessThan,defaultMin:0,defaultMax:100}}]}}},render:r=>a.jsx(p,{...r})};var i,s,o;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    aggregationGroups: {
+      variant: {
+        items: [{
+          key: 'type',
+          translation_key: 'variant_type',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'cn',
+          translation_key: 'cn',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.GreaterThan
+          }
+        }, {
+          key: 'length',
+          translation_key: 'cnv_length',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.GreaterThan
+          }
+        }, {
+          key: 'chromosome',
+          translation_key: 'chromosome',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'start',
+          translation_key: 'cnv_start',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.Between
+          }
+        }, {
+          key: 'end',
+          translation_key: 'cnv_end',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.LessThan
+          }
+        }, {
+          key: 'nb_snv',
+          translation_key: 'nb_snv',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.GreaterThan
+          }
+        }]
+      },
+      gene: {
+        items: [{
+          key: 'cytoband',
+          translation_key: 'cytoband',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'gene_divider',
+          translation_key: 'gene_divider',
+          type: FilterTypes.DIVIDER
+        }, {
+          key: 'hpo_gene_panel',
+          translation_key: 'hpo_gene_panel',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'orphanet_gene_panel',
+          translation_key: 'orphanet_gene_panel',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'omim_gene_panel',
+          translation_key: 'omim_gene_panel',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'ddd_gene_panel',
+          translation_key: 'ddd_gene_panel',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'cosmic_gene_panel',
+          translation_key: 'cosmic_gene_panel',
+          type: FilterTypes.MULTIPLE
+        }]
+      },
+      frequency: {
+        items: [{
+          key: 'frequency_divider_pulic_cohorts',
+          translation_key: 'frequency_divider_pulic_cohorts',
+          type: FilterTypes.DIVIDER
+        }, {
+          key: 'gnomad_sf',
+          translation_key: 'gnomad_sf',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.LessThan,
+            defaultMin: 0,
+            defaultMax: 100
+          }
+        }, {
+          key: 'gnomad_sc',
+          translation_key: 'gnomad_sc',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.LessThan,
+            defaultMin: 0,
+            defaultMax: 100
+          }
+        }]
+      },
+      metric_qc: {
+        items: [{
+          key: 'filter',
+          translation_key: 'filter',
+          type: FilterTypes.MULTIPLE
+        }, {
+          key: 'quality',
+          translation_key: 'cnv_quality',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.GreaterThan,
+            defaultMin: 0,
+            defaultMax: 100
+          }
+        }, {
+          key: 'pe',
+          translation_key: 'pe',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.LessThan,
+            defaultMin: 0,
+            defaultMax: 100
+          }
+        }, {
+          key: 'sm',
+          translation_key: 'sm',
+          type: FilterTypes.NUMERICAL,
+          defaults: {
+            min: 0,
+            max: 100,
+            defaultOperator: RangeOperators.LessThan,
+            defaultMin: 0,
+            defaultMax: 100
+          }
+        }]
+      }
+    }
+  },
+  render: args => <SidebarGroups {...args} />
+}`,...(o=(s=t.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const ee=["Default"];export{t as Default,ee as __namedExportsOrder,$ as default};
