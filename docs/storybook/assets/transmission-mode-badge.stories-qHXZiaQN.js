e0efc325c2a69a14e5747d3add5bf073dcfba16c
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2026-02-16 15:43:21 +0000

    Deploying to storybook from @ radiant-network/radiant-portal@77dfafefe1a20c5994e037da3214dcc88b0e60db 🚀

---
+++ docs/storybook/assets/transmission-mode-badge.stories-qHXZiaQN.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{T as a,g as n}from"./transmission-mode-badge-BU8cg3a4.js";import"./badge-B8JYzoyf.js";import"./index-C66Dxnp2.js";import"./utils-CDN07tui.js";import"./separator-ChZWIdMg.js";import"./index-CBYaBgW8.js";import"./index-sTUCEGFJ.js";import"./index-BWnBDfn-.js";import"./index-C-d7IIsQ.js";import"./index-Dy6y0jaD.js";import"./x-4HkHZ1eq.js";import"./createLucideIcon-B119WVF5.js";import"./tooltip-W7g5_Dow.js";import"./index-D9mtqW9-.js";import"./index-C8BBKGwP.js";import"./index-BL8Z-moU.js";import"./index-DnEzm5An.js";import"./index-iyInEJkZ.js";import"./index-C2iKAgIe.js";import"./index-CWHKeK-O.js";import"./index-CfXWnpL9.js";import"./i18n-xmjcbfpT.js";import"./iframe-CmfCMdQ8.js";import"./i18next-CYn7LYXT.js";const q={title:"Badges/Transmission Mode Badge",component:a,args:{value:"other",variant:"neutral"}},r={render:()=>{const m=n(()=>{});return t.jsx("div",{className:"flex flex-col gap-2 items-start",children:m.map(e=>t.jsx("div",{children:t.jsx(a,{value:e.value})},e.value))})}};var i,s,o;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => {
+    // eslint-disable-next-line prettier/prettier
+    const list = getTransmissionModeList(() => {});
+    return <div className="flex flex-col gap-2 items-start">
+        {list.map(transmission => <div key={transmission.value}>
+            <TransmissionModeBadge value={transmission.value} />
+          </div>)}
+      </div>;
+  }
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const w=["Default"];export{r as Default,w as __namedExportsOrder,q as default};
